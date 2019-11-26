cb4eeecf060a1ad68b3d5b6541fd4f5cd135a7b4
Michael Migliore <michael.migliore@kitware.com>
2019-11-26 12:17:58 +0100

    Fix icon names
    Former-commit-id: 651ad99b40f4ef99bd6b9b6d97891cb1184530aa

--- cmake/installing.cmake
+++ cmake/installing.cmake
@@ -8,11 +8,15 @@ if(UNIX AND NOT APPLE)
   install(FILES "${CMAKE_SOURCE_DIR}/resources/f3d.desktop"
     DESTINATION "share/applications")
   install(FILES "${CMAKE_SOURCE_DIR}/resources/logo32.png"
-    DESTINATION "share/icons/hicolor/32x32/apps")
+    DESTINATION "share/icons/hicolor/32x32/apps"
+    RENAME "f3d.png")
   install(FILES "${CMAKE_SOURCE_DIR}/resources/logo48.png"
-    DESTINATION "share/icons/hicolor/48x48/apps")
+    DESTINATION "share/icons/hicolor/48x48/apps"
+    RENAME "f3d.png")
   install(FILES "${CMAKE_SOURCE_DIR}/resources/logo64.png"
-    DESTINATION "share/icons/hicolor/64x64/apps")
+    DESTINATION "share/icons/hicolor/64x64/apps"
+    RENAME "f3d.png")
   install(FILES "${CMAKE_SOURCE_DIR}/resources/logo.svg"
-    DESTINATION "share/icons/hicolor/scalable/apps")
+    DESTINATION "share/icons/hicolor/scalable/apps"
+    RENAME "f3d.svg")
 endif()
