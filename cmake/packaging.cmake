bc363e755bab91167acdd4fd765e27129b56aae5
Mathieu Westphal <mathieu.westphal@kitware.com>
2020-03-12 05:16:52 +0100

    NSIS fixes
    Add automatic file associations with nsis
    Fix missing icon and correct name in uninstall
    
    
    Former-commit-id: bc8aad73d6b2f9c1d798b82a62fe89624ef81933

--- cmake/packaging.cmake
+++ cmake/packaging.cmake
@@ -21,7 +21,63 @@ if(WIN32 AND NOT UNIX)
   set(CPACK_NSIS_MODIFY_PATH ON)
   set(CPACK_NSIS_MUI_ICON ${f3d_ico})
   set(CPACK_NSIS_MUI_UNIICON ${f3d_ico})
+  set(CPACK_NSIS_INSTALLED_ICON_NAME "logo.ico")
   set(CPACK_NSIS_URL_INFO_ABOUT ${f3d_url})
+  set(CPACK_NSIS_ENABLE_UNINSTALL_BEFORE_INSTALL ON)
+  set(CPACK_NSIS_DISPLAY_NAME "f3d")
+
+  # Include the scripts
+  # FileAssociation.nsh, from https://nsis.sourceforge.io/File_Association, has to be installed in NSIS\Include
+  set(CPACK_NSIS_EXTRA_PREINSTALL_COMMANDS
+   "!include \\\"${PATH_TO_PLUGINS}\\\\FileFunc.nsh\\\"\n\
+   !include \\\"${PATH_TO_PLUGINS}\\\\FileAssociation.nsh\\\"")
+
+  # Create association on install
+  set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS
+    "\\\${RegisterExtension} '$INSTDIR\\\\bin\\\\f3d.exe' '.vtk' 'VTK Legacy Data Format'\n\
+    \\\${RegisterExtension} '$INSTDIR\\\\bin\\\\f3d.exe' '.vtp' 'VTK XML PolyData Format'\n\
+    \\\${RegisterExtension} '$INSTDIR\\\\bin\\\\f3d.exe' '.vtu' 'VTK XML UnstructuredGrid Format'\n\
+    \\\${RegisterExtension} '$INSTDIR\\\\bin\\\\f3d.exe' '.vti' 'VTK XML ImageData format'\n\
+    \\\${RegisterExtension} '$INSTDIR\\\\bin\\\\f3d.exe' '.vtr' 'VTK XML RectilinearGrid format'\n\
+    \\\${RegisterExtension} '$INSTDIR\\\\bin\\\\f3d.exe' '.vts' 'VTK XML StructuredGrid format'\n\
+    \\\${RegisterExtension} '$INSTDIR\\\\bin\\\\f3d.exe' '.vtm' 'VTK XML Multiblock Format'\n\
+    \\\${RegisterExtension} '$INSTDIR\\\\bin\\\\f3d.exe' '.ply' 'Polygon File format'\n\
+    \\\${RegisterExtension} '$INSTDIR\\\\bin\\\\f3d.exe' '.stl' 'Standard Triangle Language format'\n\
+    \\\${RegisterExtension} '$INSTDIR\\\\bin\\\\f3d.exe' '.dcm' 'DICOM file format'\n\
+    \\\${RegisterExtension} '$INSTDIR\\\\bin\\\\f3d.exe' '.nrrd' '\\\"nearly raw raster data\\\" file format'\n\
+    \\\${RegisterExtension} '$INSTDIR\\\\bin\\\\f3d.exe' '.nhrd' '\\\"nearly raw raster data\\\" file format'\n\
+    \\\${RegisterExtension} '$INSTDIR\\\\bin\\\\f3d.exe' '.mhd' 'MetaHeader MetaIO file format'\n\
+    \\\${RegisterExtension} '$INSTDIR\\\\bin\\\\f3d.exe' '.mha' 'MetaHeader MetaIO file format'\n\
+    \\\${RegisterExtension} '$INSTDIR\\\\bin\\\\f3d.exe' '.obj' 'Wavefront OBJ file format'\n\
+    \\\${RegisterExtension} '$INSTDIR\\\\bin\\\\f3d.exe' '.gltf' 'GL Transmission Format'\n\
+    \\\${RegisterExtension} '$INSTDIR\\\\bin\\\\f3d.exe' '.glb' 'GL Transmission Format'\n\
+    \\\${RegisterExtension} '$INSTDIR\\\\bin\\\\f3d.exe' '.3ds' 'Autodesk 3DS Max file format'\n\
+    \\\${RegisterExtension} '$INSTDIR\\\\bin\\\\f3d.exe' '.wrl' 'VRML file format'\n\
+    \\\${RefreshShellIcons}")
+
+  # Remove association on uninstall
+  set(CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS
+    "\\\${UnRegisterExtension} '.vtk' 'VTK Legacy Data Format'\n\
+    \\\${UnRegisterExtension} '.vtp' 'VTK XML PolyData Format'\n\
+    \\\${UnRegisterExtension} '.vtu' 'VTK XML UnstructuredGrid Format'\n\
+    \\\${UnRegisterExtension} '.vti' 'VTK XML ImageData format'\n\
+    \\\${UnRegisterExtension} '.vtr' 'VTK XML RectilinearGrid format'\n\
+    \\\${UnRegisterExtension} '.vts' 'VTK XML StructuredGrid format'\n\
+    \\\${UnRegisterExtension} '.vtm' 'VTK XML Multiblock Format'\n\
+    \\\${UnRegisterExtension} '.ply' 'Polygon File format'\n\
+    \\\${UnRegisterExtension} '.stl' 'Standard Triangle Language format'\n\
+    \\\${UnRegisterExtension} '.dcm' 'DICOM file format'\n\
+    \\\${UnRegisterExtension} '.nrrd' '\\\"nearly raw raster data\\\" file format'\n\
+    \\\${UnRegisterExtension} '.nhrd' '\\\"nearly raw raster data\\\" file format'\n\
+    \\\${UnRegisterExtension} '.mhd' 'MetaHeader MetaIO file format'\n\
+    \\\${UnRegisterExtension} '.mha' 'MetaHeader MetaIO file format'\n\
+    \\\${UnRegisterExtension} '.obj' 'Wavefront OBJ file format'\n\
+    \\\${UnRegisterExtension} '.gltf' 'GL Transmission Format'\n\
+    \\\${UnRegisterExtension} '.glb' 'GL Transmission Format'\n\
+    \\\${UnRegisterExtension} '.3ds' 'Autodesk 3DS Max file format'\n\
+    \\\${UnRegisterExtension} '.wrl' 'VRML file format'\n\
+    \\\${RefreshShellIcons}")
+
 else()
   if(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
     set(CPACK_GENERATOR TGZ TXZ)
