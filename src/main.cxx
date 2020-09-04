3c10ed2505df9a5b631af7fd762fe685eae25d2a
Michael Migliore <mcmigliore@gmail.com>
2020-09-04 20:57:53 +0200

    Add custom polydata mapper

--- src/main.cxx
+++ src/main.cxx
@@ -1,11 +1,18 @@
 #include "Config.h"
 #include "F3DLoader.h"
 
+#include "vtkF3DObjectFactory.h"
+
 int main(int argc, char** argv)
 {
 #if NDEBUG
   vtkObject::GlobalWarningDisplayOff();
 #endif
+
+  // instanciate our own polydata mapper
+  vtkNew<vtkF3DObjectFactory> factory;
+  vtkObjectFactory::RegisterFactory(factory);
+  vtkObjectFactory::SetAllEnableFlags(0, "vtkPolyDataMapper", "vtkOpenGLPolyDataMapper");
 
   F3DLoader loader;
   return loader.Start(argc, argv);
