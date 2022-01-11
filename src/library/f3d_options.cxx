b808465f8b2ede31e03926e736e2cd2ca9c1938b
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-01-12 00:16:39 +0100

    Better impl

--- src/library/f3d_options.cxx
+++ src/library/f3d_options.cxx
@@ -27,13 +27,12 @@ void options::set(const std::string &name, const T& value)
 }
 
 //----------------------------------------------------------------------------
-template void options::set<bool>(const std::string &name, const bool& value);
-template void options::set<int>(const std::string &name, const int& value);
-template void options::set<double>(const std::string &name, const double& value);
-template void options::set<std::string>(const std::string &name, const std::string& value);
-template void options::set<std::vector<int>>(const std::string &name, const std::vector<int>& value);
-template void options::set<std::vector<double>>(const std::string &name, const std::vector<double>& value);
-
+template void options::set<>(const std::string &name, const bool& value);
+template void options::set<>(const std::string &name, const int& value);
+template void options::set<>(const std::string &name, const double& value);
+template void options::set<>(const std::string &name, const std::string& value);
+template void options::set<>(const std::string &name, const std::vector<int>& value);
+template void options::set<>(const std::string &name, const std::vector<double>& value);
 
 //----------------------------------------------------------------------------
 template <typename T>
@@ -44,12 +43,12 @@ void options::get(const std::string &name, T& value) const
 }
 
 //----------------------------------------------------------------------------
-template void options::get<bool>(const std::string &name, bool& value) const;
-template void options::get<int>(const std::string &name, int& value) const;
-template void options::get<double>(const std::string &name, double& value) const;
-template void options::get<std::string>(const std::string &name, std::string& value) const;
-template void options::get<std::vector<int>>(const std::string &name, std::vector<int>& value) const;
-template void options::get<std::vector<double>>(const std::string &name, std::vector<double>& value) const;
+template void options::get<>(const std::string &name, bool& value) const;
+template void options::get<>(const std::string &name, int& value) const;
+template void options::get<>(const std::string &name, double& value) const;
+template void options::get<>(const std::string &name, std::string& value) const;
+template void options::get<>(const std::string &name, std::vector<int>& value) const;
+template void options::get<>(const std::string &name, std::vector<double>& value) const;
 
 //----------------------------------------------------------------------------
 template <typename T>
@@ -62,10 +61,10 @@ T options::get(const std::string &name) const
 }
 
 //----------------------------------------------------------------------------
-template bool options::get<bool>(const std::string &name) const;
-template int options::get<int>(const std::string &name) const;
-template double options::get<double>(const std::string &name) const;
-template std::string options::get<std::string>(const std::string &name) const;
-template std::vector<int> options::get<std::vector<int>>(const std::string &name) const;
-template std::vector<double> options::get<std::vector<double>>(const std::string &name) const;
+template bool options::get<>(const std::string &name) const;
+template int options::get<>(const std::string &name) const;
+template double options::get<>(const std::string &name) const;
+template std::string options::get<>(const std::string &name) const;
+template std::vector<int> options::get<>(const std::string &name) const;
+template std::vector<double> options::get<>(const std::string &name) const;
 }
