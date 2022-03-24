ee61aca2f48db5a79723a796e208a7a0e2cd38de
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-03-24 20:56:40 +0100

    fixup

--- .github/test_sdk/main.cxx
+++ .github/test_sdk/main.cxx
@@ -1,13 +1,8 @@
 #include <f3d_engine.h>
-#include <f3d_interactor.h>
-#include <f3d_log.h>
-#include <f3d_options.h>
-#include <f3d_window.h>
 
 int main()
 {
-  f3d::engine::WindowTypeEnum type = f3d::engine::WindowTypeEnum::WINDOW_STANDARD;
-  f3d::engine(type);
+  f3d::engine eng(f3d::engine::WindowTypeEnum::WINDOW_STANDARD);
 
   // TODO Actual test
 
