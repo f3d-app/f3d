b17cd188183cd32bda723986f656c4089855070f
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-04-06 00:07:06 +0200

    Adding a F3DLog test

--- src/library/VTKExtensions/Core/Testing/TestF3DLog.cxx
+++ src/library/VTKExtensions/Core/Testing/TestF3DLog.cxx
@@ -5,6 +5,18 @@
 
 int TestF3DLog(int argc, char* argv[])
 {
-  F3DLog::Print(F3DLog::Severity::Warning, "Test Output");
+  F3DLog::Print(F3DLog::Severity::Info, "Test Info ");
+  F3DLog::Print(F3DLog::Severity::Warning, "Test Warning ");
+  F3DLog::Print(F3DLog::Severity::Error, "Test Error ");
+
+  F3DLog::SetUseColoring(true);
+  F3DLog::Print(F3DLog::Severity::Info, "Test Info ");
+  F3DLog::Print(F3DLog::Severity::Warning, "Test Warning ");
+  F3DLog::Print(F3DLog::Severity::Error, "Test Error ");
+
+  F3DLog::SetQuiet(true);
+  F3DLog::Print(F3DLog::Severity::Info, "Test Info ");
+  F3DLog::Print(F3DLog::Severity::Warning, "Test Warning ");
+  F3DLog::Print(F3DLog::Severity::Error, "Test Error ");
   return EXIT_SUCCESS;
 }
