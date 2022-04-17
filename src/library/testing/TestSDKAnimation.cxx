ce9b341112701765da5186e734ce97d4626d3ac8
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-04-17 14:34:10 +0200

    Adding TestSDKAnimation

---
+++ src/library/testing/TestSDKAnimation.cxx
@@ -0,0 +1,23 @@
+#include <f3d_engine.h>
+#include <f3d_interactor.h>
+#include <f3d_loader.h>
+#include <f3d_window.h>
+
+int TestSDKAnimation(int argc, char* argv[])
+{
+  f3d::engine eng(f3d::engine::WindowTypeEnum::WINDOW_STANDARD);
+  f3d::loader& load = eng.getLoader();
+  load.addFile(std::string(argv[1]) + "/data/InterpolationTest.glb");
+  load.loadFile(f3d::loader::LoadFileEnum::LOAD_CURRENT);
+  f3d::window& win = eng.getWindow();
+  f3d::interactor& inter = eng.getInteractor();
+
+  inter.startAnimation();
+  inter.toggleAnimation();
+  if (inter.isPlayingAnimation())
+  {
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
+}
