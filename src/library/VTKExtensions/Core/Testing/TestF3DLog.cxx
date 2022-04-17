8d1328635eef3212f2f2fcafd3603c94e8d736b5
mwestphal <mathieu.westphal@kitware.com>
2022-04-17 09:08:21 +0200

    Small VTK modules improvements (#257)
     * Pass compile and link options to VTK modules
     * Add doc to F3DLog
     * Add test to F3DLog

--- src/library/VTKExtensions/Core/Testing/TestF3DLog.cxx
+++ src/library/VTKExtensions/Core/Testing/TestF3DLog.cxx
@@ -5,6 +5,20 @@
 
 int TestF3DLog(int argc, char* argv[])
 {
-  F3DLog::Print(F3DLog::Severity::Warning, "Test Output");
+  F3DLog::Print(F3DLog::Severity::Info, "Test Info ");
+  F3DLog::Print(F3DLog::Severity::Warning, "Test Warning ");
+  F3DLog::Print(F3DLog::Severity::Error, "Test Error ");
+
+  F3DLog::SetUseColoring(true);
+  F3DLog::Print(F3DLog::Severity::Info, "Test Info Coloring ");
+  F3DLog::Print(F3DLog::Severity::Warning, "Test Warning Coloring ");
+  F3DLog::Print(F3DLog::Severity::Error, "Test Error Coloring ");
+
+  F3DLog::SetQuiet(true); // Next print calls should print nothing
+  F3DLog::Print(F3DLog::Severity::Info, "Test Info Quiet ");
+  F3DLog::Print(F3DLog::Severity::Warning, "Test Warning Quiet ");
+  F3DLog::Print(F3DLog::Severity::Error, "Test Error Quiet ");
+
+  F3DLog::WaitForUser(); // Without the object factory created, this is expected to have no effect
   return EXIT_SUCCESS;
 }
