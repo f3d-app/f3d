ca17f5ba1ba50fe724cce9a89c5b450183f3f363
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-04-18 10:23:14 +0200

    Improve animation test

--- src/library/testing/TestSDKAnimation.cxx
+++ src/library/testing/TestSDKAnimation.cxx
@@ -3,19 +3,42 @@
 #include <f3d_loader.h>
 #include <f3d_window.h>
 
+#include <iostream>
+
 int TestSDKAnimation(int argc, char* argv[])
 {
   f3d::engine eng(f3d::engine::WindowTypeEnum::WINDOW_OFFSCREEN);
   f3d::loader& load = eng.getLoader();
-  load.addFile(std::string(argv[1]) + "/data/InterpolationTest.glb");
-  load.loadFile(f3d::loader::LoadFileEnum::LOAD_CURRENT);
   f3d::window& win = eng.getWindow();
   f3d::interactor& inter = eng.getInteractor();
+  load.addFile(std::string(argv[1]) + "/data/InterpolationTest.glb");
+  load.loadFile(f3d::loader::LoadFileEnum::LOAD_CURRENT);
 
   inter.startAnimation();
+  if (!inter.isPlayingAnimation())
+  {
+    std::cerr << "Animation is not playing even being started." << std::endl;
+    return EXIT_FAILURE;
+  }
+
   inter.toggleAnimation();
   if (inter.isPlayingAnimation())
   {
+    std::cerr << "Animation is playing even after being toggled off." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  inter.toggleAnimation();
+  if (!inter.isPlayingAnimation())
+  {
+    std::cerr << "Animation is not playing even after being toggled on." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  inter.stopAnimation();
+  if (inter.isPlayingAnimation())
+  {
+    std::cerr << "Animation is playing even after being stopped." << std::endl;
     return EXIT_FAILURE;
   }
 
