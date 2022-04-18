c3c46d0755a9d3719d586131f53eb92555aa7ff3
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-04-18 08:18:21 +0200

    Testing loader too

--- src/library/testing/TestSDKInteractionDirectory.cxx
+++ src/library/testing/TestSDKInteractionDirectory.cxx
@@ -4,6 +4,8 @@
 #include <f3d_options.h>
 #include <f3d_window.h>
 
+#include <iostream>
+
 int TestSDKInteractionDirectory(int argc, char* argv[])
 {
   // This test mimics TestInteractionDirectory.
@@ -18,7 +20,26 @@ int TestSDKInteractionDirectory(int argc, char* argv[])
   f3d::interactor& inter = eng.getInteractor();
 
   std::string filename = "TestSDKInteractionDirectory";
-  inter.playInteraction(std::string(argv[1]) + "recordings/" + filename + ".log");
+  inter.playInteraction(std::string(argv[1]) + "recordings/" + filename + ".log"); // Right;Right;Right;Left;Up;
+
+  if (load.getCurrentFileIndex() != 2)
+  {
+    std::cerr << std::string("Expecting a file index of 2, got ") + std::to_string(load.getCurrentFileIndex()) + "." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  load.loadFile(f3d::loader::LoadFileEnum::LOAD_FIRST);
+  load.loadFile(f3d::loader::LoadFileEnum::LOAD_LAST);
+
+  if (load.getCurrentFileIndex() != 3)
+  {
+    std::cerr << std::string("Expecting a file index of 3, got ") + std::to_string(load.getCurrentFileIndex()) + "." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  load.setCurrentFileIndex(2);
+  load.loadFile(f3d::loader::LoadFileEnum::LOAD_CURRENT);
+
   return win.renderAndCompareWithFile(std::string(argv[1]) + "/baselines/" + filename + ".png", 50,
            false, std::string(argv[2]) + filename + ".png")
     ? EXIT_SUCCESS
