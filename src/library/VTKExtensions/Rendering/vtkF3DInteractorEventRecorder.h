46e092d479f366c97432e2829c69b57d9b04d538
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-05-14 20:58:59 +0200

    Fixup for VTK

--- src/library/VTKExtensions/Rendering/vtkF3DInteractorEventRecorder.h
+++ src/library/VTKExtensions/Rendering/vtkF3DInteractorEventRecorder.h
@@ -29,20 +29,6 @@ protected:
   static void ProcessEvents(
     vtkObject* object, unsigned long event, void* clientdata, void* calldata);
 
-#if VTK_VERSION_NUMBER >= VTK_VERSION_CHECK(9, 1, 20220510)
-  /**
-   * A method that write an event to log file, with a trailing 0
-   */
-  void WriteEvent(const char* event, int pos[2], int modifiers, int keyCode, int repeatCount,
-    char* keySym) override;
-
-  /**
-   * A method that parse a event line and invoke the corresponding event
-   * support reading a serialized vtkStringArray
-   */
-  void ReadEvent(const std::string& line) override;
-#endif
-
 private:
   vtkF3DInteractorEventRecorder(const vtkF3DInteractorEventRecorder&) = delete;
   void operator=(const vtkF3DInteractorEventRecorder&) = delete;
