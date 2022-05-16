153424f02b6cc6a3b1b4059329c05ed30de057e1
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-05-16 21:25:42 +0200

    Rework library/application layer

--- src/library/f3d_config.h
+++ src/library/f3d_config.h
@@ -5,10 +5,6 @@
 
 namespace f3d
 {
-// TODO all of this should be prefixed Lib
-// Or move to the app dir accordingly
-extern const std::string AppName;
-extern const std::string AppTitle;
 extern const std::string LibVersion;
 extern const std::string LibBuildSystem;
 extern const std::string LibBuildDate;
