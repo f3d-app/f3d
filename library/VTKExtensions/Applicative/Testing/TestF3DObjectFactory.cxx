548a724a522817872ef5285d532002a676c8f4c4
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-05-21 15:26:30 +0200

    Fix factory and add testing

--- library/VTKExtensions/Applicative/Testing/TestF3DObjectFactory.cxx
+++ library/VTKExtensions/Applicative/Testing/TestF3DObjectFactory.cxx
@@ -1,10 +1,43 @@
-#include <vtkNew.h>
-#include <vtkTestUtilities.h>
-
+#include "vtkF3DConfigure.h"
 #include "vtkF3DObjectFactory.h"
+#include "vtkF3DPolyDataMapper.h"
+
+#if F3D_WINDOWS_GUI
+#include "vtkF3DWin32OutputWindow.h"
+#else
+#include "vtkF3DConsoleOutputWindow.h"
+#endif
+
+#include <vtkNew.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkTestUtilities.h>
 
 int TestF3DObjectFactory(int argc, char* argv[])
 {
   vtkNew<vtkF3DObjectFactory> factory;
+  vtkObjectFactory::RegisterFactory(factory);
+  vtkObjectFactory::SetAllEnableFlags(0, "vtkPolyDataMapper", "vtkOpenGLPolyDataMapper");
+
+  vtkNew<vtkPolyDataMapper> mapper;
+  vtkF3DPolyDataMapper* mapperPtr = vtkF3DPolyDataMapper::SafeDownCast(mapper);
+  if (mapperPtr == nullptr)
+  {
+    std::cerr << "vtkF3DObjectFactory failed to create a vtkF3DPolyDataMapper" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  vtkNew<vtkOutputWindow> window;
+#if F3D_WINDOWS_GUI
+  vtkF3DWin32OutputWindow* windowPtr = vtkF3DWin32OutputWindow::SafeDownCast(window);
+#else
+  vtkF3DConsoleOutputWindow* windowPtr = vtkF3DConsoleOutputWindow::SafeDownCast(window);
+#endif
+  if (windowPtr == nullptr)
+  {
+    std::cerr << "vtkF3DObjectFactory failed to create the correct type of vtkOutputWindow"
+              << std::endl;
+    return EXIT_FAILURE;
+  }
+
   return EXIT_SUCCESS;
 }
