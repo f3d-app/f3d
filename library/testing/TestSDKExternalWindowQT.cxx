5f4ceca4340be0b104b8e5ab6f2852193d18b8d8
Michael Migliore <mcmigliore@gmail.com>
2022-05-28 23:23:55 +0200

    improve qt test

--- library/testing/TestSDKExternalWindowQT.cxx
+++ library/testing/TestSDKExternalWindowQT.cxx
@@ -10,53 +10,63 @@
 class F3DWindow : public QOpenGLWindow
 {
 public:
-  F3DWindow(f3d::engine* engine)
+  F3DWindow(const std::string& filePath, const std::string& baseline)
     : QOpenGLWindow()
-    , mEngine(engine)
+    , mEngine(f3d::engine::CREATE_WINDOW | f3d::engine::WINDOW_EXTERNAL)
+    , mBaseline(baseline)
   {
+    f3d::loader& load = mEngine.getLoader();
+    load.addFile(filePath);
+    load.loadFile(f3d::loader::LoadFileEnum::LOAD_CURRENT);
   }
 
 protected:
   void resizeGL(int w, int h) override
   {
-    mEngine->getOptions().set("resolution", { w, h });
-    mEngine->getWindow().update();
+    mEngine.getOptions().set("resolution", { w, h });
+    mEngine.getWindow().update();
   }
 
   void keyPressEvent(QKeyEvent* event) override
   {
     if (event->key() == Qt::Key_Escape)
     {
-      close();
+      this->close();
     }
   }
 
   void timerEvent(QTimerEvent* event) override
   {
-    close();
+    // before closing, compare the content of the framebuffer with the baseline
+    QImage frameBuffer = grabFramebuffer().mirrored().convertToFormat(QImage::Format_RGB888);
+
+    f3d::image img;
+    img.setResolution(frameBuffer.width(), frameBuffer.height())
+      .setChannelCount(3)
+      .setData(frameBuffer.bits());
+
+    f3d::image diff;
+    if (!img.compare(f3d::image(mBaseline), diff, 50))
+    {
+      QCoreApplication::exit(EXIT_FAILURE);
+    }
+
+    this->close();
   }
 
-  void paintGL() override { mEngine->getWindow().render(); }
+  void paintGL() override { mEngine.getWindow().render(); }
 
 private:
-  f3d::engine* mEngine;
+  f3d::engine mEngine;
+  std::string mBaseline;
 };
 
 int TestSDKExternalWindowQT(int argc, char* argv[])
 {
-  // create engine and load file
-  f3d::engine eng(f3d::engine::CREATE_WINDOW | f3d::engine::WINDOW_EXTERNAL);
-  f3d::loader& load = eng.getLoader();
-  load.addFile(std::string(argv[1]) + "/data/cow.vtp");
-  load.loadFile(f3d::loader::LoadFileEnum::LOAD_CURRENT);
-
   QGuiApplication a(argc, argv);
-  QSurfaceFormat fmt;
-  fmt.setVersion(3, 2);
-  fmt.setProfile(QSurfaceFormat::CoreProfile);
-  QSurfaceFormat::setDefaultFormat(fmt);
 
-  F3DWindow w(&eng);
+  F3DWindow w(std::string(argv[1]) + "/data/cow.vtp",
+    std::string(argv[1]) + "/baselines/TestSDKExternalWindowQT.png");
   w.setTitle("F3D QT External Window");
   w.resize(300, 300);
   w.startTimer(1000);
