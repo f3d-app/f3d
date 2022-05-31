bf480112c6cc2cc98ba1053fd0190fb88a262436
Michael MIGLIORE <mcmigliore@gmail.com>
2022-05-31 19:18:31 +0200

    Improve test_sdk (#306)

--- .github/test_sdk/main.cxx
+++ .github/test_sdk/main.cxx
@@ -1,10 +1,16 @@
 #include <f3d/engine.h>
+#include <f3d/image.h>
+#include <f3d/interactor.h>
+#include <f3d/loader.h>
+#include <f3d/log.h>
+#include <f3d/options.h>
+#include <f3d/window.h>
 
 int main()
 {
-  f3d::engine eng(f3d::engine::CREATE_WINDOW | f3d::engine::CREATE_INTERACTOR);
+  f3d::engine eng(f3d::engine::FLAGS_NONE);
 
-  // TODO Actual test
+  f3d::log::info("F3D engine is loaded");
 
   return 0;
 }
