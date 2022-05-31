7d015ab0e8e37e9644cb8beee56ae0ceeda0c93b
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-05-31 17:24:03 +0200

    Improve options doc

--- library/public/options.h
+++ library/public/options.h
@@ -1,3 +1,11 @@
+/**
+ * @class   options
+ * @brief   Class used to control the different options
+ *
+ * A class to control the different options of f3d.
+ * See the TODO for the full listing of options
+ */
+
 #ifndef f3d_options_h
 #define f3d_options_h
 
@@ -7,19 +15,23 @@
 #include <string>
 #include <vector>
 
-// TODO documentation
-
 namespace f3d
 {
 class F3D_EXPORT options
 {
 public:
+
+  //@{
+  /**
+   * Default/Copy/move constructors/operators
+   */
   options();
   ~options();
   options(const options& opt);
   options& operator=(const options& opt);
   options(options&& other);
   options& operator=(options&& other);
+  //@}
 
   class exception : public std::runtime_error
   {
@@ -30,6 +42,10 @@ public:
     }
   };
 
+  //@{
+  /**
+   * Setters for all supported types
+   */
   options& set(const std::string& name, bool value);
   options& set(const std::string& name, int value);
   options& set(const std::string& name, double value);
@@ -39,28 +55,44 @@ public:
   options& set(const std::string& name, const std::vector<double>& values);
   options& set(const std::string& name, std::initializer_list<int> values);
   options& set(const std::string& name, std::initializer_list<double> values);
+  //@}
 
+  //@{
+  /**
+   * Reference getters for all supported types
+   */
   void get(const std::string& name, bool& value) const;
   void get(const std::string& name, int& value) const;
   void get(const std::string& name, double& value) const;
   void get(const std::string& name, std::string& value) const;
   void get(const std::string& name, std::vector<int>& value) const;
   void get(const std::string& name, std::vector<double>& value) const;
+  //@}
 
+  //@{
+  /**
+   * Explicit getters for all supported types
+   */
   bool getAsBool(const std::string& name) const;
   int getAsInt(const std::string& name) const;
   double getAsDouble(const std::string& name) const;
   std::string getAsString(const std::string& name) const;
   std::vector<int> getAsIntVector(const std::string& name) const;
   std::vector<double> getAsDoubleVector(const std::string& name) const;
+  //@}
 
-  // can throw exception
+  //@{
+  /**
+   * Explicit reference getters for all supported types.
+   * Can throw a f3d::options::exception in case of failure.
+   */
   bool& getAsBoolRef(const std::string& name);
   int& getAsIntRef(const std::string& name);
   double& getAsDoubleRef(const std::string& name);
   std::string& getAsStringRef(const std::string& name);
   std::vector<int>& getAsIntVectorRef(const std::string& name);
   std::vector<double>& getAsDoubleVectorRef(const std::string& name);
+  //@}
 
 private:
   class internals;
