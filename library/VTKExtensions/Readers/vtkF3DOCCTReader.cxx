290ca1831471cca8208d368efa9f7a1046dd6c24
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-06-01 08:12:13 +0200

    Fixup occt

--- library/VTKExtensions/Readers/vtkF3DOCCTReader.cxx
+++ library/VTKExtensions/Readers/vtkF3DOCCTReader.cxx
@@ -432,7 +432,7 @@ bool TransferToDocument(vtkF3DOCCTReader* that, T& reader, Handle(TDocStd_Docume
   reader.SetNameMode(true);
   reader.SetLayerMode(true);
 
-  if (reader.ReadFile(that->GetFileName()) == IFSelect_RetDone)
+  if (reader.ReadFile(that->GetFileName().c_str()) == IFSelect_RetDone)
   {
     ProgressIndicator pi(that);
     return reader.Transfer(doc, pi.Start());
@@ -509,7 +509,7 @@ int vtkF3DOCCTReader::RequestData(
     reader = new IGESControl_Reader();
   }
 
-  if (reader && reader->ReadFile(this->GetFileName()) == IFSelect_RetDone)
+  if (reader && reader->ReadFile(this->GetFileName().c_str()) == IFSelect_RetDone)
   {
     ProgressIndicator pi(this);
     reader->TransferRoots(pi.Start());
