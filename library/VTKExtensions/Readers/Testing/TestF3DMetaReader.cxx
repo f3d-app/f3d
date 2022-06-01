4397a21b020bd2497b28002059ee4387cb76bbf2
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-06-01 06:51:00 +0200

    TestF3DMetaReader

--- library/VTKExtensions/Readers/Testing/TestF3DMetaReader.cxx
+++ library/VTKExtensions/Readers/Testing/TestF3DMetaReader.cxx
@@ -7,24 +7,8 @@
 int TestF3DMetaReader(int argc, char* argv[])
 {
   F3DReaderInstantiator ReaderInstantiator;
-  vtkNew<vtkF3DMetaReader> reader;
-
-  /*  std::string dummyFilename = std::string(argv[1]) + "data/foo.dummy";
-    reader->SetFileName(dummyFilename);
-    if (reader->IsReaderValid())
-    {
-      std::cerr << "Reader unexpectedly can read a non-existent file" << std::endl;
-      return EXIT_FAILURE;
-    }
-    reader->Update();
-    reader->Print(cout);
-    if (reader->GetOutputInformation(0))
-    {
-      std::cerr << "Reader unexpectedly provide an output information for a non-existent file" <<
-    std::endl; return EXIT_FAILURE;
-    }*/ // TODO
-
   std::string filename = std::string(argv[1]) + "data/cow.vtp";
+  vtkNew<vtkF3DMetaReader> reader;
   reader->SetFileName(filename);
   reader->SetFileName(filename);
   if (!reader->IsReaderValid())
