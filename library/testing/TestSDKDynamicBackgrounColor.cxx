196ea5aa63c1c035f7099adf20c2277cd0b52efa
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-06-05 12:26:08 +0200

    Checking HDRI, color and FontFile

---
+++ library/testing/TestSDKDynamicBackgrounColor.cxx
@@ -0,0 +1,30 @@
+#include <engine.h>
+#include <loader.h>
+#include <options.h>
+#include <window.h>
+
+#include "TestSDKHelpers.h"
+
+int TestSDKDynamicBackgrounColor(int argc, char* argv[])
+{
+  f3d::engine eng(f3d::window::Type::NATIVE_OFFSCREEN);
+  f3d::loader& load = eng.getLoader();
+  f3d::window& win = eng.getWindow();
+  f3d::options& opt = eng.getOptions();
+  win.setSize(300, 300);
+  opt.set("filename", true);
+
+  load.addFile(std::string(argv[1]) + "/data/cow.vtp");
+  load.loadFile(f3d::loader::LoadFileEnum::LOAD_CURRENT);
+
+  win.render();
+
+  // Change the background color and make sure it is taken into account
+  opt.set("background-color", { 1.0, 1.0, 1.0 });
+  win.update();
+
+  return TestSDKHelpers::RenderTest(eng.getWindow(), std::string(argv[1]) + "baselines/",
+           std::string(argv[2]), "TestSDKDynamicBackgrounColor", 50)
+    ? EXIT_SUCCESS
+    : EXIT_FAILURE;
+}
