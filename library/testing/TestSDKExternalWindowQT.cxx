8045ae6bb1ba6fad73fc9e598aed4e2258b284f8
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-06-13 23:31:05 +0200

    Cleanup

--- library/testing/TestSDKExternalWindowQT.cxx
+++ library/testing/TestSDKExternalWindowQT.cxx
@@ -67,11 +67,9 @@ int TestSDKExternalWindowQT(int argc, char* argv[])
     std::string(argv[1]) + "/data/cow.vtp", std::string(argv[1]) + "/baselines/", argv[2]);
   w.setTitle("F3D QT External Window");
   w.resize(300, 300);
-  int timerId = w.startTimer(1000);
+  w.startTimer(1000);
 
   w.show();
 
-  bool ret = a.exec();
-  w.killTimer(timerId);
-  return ret;
+  return a.exec();
 }
