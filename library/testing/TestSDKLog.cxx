6625d35d64b78d1d2ef8ad28e0b83c181faea2d4
Mathieu Westphal <mathieu.westphal@kitware.com>
2022-06-25 16:30:10 +0200

    Add log::print test

--- library/testing/TestSDKLog.cxx
+++ library/testing/TestSDKLog.cxx
@@ -28,12 +28,25 @@ int TestSDKLog(int argc, char* argv[])
   f3d::log::warn("Test Warning Quiet");
   f3d::log::error("Test Error Quiet");
 
+  f3d::log::setVerboseLevel(f3d::log::VerboseLevel::DEBUG); // All print should print except QUIET
+  f3d::log::print(f3d::log::VerboseLevel::QUIET, "Test Print Quiet");
+  f3d::log::print(f3d::log::VerboseLevel::DEBUG, "Test Print Debug");
+  f3d::log::print(f3d::log::VerboseLevel::INFO, "Test Print Info");
+  f3d::log::print(f3d::log::VerboseLevel::WARN, "Test Print Warning");
+  f3d::log::print(f3d::log::VerboseLevel::ERROR, "Test Print Error");
+
   f3d::log::setVerboseLevel(f3d::log::VerboseLevel::DEBUG); // Next log calls should all be printed
   f3d::log::setUseColoring(true);
   f3d::log::info("Test Debug Coloring");
   f3d::log::info("Test Info Coloring");
   f3d::log::warn("Test Warning Coloring");
   f3d::log::error("Test Error Coloring");
+
+  f3d::log::print(f3d::log::VerboseLevel::QUIET, "Test Print Quiet Coloring");
+  f3d::log::print(f3d::log::VerboseLevel::DEBUG, "Test Print Debug Coloring");
+  f3d::log::print(f3d::log::VerboseLevel::INFO, "Test Print Info Coloring");
+  f3d::log::print(f3d::log::VerboseLevel::WARN, "Test Print Warning Coloring");
+  f3d::log::print(f3d::log::VerboseLevel::ERROR, "Test Print Error Coloring");
 
   f3d::log::waitForUser(); // This just returns immediately in testing environment
   return EXIT_SUCCESS;
