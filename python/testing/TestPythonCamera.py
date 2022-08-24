49fe2253a3035bda02dbc521890fb128f2ed9665
mwestphal <mathieu.westphal@kitware.com>
2022-08-24 16:07:39 +0800

    Add camera python bindings (#363)
    * Add camera python bindings
    * Adding TestPythonCamera test

---
+++ python/testing/TestPythonCamera.py
@@ -0,0 +1,42 @@
+import f3d
+
+engine = f3d.engine(f3d.window.NATIVE_OFFSCREEN)
+camera = engine.getWindow().getCamera()
+
+# Behavior is tested in the SDK test, test only the bindings
+
+# TODO Once angle_deg_t is an actual struct, this will need to be changed
+angle = 30
+camera.setViewAngle(angle)
+camera.getViewAngle(angle)
+angle = camera.getViewAngle()
+
+point = f3d.point3_t(0, 0, 0)
+camera.setPosition(point)
+camera.getPosition(point)
+point = camera.getPosition()
+
+camera.setFocalPoint(point)
+camera.getFocalPoint(point)
+point = camera.getFocalPoint()
+
+vector = f3d.vector3_t(0, 0, 1)
+camera.setViewUp(vector)
+camera.getViewUp(vector)
+vector = camera.getViewUp()
+
+matrix = f3d.matrix4_t(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, -1, 0, 0.5, -0.5, 10, 1)
+camera.setViewMatrix(matrix)
+camera.getViewMatrix(matrix)
+matrix = camera.getViewMatrix()
+
+camera.dolly(10)
+camera.roll(angle)
+camera.azimuth(angle)
+camera.yaw(angle)
+camera.elevation(angle)
+camera.pitch(angle)
+
+camera.setCurrentAsDefault()
+camera.resetToBounds()
+camera.resetToDefault()
