7ab06bca77c600af62d5a69eaa2045f927a85a42
Mathieu Westphal <mathieu.westphal@kitware.com>
2023-01-22 18:40:00 +0800

    input sanity generic importer

--- library/VTKExtensions/Readers/vtkF3DGenericImporter.cxx
+++ library/VTKExtensions/Readers/vtkF3DGenericImporter.cxx
@@ -124,6 +124,13 @@ void vtkF3DGenericImporter::ImportActors(vtkRenderer* ren)
   this->PostPro->SetInputConnection(this->Reader->GetOutputPort());
   this->PostPro->Update();
 
+  vtkDataObject* readerOutput = this->Reader->GetOutputDataObject(0);
+  if (!readerOutput)
+  {
+    F3DLog::Print(F3DLog::Severity::Error, "Reader did not produce any output");
+    return;
+  }
+
   this->OutputDescription =
     vtkF3DGenericImporter::GetDataObjectDescription(this->Reader->GetOutputDataObject(0));
 
