efd61409c05b1de44996a0efd9e0b7379d40ce86
Mathieu Westphal <mathieu.westphal@kitware.com>
2023-03-09 08:11:21 +0100

    Improving Generic Importer test

--- library/VTKExtensions/Readers/Testing/TestF3DGenericImporter.cxx
+++ library/VTKExtensions/Readers/Testing/TestF3DGenericImporter.cxx
@@ -1,5 +1,7 @@
+#include <vtkDoubleArray.h>
 #include <vtkGLTFReader.h>
 #include <vtkNew.h>
+#include <vtkPLYReader.h>
 #include <vtkTestUtilities.h>
 
 #include "vtkF3DGenericImporter.h"
@@ -10,6 +12,7 @@ int TestF3DGenericImporter(int argc, char* argv[])
 {
   vtkNew<vtkF3DGenericImporter> importer;
 
+  // Test without any reader
   if (importer->CanReadFile())
   {
     std::cerr << "Importer unexpectedly can read a file without internal reader" << std::endl;
@@ -23,7 +26,24 @@ int TestF3DGenericImporter(int argc, char* argv[])
     return EXIT_FAILURE;
   }
 
-  // Test valid reader
+  int nbTimeSteps;
+  double timeRange[2];
+  vtkNew<vtkDoubleArray> timeSteps;
+
+  // Complete GetTemporalInformation needs https://gitlab.kitware.com/vtk/vtk/-/merge_requests/7246
+#if VTK_VERSION_NUMBER >= VTK_VERSION_CHECK(9, 0, 20201016)
+  bool ret = importer->GetTemporalInformation(
+    0, 60, nbTimeSteps, timeRange, timeSteps);
+#else
+  bool ret = importer->GetTemporalInformation(0, nbTimeSteps, timeRange, timeSteps);
+#endif
+  if (ret)
+  {
+    std::cerr << "Unexpected return value with GetTemporalInformation" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Test with reader
   vtkNew<vtkGLTFReader> reader;
 
   std::string filename = std::string(argv[1]) + "data/BoxAnimated.gltf";
@@ -57,6 +77,32 @@ int TestF3DGenericImporter(int argc, char* argv[])
   if (importer->IsAnimationEnabled(0))
   {
     std::cerr << "Unexpected enabled animation" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Test with multiple readers
+  vtkNew<vtkPLYReader> reader2;
+  filename = std::string(argv[1]) + "data/suzanne.ply";
+  reader2->SetFileName(filename.c_str());
+  importer->AddInternalReader("Test2", reader2);
+
+  if (!importer->CanReadFile())
+  {
+    std::cerr << "Importer unexpectedly can not read a valid file" << std::endl;
+    return EXIT_FAILURE;
+  }
+  importer->Update();
+  std::string description = importer->GetMetaDataDescription();
+  if (description.find("Number of geometries: 2") == std::string::npos)
+  {
+    std::cerr << "Unexpected meta data description with multiple geometries" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // Static method testing
+  if (vtkF3DGenericImporter::GetDataObjectDescription(nullptr) != "")
+  {
+    std::cerr << "Unexpected data object description with null input" << std::endl;
     return EXIT_FAILURE;
   }
 
