dbad701720023eb3b914b6ecf49f6f2644c5cf8a
Mathieu Westphal <mathieu.westphal@kitware.com>
2023-03-29 13:15:01 +0200

    Fixup test

--- library/testing/TestSDKInteractorDropFullScene.cxx
+++ library/testing/TestSDKInteractorDropFullScene.cxx
@@ -18,12 +18,7 @@ int TestSDKInteractorDropFullScene(int argc, char* argv[])
   win.setSize(300, 300);
 
   std::string filename = "TestSDKInteractorDropFullScene";
-#if defined(_WIN32)
-  std::string interactionFilePath = std::string(argv[2]) + "..\\..\\" + filename + ".log";
-#else
   std::string interactionFilePath = std::string(argv[2]) + "../../" + filename + ".log";
-#endif
-
   inter.playInteraction(interactionFilePath); // world.obj; S
 
   return TestSDKHelpers::RenderTest(
