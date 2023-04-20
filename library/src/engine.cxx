12e133cb479a31f7aa4fe3635821cdd1e5db6569
Mathieu Westphal <mathieu.westphal@gmail.com>
2023-04-20 12:07:07 +0200

    Update copyright year (#742)

--- library/src/engine.cxx
+++ library/src/engine.cxx
@@ -300,7 +300,7 @@ engine::libInformation engine::getLibInfo()
     std::to_string(VTK_BUILD_VERSION) + std::string(")");
 
   libInfo.PreviousCopyright = "Copyright (C) 2019-2021 Kitware SAS";
-  libInfo.Copyright = "Copyright (C) 2021-2022 Michael Migliore, Mathieu Westphal";
+  libInfo.Copyright = "Copyright (C) 2021-2023 Michael Migliore, Mathieu Westphal";
   libInfo.License = "BSD-3-Clause";
   libInfo.Authors = "Michael Migliore, Mathieu Westphal and Joachim Pouderoux";
 
