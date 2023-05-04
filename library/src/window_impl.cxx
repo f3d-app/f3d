1f2d561d1e6bea146912ece624ff1c66c1867782
Mathieu Westphal <mathieu.westphal@kitware.com>
2023-05-04 07:43:04 +0200

    upload

--- library/src/window_impl.cxx
+++ library/src/window_impl.cxx
@@ -415,6 +415,7 @@ image window_impl::renderToImage(bool noBackground)
 
   exporter->Export(output.getData());
 
+  std::cout<<this->Internals->RenWin->ReportCapabilities()<<std::endl;
   return output;
 }
 
