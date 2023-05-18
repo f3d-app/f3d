b7c1964e26a6538e77f03c0a14df638745e6d2e6
Mathieu Westphal <mathieu.westphal@kitware.com>
2023-05-18 19:43:31 +0800

    Fix VTK Version in --version

--- library/src/engine.cxx
+++ library/src/engine.cxx
@@ -297,8 +297,23 @@ engine::libInformation engine::getLibInfo()
 #endif
   libInfo.ExternalRenderingModule = tmp;
 
-  libInfo.VTKVersion = std::string(VTK_VERSION) + std::string(" (build ") +
-    std::to_string(VTK_BUILD_VERSION) + std::string(")");
+  // First version of VTK including the version check (and the feature used)
+#if VTK_VERSION_NUMBER >= VTK_VERSION_CHECK(9, 0, 20200527)
+  std::string vtkVersion = std::string(vtkVersion::GetVTKVersionFull());
+  if (!vtkVersion.empty())
+  {
+    libInfo.VTKVersion = vtkVersion;
+    std::string date = std::to_string(vtkVersion::GetVTKBuildVersion());
+    if (date.size() == 8)
+    {
+      libInfo.VTKVersion += std::string(" (date: ") + date + ")";
+    }
+  }
+  else
+#endif
+  {
+    libInfo.VTKVersion = vtkVersion::GetVTKVersion();
+  }
 
   libInfo.PreviousCopyright = "Copyright (C) 2019-2021 Kitware SAS";
   libInfo.Copyright = "Copyright (C) 2021-2023 Michael Migliore, Mathieu Westphal";
