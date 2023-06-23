ac39c59b3af92ec24a31cc8eea674a8ee805289f
Michael Migliore <mcmigliore@gmail.com>
2023-06-23 16:52:42 +0200

    fix test

--- python/testing/TestPythonPlugins.py
+++ python/testing/TestPythonPlugins.py
@@ -6,10 +6,14 @@ if sys.platform.startswith('win32'):
 
 import f3d
 
-def test_plugins():
-  plugins = f3d.engine.getPluginsList(os.path.dirname(f3d.__file__) + '/share/f3d/plugins')
+from pathlib import Path
+
+ROOT_DIR = Path(f3d.__file__).parent
+
+def test_plugins(root_dir = ROOT_DIR):
+  plugins = f3d.engine.getPluginsList(str(root_dir / 'share' / 'f3d' / 'plugins'))
   assert len(plugins) > 0
   assert plugins.index('native') >= 0
 
 if __name__ == '__main__':
-  test_plugins()
+  test_plugins(Path(f3d.__file__).parent.parent)
