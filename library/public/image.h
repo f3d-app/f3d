d766c805b6d6e3c3b1a784000ae9611f23da5851
Michael Migliore <mcmigliore@gmail.com>
2023-07-10 15:04:45 +0200

    style

--- library/public/image.h
+++ library/public/image.h
@@ -123,7 +123,7 @@ public:
   /**
    * Set/Get image buffer data.
    * Its size is expected to be `width * height * channelCount * typeSize`.
-   * 
+   *
    * \deprecated { setData and getData are deprecated, use setContent and getContent instead }
    */
   image& setContent(void* buffer);
