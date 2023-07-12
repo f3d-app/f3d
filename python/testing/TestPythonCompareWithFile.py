e736cd0256348f5b53246c7fe5f367a42cd0309b
snoyer <noyer.stephane@gmail.com>
2023-07-12 07:18:00 +0400

    split deprecated tests

--- python/testing/TestPythonCompareWithFile.py
+++ python/testing/TestPythonCompareWithFile.py
@@ -1,5 +1,3 @@
-import os
-
 import pytest
 
 import f3d
@@ -49,39 +47,3 @@ def test_compare_with_file(f3d_source_dir, cmake_binary_dir):
     error = 0.0
 
     assert img.compare(f3d.image(reference), 50, diff, error)
-
-
-@pytest.mark.skipif(
-    os.environ.get("F3D_NO_DEPRECATED"), reason="F3D_NO_DEPRECATED is set"
-)
-def test_legacy_tests(f3d_source_dir, cmake_binary_dir):
-    dataset = f3d_source_dir + "/testing/data/cow.vtp"
-    reference = f3d_source_dir + "/testing/baselines/TestPythonCompareWithFile.png"
-    output = cmake_binary_dir + "/Testing/Temporary/TestPythonCompareWithFile.png"
-    outputDiff = (
-        cmake_binary_dir + "/Testing/Temporary/TestPythonCompareWithFile.diff.png"
-    )
-
-    f3d.engine.autoloadPlugins()
-
-    engine = f3d.engine(f3d.window.NATIVE_OFFSCREEN)
-    engine.getWindow().setSize(300, 300)
-
-    # verify the size is properly set
-    assert engine.getWindow().getWidth() == 300
-    assert engine.getWindow().getHeight() == 300
-
-    engine.getLoader().loadGeometry(dataset, True)
-
-    img = engine.getWindow().renderToImage()
-    img.save(output)
-
-    diff = f3d.image()
-    error = 0.0
-
-    ret = img.compare(f3d.image(reference), 50, diff, error)
-
-    if not ret:
-        diff.save(outputDiff)
-
-    assert ret is True
--- python/testing/TestPythonPlugins.py
+++ python/testing/TestPythonPlugins.py
@@ -11,10 +11,7 @@ def cmake_binary_dir(pytestconfig):
     return pytestconfig.getoption("cmake_binary_dir")
 
 
-@pytest.mark.skipif(
-    os.environ.get("F3D_NO_DEPRECATED"), reason="F3D_NO_DEPRECATED is set"
-)
-def test_legacy_tests(cmake_binary_dir):
-    plugins = f3d.engine.getPluginsList(cmake_binary_dir + "/share/f3d/plugins")
+def test_plugins_list(cmake_binary_dir):
+    plugins = f3d.engine.get_plugins_list(cmake_binary_dir + "/share/f3d/plugins")
     assert len(plugins) > 0
     assert plugins.index("native") >= 0
