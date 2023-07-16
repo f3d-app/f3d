3a9a4e206c8e43f72e8614a18b734a342579bd09
Michael Migliore <mcmigliore@gmail.com>
2023-07-16 11:08:27 +0200

    review

--- python/cmake/f3dPython.cmake
+++ python/cmake/f3dPython.cmake
@@ -1,24 +1,13 @@
+# On Windows, usage of PATH to find the DLL is not possible anymore
+# see https://stackoverflow.com/a/64303856/2609654
+# This function should be called to generate F3D_ABSOLUTE_DLLS_FIXUP and F3D_RELATIVE_DLLS_FIXUP
+# CMake variables used to fill the "blessed" paths for Windows
 function(f3d_python_windows_dll_fixup)
-	cmake_parse_arguments(F3D_DLL_FIXUP "" "OUTPUT" "RELATIVE_PATHS;ABSOLUTE_PATHS" ${ARGN})
+	cmake_parse_arguments(F3D_DLL_FIXUP "" "OUTPUT" "PATHS" ${ARGN})
 
-	if(WIN32)
-		set(_fixup [=[
-if sys.version_info >= (3, 8) and sys.platform == 'win32':
-    from pathlib import Path
-    import os
-]=]
-		)
-
-		foreach(_path ${F3D_DLL_FIXUP_ABSOLUTE_PATHS})
-			set(_fixup "${_fixup}    os.add_dll_directory('${_path}')\n")
-		endforeach()
-
-		foreach(_path ${F3D_DLL_FIXUP_RELATIVE_PATHS})
-			set(_fixup "${_fixup}    os.add_dll_directory((Path(__file__).parent / '${_path}').resolve())\n")
-		endforeach()
-
-		set(${F3D_DLL_FIXUP_OUTPUT} ${_fixup} PARENT_SCOPE)
-	else()
-		set(${F3D_DLL_FIXUP_OUTPUT} "" PARENT_SCOPE)
-	endif()
+	set(_fixup "generated at build\n")
+	foreach(_path ${F3D_DLL_FIXUP_PATHS})
+		set(_fixup "${_fixup}    '${_path}',\n")
+	endforeach()
+	set(${F3D_DLL_FIXUP_OUTPUT} ${_fixup} PARENT_SCOPE)
 endfunction()
