3d4b0ebdd154bf8b2c55332eee2741422c982b06
snoyer <noyer.stephane@gmail.com>
2023-08-20 18:59:06 +0100

    improve testing

--- plugins/occt/module/Testing/TestF3DOCCTReader.cxx
+++ plugins/occt/module/Testing/TestF3DOCCTReader.cxx
@@ -4,19 +4,27 @@
 
 #include "vtkF3DOCCTReader.h"
 
+#include <cassert>
 #include <iostream>
 
-int TestF3DOCCTReader(int vtkNotUsed(argc), char* argv[])
+void testReader(const std::string& filename, const vtkF3DOCCTReader::FILE_FORMAT& format)
 {
-  std::string filename = std::string(argv[1]) + "data/f3d.stp";
   vtkNew<vtkF3DOCCTReader> reader;
   reader->RelativeDeflectionOn();
   reader->SetLinearDeflection(0.1);
   reader->SetAngularDeflection(0.5);
   reader->ReadWireOn();
   reader->SetFileName(filename);
-  reader->SetFileFormat(vtkF3DOCCTReader::FILE_FORMAT::STEP);
+  reader->SetFileFormat(format);
   reader->Update();
   reader->Print(cout);
-  return reader->GetOutput()->GetNumberOfBlocks() > 0 ? EXIT_SUCCESS : EXIT_FAILURE;
+  assert(reader->GetOutput()->GetNumberOfBlocks() > 0);
+}
+
+int TestF3DOCCTReader(int vtkNotUsed(argc), char* argv[])
+{
+  testReader(std::string(argv[1]) + "data/f3d.stp", vtkF3DOCCTReader::FILE_FORMAT::STEP);
+  testReader(std::string(argv[1]) + "data/f3d.igs", vtkF3DOCCTReader::FILE_FORMAT::IGES);
+  testReader(std::string(argv[1]) + "data/f3d.brep", vtkF3DOCCTReader::FILE_FORMAT::BREP);
+  return EXIT_SUCCESS;
 }
