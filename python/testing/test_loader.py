09719ce40f50871ad612abe67b8c29a8f7c3d977
Michael Migliore <mcmigliore@gmail.com>
2023-09-12 09:15:57 +0200

    Fix load from memory

--- python/testing/test_loader.py
+++ python/testing/test_loader.py
@@ -1,10 +1,13 @@
+from pathlib import Path
 import pytest
 import tempfile
+
 import f3d
 
 
 def test_load_memory():
-    reference = "./testing/baselines/TestPythonLoadMemory.png"
+    testing_dir = Path(__file__).parent.parent.parent / "testing"
+    reference = f"{testing_dir}/baselines/TestPythonLoadMemory.png"
     output = tempfile.gettempdir() + "/TestPythonLoadMemory.png"
     outputDiff = tempfile.gettempdir() + "/TestPythonLoadMemory.diff.png"
 
