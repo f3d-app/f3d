7ab558d221a5f5eb51fbfd977a5a2240b258f818
Michael Migliore <mcmigliore@gmail.com>
2023-12-21 22:42:11 +0100

    Fix animation with many bones

--- library/VTKExtensions/Rendering/vtkF3DPolyDataMapper.cxx
+++ library/VTKExtensions/Rendering/vtkF3DPolyDataMapper.cxx
@@ -1,5 +1,7 @@
 #include "vtkF3DPolyDataMapper.h"
 
+#include "F3DLog.h"
+
 #include <vtkActor.h>
 #include <vtkDoubleArray.h>
 #include <vtkMatrix4x4.h>
@@ -130,25 +132,64 @@ void vtkF3DPolyDataMapper::ReplaceShaderValues(
   type = uniforms->GetUniformTupleType("jointMatrices");
   if (type != vtkUniforms::TupleTypeInvalid)
   {
-    customDecl += "in vec4 joints;\n"
-                  "in vec4 weights;\n";
+    int nbJoints = uniforms->GetUniformNumberOfTuples("jointMatrices");
 
-    // compute skinning matrix with current uniform weights
-    beginImpl += "  mat4 skinMat = weights.x * jointMatrices[int(joints.x)]\n"
-                 "               + weights.y * jointMatrices[int(joints.y)]\n"
-                 "               + weights.z * jointMatrices[int(joints.z)]\n"
-                 "               + weights.w * jointMatrices[int(joints.w)];\n";
+    bool skinningSupported = true;
 
-    posImpl += "  posMC = skinMat * posMC;\n";
-
-    // apply the matrix to normals and tangents
-    if (hasNormals)
+    // The number of uniform values required by OpenGL is 4096
+    // Since a mat4 is 16 values, it means we can only support 256 bones
+    // However, there are other uniform values used in practice for other
+    // things like materials and we've seen issues starting at 253 bones
+    // let's be conservative here and trigger SSBO at 250 bones
+    if (nbJoints > 250)
     {
-      normalImpl += "  normalVCVSOutput = mat3(skinMat) * normalVCVSOutput;\n";
+#if VTK_VERSION_NUMBER >= VTK_VERSION_CHECK(9, 3, 20231108)
+      std::vector<float> buffer(16 * sizeof(float) * nbJoints);
+      uniforms->GetUniformMatrix4x4v("jointMatrices", buffer);
+
+      this->JointMatrices->Upload(buffer, vtkOpenGLBufferObject::ArrayBuffer);
+      this->JointMatrices->BindShaderStorage(0);
+
+      uniforms->RemoveUniform("jointMatrices");
+
+      customDecl += "layout(std430, binding = 0) readonly buffer JointsData\n"
+                    "{\n"
+                    "  mat4 jointMatrices[];\n"
+                    "};\n";
+
+      // 4.3 is required for SSBO
+      vtkShaderProgram::Substitute(VSSource, "//VTK::System::Dec", "#version 430");
+#else
+      std::string msg = "A mesh is associated with " + std::to_string(nbJoints) +
+        " bones, which is not supported by VTK <= 9.3";
+      F3DLog::Print(F3DLog::Severity::Warning, msg);
+
+      skinningSupported = false;
+#endif
     }
-    if (hasTangents)
+
+    if (skinningSupported)
     {
-      normalImpl += "  tangentVCVSOutput = mat3(skinMat) * tangentVCVSOutput;\n";
+      customDecl += "in vec4 joints;\n"
+                    "in vec4 weights;\n";
+
+      // compute skinning matrix with current uniform weights
+      beginImpl += "  mat4 skinMat = weights.x * jointMatrices[int(joints.x)]\n"
+                   "               + weights.y * jointMatrices[int(joints.y)]\n"
+                   "               + weights.z * jointMatrices[int(joints.z)]\n"
+                   "               + weights.w * jointMatrices[int(joints.w)];\n";
+
+      posImpl += "  posMC = skinMat * posMC;\n";
+
+      // apply the matrix to normals and tangents
+      if (hasNormals)
+      {
+        normalImpl += "  normalVCVSOutput = mat3(skinMat) * normalVCVSOutput;\n";
+      }
+      if (hasTangents)
+      {
+        normalImpl += "  tangentVCVSOutput = mat3(skinMat) * tangentVCVSOutput;\n";
+      }
     }
   }
 
