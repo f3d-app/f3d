c3bf40e1571da24a168977027e64697f93c8f4cc
Michael Migliore <mcmigliore@gmail.com>
2024-02-04 22:21:55 +0100

    try

--- library/VTKExtensions/Rendering/Testing/TestF3DBitonicSort.cxx
+++ library/VTKExtensions/Rendering/Testing/TestF3DBitonicSort.cxx
@@ -30,12 +30,12 @@ int TestF3DBitonicSort(int argc, char* argv[])
   constexpr int nbElements = 10000;
 
   // fill CPU keys and values buffers
-  std::vector<double> keys(nbElements);
+  std::vector<float> keys(nbElements);
   std::vector<int> values(nbElements);
 
   std::random_device dev;
   std::mt19937 rng(dev());
-  std::uniform_real_distribution<double> dist(0.0, 1.0);
+  std::uniform_real_distribution<float> dist(0.0f, 1.0f);
 
   std::generate(std::begin(keys), std::end(keys), [&]() { return dist(rng); });
   std::fill(std::begin(values), std::end(values), 0); // we do not care about the values
@@ -49,7 +49,7 @@ int TestF3DBitonicSort(int argc, char* argv[])
 
   // sort
   vtkNew<vtkF3DBitonicSort> sorter;
-  sorter->Initialize(128, VTK_DOUBLE, VTK_INT);
+  sorter->Initialize(128, VTK_FLOAT, VTK_INT);
   sorter->Run(vtkOpenGLRenderWindow::SafeDownCast(renWin), nbElements, bufferKeys, bufferValues);
 
   // download sorted key buffer to CPU
