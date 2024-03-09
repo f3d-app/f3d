4115b12b4bdc2c85fe495370234bf3a1abfba0ba
Mathieu Westphal <mathieu.westphal@kitware.com>
2024-03-09 17:55:13 +0100

    fixup install plugin

--- cmake/plugin-config.cmake
+++ cmake/plugin-config.cmake
@@ -1,5 +1,3 @@
-set(f3d_PLUGINS_INSTALL_DIR "@F3D_PLUGINS_INSTALL_DIR@")
-
 # Find VTK at top level as f3d_vtkext-targets need it
 find_package(VTK 9.0 REQUIRED COMPONENTS CommonCore CommonExecutionModel IOImport)
 include("${CMAKE_CURRENT_LIST_DIR}/f3dPlugin.cmake")
