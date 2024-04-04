931c9a2e0d04a69cc456f11fa597cc77ec8d1e3f
David Allemang <david.allemang@kitware.com>
2024-04-04 10:56:14 -0400

    Fix zsh completion when an option is specified in comments (#1365)

--- resources/completion.zsh
+++ resources/completion.zsh
@@ -8,7 +8,7 @@ local f3dhelp
 f3dhelp=$(f3d --help 2>&1 | sed '1,/Examples/!d' | sed 's/\[.*\] *//')
 
 shortopts=$(echo $f3dhelp | grep "^\s*[-].," | sed "s/^ *\(-.\), *--[^ ]* *\(.*\)$/\1[\2]/")
-longopts=$(echo $f3dhelp | grep "[-]-" | sed 's/=.*>//g' | sed 's/-.,//g' | sed "s/^ *\([^ ]*\) *\(.*\)$/\1[\2]/g")
+longopts=$(echo $f3dhelp | grep "[, ] [-]-" | sed 's/=.*>//g' | sed 's/-.,//g' | sed "s/^ *\([^ ]*\) *\(.*\)$/\1[\2]/g")
 
 arguments=("${(f)shortopts}")
 arguments+=("${(f)longopts}")
