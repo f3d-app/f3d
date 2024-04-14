72c5edd4c60cb2ae4a5d5100dbd3c0e27d34a9e0
Mathieu Westphal <mathieu.westphal@kitware.com>
2024-04-14 09:52:12 +0200

    Fix incorrect usage of internal variable PACKAGE_PREFIX_DIR

--- cmake/library-config.cmake
+++ cmake/library-config.cmake
@@ -2,7 +2,7 @@
 include("${CMAKE_CURRENT_LIST_DIR}/f3dLibraryTargets.cmake")
 
 # Provide f3d_INCLUDE_DIR
-set_and_check(f3d_INCLUDE_DIR "${PACKAGE_PREFIX_DIR}/include")
+set_and_check(f3d_INCLUDE_DIR "${f3d_PREFIX_DIR}/include")
 
 # Set the required variable
 message(STATUS "Found f3d library component")
