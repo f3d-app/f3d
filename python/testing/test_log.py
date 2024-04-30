1acb4c129074c25802ac22db32263232cb55d6cc
snoyer <noyer.stephane@gmail.com>
2024-04-30 17:11:46 +0400

    make tests cross-platform

--- python/testing/test_log.py
+++ python/testing/test_log.py
@@ -2,14 +2,15 @@ import subprocess
 import sys
 
 
-def test_default():
+def test_default_level():
     assert (
         run_python(
             "from f3d import Log",
+            "Log.set_use_coloring(False)",
             "Log.print(Log.DEBUG, 'debug')",
             "Log.print(Log.INFO, 'info')",
         )
-        == "info\x1b[0m\n"
+        == "info\n"
     )
 
 
@@ -17,21 +18,22 @@ def test_debug():
     assert (
         run_python(
             "from f3d import Log",
+            "Log.set_use_coloring(False)",
             "Log.set_verbose_level(Log.DEBUG)",
             "Log.print(Log.DEBUG, 'debug')",
         )
-        == "debug\x1b[0m\n"
+        == "debug\n"
     )
 
 
-def test_no_coloring():
+def test_coloring():
     assert (
         run_python(
             "from f3d import Log",
-            "Log.set_use_coloring(False)",
+            "Log.set_use_coloring(True)",
             "Log.print(Log.INFO, 'info')",
         )
-        == "info\n"
+        == "info\x1b[0m\n"
     )
 
 
