a35b4c652aff98f164b0e51c3da5972a7f1f7cc4
snoyer <noyer.stephane@gmail.com>
2024-05-25 15:01:35 +0400

    handle multiple configs

--- application/F3DOptionsParser.cxx
+++ application/F3DOptionsParser.cxx
@@ -65,34 +65,13 @@ public:
 protected:
   bool GetOptionConfig(const std::string& option, std::string& configValue) const
   {
-    bool ret = false;
-    if (this->FilePathForConfigBlock.empty())
+    auto localIt = this->CurrentFileConfig.find(option);
+    if (localIt != this->CurrentFileConfig.end())
     {
-      auto localIt = this->GlobalConfigEntry.find(option);
-      if (localIt != this->GlobalConfigEntry.end())
-      {
-        configValue = localIt->second;
-        ret = true;
-      }
+      configValue = localIt->second;
+      return true;
     }
-    else
-    {
-      for (auto const& it : this->RegexConfigEntries)
-      {
-        std::regex re(it.first, std::regex_constants::icase);
-        std::smatch matches;
-        if (std::regex_match(this->FilePathForConfigBlock, matches, re))
-        {
-          auto localIt = it.second.find(option);
-          if (localIt != it.second.end())
-          {
-            configValue = localIt->second;
-            ret = true;
-          }
-        }
-      }
-    }
-    return ret;
+    return false;
   }
 
   template<class T>
@@ -202,14 +181,16 @@ private:
   int Argc;
   char** Argv;
 
-  std::string FilePathForConfigBlock;
-
-  using ConfigEntry = std::map<std::string, std::string>;
-  using ConfigEntries = std::vector<std::pair<std::string, ConfigEntry> >;
-  ConfigEntry GlobalConfigEntry;
+  using ConfigDict = std::map<std::string, std::string>;
+  using ConfigDiff = std::map<std::string, std::optional<std::string> >;
+  using ConfigEntry = std::tuple<ConfigDiff, fs::path, std::string>;
+  using ConfigEntries = std::vector<ConfigEntry>;
+  ConfigEntries GlobalConfigEntries;
   ConfigEntries RegexConfigEntries;
   std::string ExecutableName;
   std::vector<std::string> AllLongOptions;
+
+  ConfigDict CurrentFileConfig;
 };
 
 //----------------------------------------------------------------------------
@@ -279,7 +260,67 @@ void ConfigurationOptions::GetOptions(F3DAppOptions& appOptions, f3d::options& o
 {
   inputs.clear(); /* needed because this function is called multiple times */
 
-  this->FilePathForConfigBlock = std::move(filePathForConfigBlock);
+  /* start with an empty config ... */
+  ConfigDict tmpConfig;
+
+  const auto update = [&](const ConfigDiff& config)
+  {
+    for (const auto& [key, maybeValue] : config)
+    {
+      const std::string del = tmpConfig.count(key) ? key + ": " + tmpConfig[key]
+        : !maybeValue.has_value()                  ? key
+                                                   : "";
+      const std::string add = maybeValue.has_value() ? key + ": " + maybeValue.value() : "";
+      if (add == del && !add.empty())
+      {
+        f3d::log::debug("= ", add);
+      }
+      else
+      {
+        if (!del.empty())
+        {
+          f3d::log::debug("- ", del);
+        }
+        if (!add.empty())
+        {
+          f3d::log::debug("+ ", add);
+        }
+      }
+
+      if (maybeValue.has_value())
+      {
+        tmpConfig[key] = maybeValue.value();
+      }
+      else
+      {
+        tmpConfig.erase(key);
+      }
+    }
+  };
+
+  /* apply global entries */
+  for (const auto& [conf, source, pattern] : this->GlobalConfigEntries)
+  {
+    f3d::log::debug("using `", pattern, "` config from ", source.string());
+    update(conf);
+  }
+
+  /* ... then go through matching regex configs and override */
+  if (!filePathForConfigBlock.empty())
+  {
+    for (auto const& [conf, source, pattern] : this->RegexConfigEntries)
+    {
+      std::regex re(pattern, std::regex_constants::icase);
+      std::smatch matches;
+      if (std::regex_match(filePathForConfigBlock, matches, re))
+      {
+        f3d::log::debug("using `", pattern, "` config from ", source.string());
+        update(conf);
+      }
+    }
+
+    this->CurrentFileConfig = tmpConfig;
+  }
 
   // When parsing multiple times, hasDefault should be forced to yes after the first pass as all
   // options are expected to be already initialized, which means they have a "default" in the
@@ -675,6 +716,7 @@ void ConfigurationOptions::PrintReadersList()
 //----------------------------------------------------------------------------
 bool ConfigurationOptions::InitializeDictionaryFromConfigFile(const std::string& config)
 {
+  this->GlobalConfigEntries.clear();
   this->RegexConfigEntries.clear();
 
   std::string configSearch = "config";
@@ -693,49 +735,49 @@ bool ConfigurationOptions::InitializeDictionaryFromConfigFile(const std::string&
     }
   }
 
-  fs::path configPath;
+  std::vector<fs::path> actualConfigFilePaths;
+
+  std::vector<fs::path> configPaths;
+
   if (!configSearch.empty())
   {
-    configPath = F3DConfigFileTools::GetConfigPath(configSearch);
-  }
-  else
-  {
-    configPath = config;
-  }
-
-  if (configPath.empty())
-  {
-    if (!config.empty())
+    for (const auto& path : F3DConfigFileTools::GetConfigPaths(configSearch))
     {
-      f3d::log::error("Configuration file for \"", config, "\" could not been found");
-    }
-    return false;
-  }
-
-  // Recover an absolute canonical path to config file
-  try
-  {
-    configPath = fs::canonical(fs::path(configPath)).string();
-  }
-  catch (const fs::filesystem_error&)
-  {
-    f3d::log::error("Configuration file does not exist: ", configPath.string());
-    return false;
-  }
-  f3d::log::debug("Using config file ", configPath.string());
-
-  // Recover all config files if needed
-  std::set<fs::path> actualConfigFilePaths;
-  if (fs::is_directory(configPath))
-  {
-    for (auto& entry : std::filesystem::directory_iterator(configPath))
-    {
-      actualConfigFilePaths.emplace(entry);
+      configPaths.emplace_back(path);
     }
   }
   else
   {
-    actualConfigFilePaths.emplace(configPath);
+    configPaths.emplace_back(config);
+  }
+
+  for (auto configPath : configPaths)
+  {
+    // Recover an absolute canonical path to config file
+    try
+    {
+      configPath = fs::canonical(fs::path(configPath)).string();
+    }
+    catch (const fs::filesystem_error&)
+    {
+      f3d::log::error("Configuration file does not exist: ", configPath.string());
+      return false;
+    }
+
+    // Recover all config files if needed
+    if (fs::is_directory(configPath))
+    {
+      f3d::log::debug("Using config directory ", configPath.string());
+      for (auto& entry : std::filesystem::directory_iterator(configPath))
+      {
+        actualConfigFilePaths.emplace_back(entry);
+      }
+    }
+    else
+    {
+      f3d::log::debug("Using config file ", configPath.string());
+      actualConfigFilePaths.emplace_back(configPath);
+    }
   }
 
   // Read config files
@@ -762,32 +804,36 @@ bool ConfigurationOptions::InitializeDictionaryFromConfigFile(const std::string&
       return false;
     }
 
-    for (const auto& regexpConfig : json.items())
+    for (const auto& configBlock : json.items())
     {
-      std::map<std::string, std::string> localDic;
-      for (const auto& prop : regexpConfig.value().items())
+      ConfigDiff config;
+      for (const auto& item : configBlock.value().items())
       {
-        if (prop.value().is_number() || prop.value().is_boolean())
+        if (item.value().is_null())
         {
-          localDic[prop.key()] = ToString(prop.value());
+          config[item.key()] = std::nullopt;
         }
-        else if (prop.value().is_string())
+        else if (item.value().is_number() || item.value().is_boolean())
         {
-          localDic[prop.key()] = prop.value().get<std::string>();
+          config[item.key()] = ToString(item.value());
+        }
+        else if (item.value().is_string())
+        {
+          config[item.key()] = item.value().get<std::string>();
         }
         else
         {
-          f3d::log::error(prop.key(), " must be a string, a boolean or a number");
+          f3d::log::error(item.key(), " must be a string, a boolean, a number, or null");
           return false;
         }
       }
-      if (regexpConfig.key() == "global")
+      if (configBlock.key() == "global")
       {
-        this->GlobalConfigEntry = localDic;
+        this->GlobalConfigEntries.emplace_back(config, configFilePath, configBlock.key());
       }
       else
       {
-        this->RegexConfigEntries.emplace_back(regexpConfig.key(), localDic);
+        this->RegexConfigEntries.emplace_back(config, configFilePath, configBlock.key());
       }
     }
   }
