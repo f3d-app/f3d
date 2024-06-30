f55b044d3a866f7ce37c0055e53016d70a0f95b2
snoyer <noyer.stephane@gmail.com>
2024-06-30 17:53:55 +0400

    clamp animation time to available range

--- library/src/animationManager.cxx
+++ library/src/animationManager.cxx
@@ -251,12 +251,21 @@ bool animationManager::LoadAtTime(double timeValue)
   {
     return false;
   }
-  if (timeValue < this->TimeRange[0] || timeValue > this->TimeRange[1])
+
+  /* clamp target time to available range */
+  if (timeValue < this->TimeRange[0])
   {
-    log::warn("Provided time value: ", timeValue, " is outside of animation time range: [",
-      this->TimeRange[0], ", ", this->TimeRange[1], "] .");
-    return false;
+    log::warn("animation time ", timeValue, " is outside of range [", this->TimeRange[0], ", ",
+      this->TimeRange[1], "], using ", this->TimeRange[0], ".");
+    timeValue = this->TimeRange[0];
   }
+  else if (timeValue > this->TimeRange[1])
+  {
+    log::warn("animation time ", timeValue, " is outside of range [", this->TimeRange[0], ", ",
+      this->TimeRange[1], "], using ", this->TimeRange[1], ".");
+    timeValue = this->TimeRange[1];
+  }
+
   this->CurrentTime = timeValue;
   this->CurrentTimeSet = true;
   this->Importer->UpdateTimeStep(this->CurrentTime);
