2288d463e21ea0947efa41cc53bd2268bdf1d805
Mathieu Westphal <mathieu.westphal@kitware.com>
2024-08-01 14:09:32 +0200

    clf

--- library/public/types.h
+++ library/public/types.h
@@ -82,9 +82,16 @@ struct mesh_t
 };
 }
 
-enum class option_types{ _bool, _int, _double, _ratio, _string, _double_vector};
+enum class option_types
+{
+  _bool,
+  _int,
+  _double,
+  _ratio,
+  _string,
+  _double_vector
+};
 
-using option_variant_t =
-  std::variant<bool, int, double, std::string, std::vector<double>>;
+using option_variant_t = std::variant<bool, int, double, std::string, std::vector<double>>;
 
 #endif
