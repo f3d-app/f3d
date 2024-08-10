3c04aac776ed298fdf3c422171dbe66b4350e6ac
Mathieu Westphal <mathieu.westphal@kitware.com>
2024-08-10 22:30:20 +0200

    fixup cxx17

--- library/public/types.h
+++ library/public/types.h
@@ -91,9 +91,4 @@ enum class option_types
   _double_vector
 };
 
-#ifdef F3D_ENABLE_CXX17_API
-#include <variant>
-using option_variant_t = std::variant<bool, int, double, std::string, std::vector<double>>;
-#endif
-
 #endif
