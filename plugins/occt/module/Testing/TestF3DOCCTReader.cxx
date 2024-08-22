dc9d50d08a92f5dcd28bf2792ccd9d5ef9f10923
snoyer <noyer.stephane@gmail.com>
2024-08-22 17:14:12 +0100

    style

--- plugins/occt/module/Testing/TestF3DOCCTReader.cxx
+++ plugins/occt/module/Testing/TestF3DOCCTReader.cxx
@@ -22,10 +22,11 @@ bool testReader(const std::string& filename, const vtkF3DOCCTReader::FILE_FORMAT
 
 int TestF3DOCCTReader(int vtkNotUsed(argc), char* argv[])
 {
-  return (testReader(std::string(argv[1]) + "data/f3d.stp", vtkF3DOCCTReader::FILE_FORMAT::STEP) &&
-           testReader(std::string(argv[1]) + "data/f3d.igs", vtkF3DOCCTReader::FILE_FORMAT::IGES) &&
-           testReader(std::string(argv[1]) + "data/f3d.brep", vtkF3DOCCTReader::FILE_FORMAT::BREP)&&
-           testReader(std::string(argv[1]) + "data/f3d.xbf", vtkF3DOCCTReader::FILE_FORMAT::XBF))
+  const std::string data = std::string(argv[1]) + "data";
+  return (testReader(data + "/f3d.stp", vtkF3DOCCTReader::FILE_FORMAT::STEP) &&
+           testReader(data + "/f3d.igs", vtkF3DOCCTReader::FILE_FORMAT::IGES) &&
+           testReader(data + "/f3d.brep", vtkF3DOCCTReader::FILE_FORMAT::BREP) &&
+           testReader(data + "/f3d.xbf", vtkF3DOCCTReader::FILE_FORMAT::XBF))
     ? EXIT_SUCCESS
     : EXIT_FAILURE;
 }
