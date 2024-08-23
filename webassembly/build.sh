bb18f0478c09fec39b33254d14507347ba74871f
Michael MIGLIORE <mcmigliore@gmail.com>
2024-08-23 12:33:42 +0200

    Add assimp plugin to wasm build and use npm in CI (#1582)

--- webassembly/build.sh
+++ webassembly/build.sh
@@ -3,10 +3,11 @@
 cmake -S /src -B /build \
     -DBUILD_SHARED_LIBS=OFF \
     -DF3D_STRICT_BUILD=ON \
+    -DF3D_PLUGIN_BUILD_ASSIMP=ON \
     -DF3D_PLUGIN_BUILD_EXODUS=OFF \
     -DF3D_WASM_COPY_APP=ON \
     -DCMAKE_BUILD_TYPE=$1 \
-    -DVTK_DIR:PATH=/depends/lib/cmake/vtk
+    -DCMAKE_FIND_ROOT_PATH:PATH=/depends/
 
 cmake --build /build
 
