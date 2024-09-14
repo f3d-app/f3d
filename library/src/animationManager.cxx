4fae334b01606fd5a1dc34339174785e8d08ea4a
Mathieu Westphal <mathieu.westphal@kitware.com>
2024-09-14 22:13:09 +0200

    last fixups

--- library/src/animationManager.cxx
+++ library/src/animationManager.cxx
@@ -182,7 +182,7 @@ void animationManager::ToggleAnimation()
         this->Interactor->createTimerCallBack(1000.0 / frameRate, [this]() { this->Tick(); });
     }
 
-    if (this->Playing && this->Options->scene.camera.index >= 0)
+    if (this->Playing && this->Options->scene.camera.index.has_value())
     {
       this->Interactor->disableCameraMovement();
     }
