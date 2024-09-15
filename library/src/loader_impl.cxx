7f3f34021954990d60078b61403ba19ebe1e6eeb
Mathieu Westphal <mathieu.westphal@kitware.com>
2024-09-15 07:57:22 +0200

    clf

--- library/src/loader_impl.cxx
+++ library/src/loader_impl.cxx
@@ -290,7 +290,8 @@ loader& loader_impl::loadScene(const std::string& filePath)
 
   if (this->Internals->Options.scene.camera.index.has_value())
   {
-    this->Internals->CurrentFullSceneImporter->SetCamera(this->Internals->Options.scene.camera.index.value());
+    this->Internals->CurrentFullSceneImporter->SetCamera(
+      this->Internals->Options.scene.camera.index.value());
   }
 
   log::debug("Loading 3D scene: ", filePath, "\n");
