f08eeea414d910db0bb568eb926e67f026620948
Schwarzemann <barantbsd@gmail.com>
2024-10-06 17:18:46 +0300

    FIX: Add getters for camera parameters #1419

--- java/F3DJavaBindings.cxx
+++ java/F3DJavaBindings.cxx
@@ -136,17 +136,17 @@ extern "C"
     GetEngine(env, self)->getWindow().getCamera().roll(angle);
   }
 
-  JNIEXPORT void JAVA_BIND(Camera, AddAzimuth)(JNIEnv* env, jobject self, jdouble angle)
+  JNIEXPORT void JAVA_BIND(Camera, addAzimuth)(JNIEnv* env, jobject self, jdouble angle)
   {
     GetEngine(env, self)->getWindow().getCamera().addAzimuth(angle);
   }
 
-  JNIEXPORT void JAVA_BIND(Camera, AddYaw)(JNIEnv* env, jobject self, jdouble angle)
+  JNIEXPORT void JAVA_BIND(Camera, addYaw)(JNIEnv* env, jobject self, jdouble angle)
   {
     GetEngine(env, self)->getWindow().getCamera().addYaw(angle);
   }
 
-  JNIEXPORT void JAVA_BIND(Camera, AddElevation)(JNIEnv* env, jobject self, jdouble angle)
+  JNIEXPORT void JAVA_BIND(Camera, addElevation)(JNIEnv* env, jobject self, jdouble angle)
   {
     GetEngine(env, self)->getWindow().getCamera().addElevation(angle);
   }
