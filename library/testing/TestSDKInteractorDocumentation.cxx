fc9890a2d6e3b279f51140dd64452a4f5219884e
Mathieu Westphal <mathieu.westphal@kitware.com>
2024-11-23 11:54:24 +0100

    fixup test

--- library/testing/TestSDKInteractorDocumentation.cxx
+++ library/testing/TestSDKInteractorDocumentation.cxx
@@ -7,9 +7,9 @@
 using mod_t = f3d::interaction_bind_t::ModifierKeys;
 
 constexpr int nGroup = 3;
-constexpr int nBindingsScene = 26;
-constexpr std::string_view initDoc = "Toggle trackball interaction";
-constexpr std::string_view initVal = "OFF";
+constexpr int nBindingsCamera = 7;
+constexpr std::string_view initDoc = "Toggle Orthographic Projection";
+constexpr std::string_view initVal = "N/A";
 
 int TestSDKInteractorDocumentation(int argc, char* argv[])
 {
@@ -22,15 +22,15 @@ int TestSDKInteractorDocumentation(int argc, char* argv[])
   {
     // Test initial state
     test("Initial group size", inter.getBindGroups().size() == nGroup);
-    test("Initial nBindings Scene", inter.getBindsForGroup("Scene").size() == nBindingsScene);
-    const auto& [doc, val] = inter.getBindingDocumentation({ mod_t::NONE, "K" });
+    test("Initial nBindings Camera", inter.getBindsForGroup("Camera").size() == nBindingsCamera);
+    const auto& [doc, val] = inter.getBindingDocumentation({ mod_t::ANY, "5" });
     test("Initial doc and val", doc == initDoc && val == initVal);
   }
 
   {
     // Test invalid args
     test("Initial invalid group", inter.getBindsForGroup("Invalid").size() == 0);
-    const auto& [doc, val] = inter.getBindingDocumentation({ mod_t::NONE, "Invalid" });
+    const auto& [doc, val] = inter.getBindingDocumentation({ mod_t::ANY, "Invalid" });
     test("Initial invalid doc and val", doc == "" && val == "");
   }
 
@@ -46,21 +46,32 @@ int TestSDKInteractorDocumentation(int argc, char* argv[])
   {
     // Test empty state
     test("Empty group size", inter.getBindGroups().size() == 0);
-    test("Empty nBindings Scene", inter.getBindsForGroup("Scene").size() == 0);
-    const auto& [doc, val] = inter.getBindingDocumentation({ mod_t::NONE, "K" });
+    test("Empty nBindings Camera", inter.getBindsForGroup("Camera").size() == 0);
+    const auto& [doc, val] = inter.getBindingDocumentation({ mod_t::ANY, "5" });
     test("Empty doc and val", doc == "" && val == "");
   }
 
   // Add a dummy binding
-  inter.addBinding({ mod_t::NONE, "DummyBind" }, "DummyCommand", "DummyGroup",
+  inter.addBinding({ mod_t::ANY, "DummyBind" }, "DummyCommand", "DummyGroup",
     []() -> std::pair<std::string, std::string> { return std::pair("DummyDoc", "DummyVal"); });
 
   {
     // Test dummy binding
     test("Dummy group size", inter.getBindGroups().size() == 1);
     test("Dummy nBindings DummyGroup", inter.getBindsForGroup("DummyGroup").size() == 1);
-    const auto& [doc, val] = inter.getBindingDocumentation({ mod_t::NONE, "DummyBind" });
+    const auto& [doc, val] = inter.getBindingDocumentation({ mod_t::ANY, "DummyBind" });
     test("Dummy doc and val", doc == "DummyDoc" && val == "DummyVal");
+  }
+
+  // Initialize two times
+  inter.initBindings();
+  inter.initBindings();
+  {
+    // Test initial state
+    test("Initial group size after init", inter.getBindGroups().size() == nGroup);
+    test("Initial nBindings Camera after init", inter.getBindsForGroup("Camera").size() == nBindingsCamera);
+    const auto& [doc, val] = inter.getBindingDocumentation({ mod_t::ANY, "5" });
+    test("Initial doc and val after init", doc == initDoc && val == initVal);
   }
 
   return test.result();
