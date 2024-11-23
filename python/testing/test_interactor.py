a921fab92c7bbff0443e2d15c62e255eb614889b
Mathieu Westphal <mathieu.westphal@kitware.com>
2024-11-23 12:01:28 +0100

    black

--- python/testing/test_interactor.py
+++ python/testing/test_interactor.py
@@ -41,17 +41,38 @@ def test_binding():
     # Check default interactions can be removed
     groups = inter.get_bind_groups()
     for group in groups:
-      binds = inter.get_binds_for_group(group)
-      for bind in binds:
-        inter.remove_binding(bind)
+        binds = inter.get_binds_for_group(group)
+        for bind in binds:
+            inter.remove_binding(bind)
     assert len(inter.get_bind_groups()) == 0
 
     # Smoke test
-    inter.add_binding(f3d.InteractionBind(f3d.InteractionBind.ModifierKeys.ANY, "P"), "dummy command", "DummyGroup", doc_fn)
-    inter.add_binding(f3d.InteractionBind(f3d.InteractionBind.ModifierKeys.NONE, "P"), "dummy command", "DummyGroup", doc_fn)
-    inter.add_binding(f3d.InteractionBind(f3d.InteractionBind.ModifierKeys.CTRL, "P"), "dummy command", "DummyGroup", doc_fn)
-    inter.add_binding(f3d.InteractionBind(f3d.InteractionBind.ModifierKeys.SHIFT, "P"), "dummy command", "DummyGroup", doc_fn)
-    inter.add_binding(f3d.InteractionBind(f3d.InteractionBind.ModifierKeys.CTRL_SHIFT, "P"),
+    inter.add_binding(
+        f3d.InteractionBind(f3d.InteractionBind.ModifierKeys.ANY, "P"),
+        "dummy command",
+        "DummyGroup",
+        doc_fn,
+    )
+    inter.add_binding(
+        f3d.InteractionBind(f3d.InteractionBind.ModifierKeys.NONE, "P"),
+        "dummy command",
+        "DummyGroup",
+        doc_fn,
+    )
+    inter.add_binding(
+        f3d.InteractionBind(f3d.InteractionBind.ModifierKeys.CTRL, "P"),
+        "dummy command",
+        "DummyGroup",
+        doc_fn,
+    )
+    inter.add_binding(
+        f3d.InteractionBind(f3d.InteractionBind.ModifierKeys.SHIFT, "P"),
+        "dummy command",
+        "DummyGroup",
+        doc_fn,
+    )
+    inter.add_binding(
+        f3d.InteractionBind(f3d.InteractionBind.ModifierKeys.CTRL_SHIFT, "P"),
         ["dummy command", "dummy command"],
         "DummyGroup",
         doc_fn,
