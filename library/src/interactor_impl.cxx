fd5b70f8ab44a4f8c3c02321f599ea1cfaeea432
Mathieu Westphal <mathieu.westphal@kitware.com>
2024-11-23 11:07:12 +0100

    Revert "fixup std::bind"
    This reverts commit 296e5ff46dd673dd2fcdad8182d4e3d824dec31b.

--- library/src/interactor_impl.cxx
+++ library/src/interactor_impl.cxx
@@ -810,71 +810,70 @@ interactor& interactor_impl::initBindings()
   };
 
   // Available standard keys: None
-  this->addBinding({mod_t::NONE, "W"}, "cycle_animation", "Scene", [=]() { return docStrVal("Cycle animation", animStr); });
-
-  this->addBinding({mod_t::NONE, "C"}, "cycle_coloring field", "Scene", [=]() { return docStrVal("Cycle point/cell data coloring", pointCellStr); });
-  this->addBinding({mod_t::NONE, "S"}, "cycle_coloring array", "Scene", [=]() { return docStrVal("Cycle array to color with", arrayStr); });
-  this->addBinding({mod_t::NONE, "Y"}, "cycle_coloring component", "Scene", [=]() { return docStrVal("Cycle component to color with", compStr); });
-  this->addBinding({mod_t::NONE, "B"}, "toggle ui.scalar_bar", "Scene", [=]() { return docStrVal("Toggle the scalar bar display", [&](){ return docTgl(opts.ui.scalar_bar);});});
-
-  this->addBinding({mod_t::NONE, "P"}, "toggle render.effect.translucency_support", "Scene", [=]() { return docStrVal("Toggle scalar bar display",
-    [&]() -> std::string { return docTgl(opts.render.effect.translucency_support);}); });
-  this->addBinding({mod_t::NONE, "Q"}, "toggle render.effect.ambient_occlusion","Scene", [=]() { return docStrVal("Toggle ambient occlusion",
-    [&]() -> std::string { return docTgl(opts.render.effect.ambient_occlusion);}); });
-  this->addBinding({mod_t::NONE, "A"}, "toggle render.effect.anti_aliasing","Scene", [=]() { return docStrVal("Toggle anti-aliasing",
-    [&]() -> std::string { return docTgl(opts.render.effect.anti_aliasing);}); });
-  this->addBinding({mod_t::NONE, "T"}, "toggle render.effect.tone_mapping","Scene", [=]() { return docStrVal("Toggle tone mapping",
-    [&]() -> std::string { return docTgl(opts.render.effect.tone_mapping);}); });
-  this->addBinding({mod_t::NONE, "E"}, "toggle render.show_edges","Scene", [=]() { return docStrVal("Toggle edges display",
-    [&]() -> std::string { return docTglOpt(opts.render.show_edges);}); });
-  this->addBinding({mod_t::NONE, "X"}, "toggle interactor.axis","Scene", [=]() { return docStrVal("Toggle axes display",
-    [&]() -> std::string { return docTgl(opts.interactor.axis);}); });
-  this->addBinding({mod_t::NONE, "G"}, "toggle render.grid.enable","Scene", [=]() { return docStrVal("Toggle grid display",
-    [&]() -> std::string { return docTgl(opts.render.grid.enable);}); });
-  this->addBinding({mod_t::NONE, "N"}, "toggle ui.filename","Scene", [=]() { return docStrVal("Toggle filename display",
-    [&]() -> std::string { return docTgl(opts.ui.filename);}); });
-  this->addBinding({mod_t::NONE, "M"}, "toggle ui.metadata","Scene", [=]() { return docStrVal("Toggle metadata display",
-    [&]() -> std::string { return docTgl(opts.ui.metadata);}); });
-  this->addBinding({mod_t::NONE, "Z"}, "toggle_fps","Scene", [=]() { return docStrVal("Toggle FPS counter display",
-    [&]() -> std::string { return docTgl(opts.ui.fps);}); });
+  this->addBinding({mod_t::NONE, "W"}, "cycle_animation", "Scene", std::bind(docStrVal, "Cycle animation", animStr));
+  this->addBinding({mod_t::NONE, "C"}, "cycle_coloring field", "Scene", std::bind(docStrVal, "Cycle point/cell data coloring", pointCellStr));
+  this->addBinding({mod_t::NONE, "S"}, "cycle_coloring array", "Scene", std::bind(docStrVal, "Cycle array to color with", arrayStr));
+  this->addBinding({mod_t::NONE, "Y"}, "cycle_coloring component", "Scene", std::bind(docStrVal, "Cycle component to color with", compStr));
+  this->addBinding({mod_t::NONE, "B"}, "toggle ui.scalar_bar", "Scene", std::bind(docStrVal, "Toggle the scalar bar display",
+    [&]() -> std::string { return docTgl(opts.ui.scalar_bar);}));
+  this->addBinding({mod_t::NONE, "P"}, "toggle render.effect.translucency_support", "Scene", std::bind(docStrVal, "Toggle scalar bar display",
+    [&]() -> std::string { return docTgl(opts.render.effect.translucency_support);}));
+  this->addBinding({mod_t::NONE, "Q"}, "toggle render.effect.ambient_occlusion","Scene", std::bind(docStrVal, "Toggle ambient occlusion",
+    [&]() -> std::string { return docTgl(opts.render.effect.ambient_occlusion);}));
+  this->addBinding({mod_t::NONE, "A"}, "toggle render.effect.anti_aliasing","Scene", std::bind(docStrVal, "Toggle anti-aliasing",
+    [&]() -> std::string { return docTgl(opts.render.effect.anti_aliasing);}));
+  this->addBinding({mod_t::NONE, "T"}, "toggle render.effect.tone_mapping","Scene", std::bind(docStrVal, "Toggle tone mapping",
+    [&]() -> std::string { return docTgl(opts.render.effect.tone_mapping);}));
+  this->addBinding({mod_t::NONE, "E"}, "toggle render.show_edges","Scene", std::bind(docStrVal, "Toggle edges display",
+    [&]() -> std::string { return docTglOpt(opts.render.show_edges);}));
+  this->addBinding({mod_t::NONE, "X"}, "toggle interactor.axis","Scene", std::bind(docStrVal, "Toggle axes display",
+    [&]() -> std::string { return docTgl(opts.interactor.axis);}));
+  this->addBinding({mod_t::NONE, "G"}, "toggle render.grid.enable","Scene", std::bind(docStrVal, "Toggle grid display",
+    [&]() -> std::string { return docTgl(opts.render.grid.enable);}));
+  this->addBinding({mod_t::NONE, "N"}, "toggle ui.filename","Scene", std::bind(docStrVal, "Toggle filename display",
+    [&]() -> std::string { return docTgl(opts.ui.filename);}));
+  this->addBinding({mod_t::NONE, "M"}, "toggle ui.metadata","Scene", std::bind(docStrVal, "Toggle metadata display",
+    [&]() -> std::string { return docTgl(opts.ui.metadata);}));
+  this->addBinding({mod_t::NONE, "Z"}, "toggle_fps","Scene", std::bind(docStrVal, "Toggle FPS counter display",
+    [&]() -> std::string { return docTgl(opts.ui.fps);}));
 #if F3D_MODULE_RAYTRACING
-  this->addBinding({mod_t::NONE, "R"}, "toggle render.raytracing.enable","Scene", [=]() { return docStrVal("Toggle raytracing rendering",
-    [&]() -> std::string { return docTgl(opts.render.raytracing.enable);}); });
-  this->addBinding({mod_t::NONE, "D"}, "toggle render.raytracing.denoise","Scene", [=]() { return docStrVal("Toggle denoising when raytracing",
-    [&]() -> std::string { return docTgl(opts.render.raytracing.denoise);}); });
+  this->addBinding({mod_t::NONE, "R"}, "toggle render.raytracing.enable","Scene", std::bind(docStrVal, "Toggle raytracing rendering",
+    [&]() -> std::string { return docTgl(opts.render.raytracing.enable);}));
+  this->addBinding({mod_t::NONE, "D"}, "toggle render.raytracing.denoise","Scene", std::bind(docStrVal, "Toggle denoising when raytracing",
+    [&]() -> std::string { return docTgl(opts.render.raytracing.denoise);}));
 #endif
-  this->addBinding({mod_t::NONE, "V"}, "toggle_volume_rendering","Scene", [=]() { return docStrVal("Toggle volume rendering",
-    [&]() -> std::string { return docTgl(opts.model.volume.enable);}); });
-  this->addBinding({mod_t::NONE, "I"}, "toggle model.volume.inverse","Scene", [=]() { return docStrVal("Toggle inverse volume opacity",
-    [&]() -> std::string { return docTgl(opts.model.volume.inverse);}); });
-  this->addBinding({mod_t::NONE, "O"}, "toggle model.point_sprites.enable","Scene", [=]() { return docStrVal("Toggle point sprites rendering",
-    [&]() -> std::string { return docTgl(opts.model.point_sprites.enable);}); });
-  this->addBinding({mod_t::NONE, "U"}, "toggle render.background.blur","Scene", [=]() { return docStrVal("Toggle blur background",
-    [&]() -> std::string { return docTgl(opts.render.background.blur);}); });
-  this->addBinding({mod_t::NONE, "K"}, "toggle interactor.trackball","Scene", [=]() { return docStrVal("Toggle trackball interaction",
-    [&]() -> std::string { return docTgl(opts.interactor.trackball);}); });
-  this->addBinding({mod_t::NONE, "F"}, "toggle render.hdri.ambient","Scene", [=]() { return docStrVal("Toggle HDRI ambient lighting",
-    [&]() -> std::string { return docTgl(opts.render.hdri.ambient);}); });
-  this->addBinding({mod_t::NONE, "J"}, "toggle render.background.skybox","Scene", [=]() { return docStrVal("Toggle HDRI skybox",
-    [&]() -> std::string { return docTgl(opts.render.background.skybox);}); });
-  this->addBinding({mod_t::NONE, "L"}, "increase_light_intensity", "Scene", [=]() { return docStrVal("Increase lights intensity",
-    [&]() -> std::string { return docDbl(opts.render.light.intensity, 2);}); });
-  this->addBinding({mod_t::SHIFT, "L"}, "decrease_light_intensity", "Scene", [=]() { return docStrVal("Decrease lights intensity",
-    [&]() -> std::string { return docDbl(opts.render.light.intensity, 2);}); });
-  this->addBinding({mod_t::ANY, "1"}, "set_camera front", "Camera", [=]() { return docStr("Front View camera"); });
-  this->addBinding({mod_t::ANY, "3"}, "set_camera right", "Camera", [=]() { return docStr("Right View camera"); });
-  this->addBinding({mod_t::ANY, "4"}, "roll_camera -90", "Camera", [=]() { return docStr("Rotate camera right"); });
-  this->addBinding({mod_t::ANY, "5"}, "toggle scene.camera.orthographic", "Camera", [=]() { return docStrVal("Toggle Orthographic Projection",
-    [&]() -> std::string { return docTglOpt(opts.scene.camera.orthographic);}); });
-  this->addBinding({mod_t::ANY, "6"}, "roll_camera 90", "Camera", [=]() { return docStr("Rotate camera left"); });
-  this->addBinding({mod_t::ANY, "7"}, "set_camera top", "Camera", [=]() { return docStr("Top View camera"); });
-  this->addBinding({mod_t::ANY, "9"}, "set_camera isometric", "Camera", [=]() { return docStr("Isometric View camera"); });
-  this->addBinding({mod_t::NONE, "H"}, "toggle ui.cheatsheet", "Others", [=]() { return docStr("Toggle cheatsheet display"); });
-  this->addBinding({mod_t::ANY, "Question"}, "print_scene_info", "Others", [=]() { return docStr("Print scene descr to terminal"); });
-  this->addBinding({mod_t::NONE, F3D_EXIT_HOTKEY_SYM}, "stop_interactor", "Others", [=]() { return docStr("Quit"); });
-  this->addBinding({mod_t::NONE, "Return"}, "reset_camera", "Others", [=]() { return docStr("Reset camera to initial parameters"); });
-  this->addBinding({mod_t::NONE, "Space"}, "toggle_animation", "Others", [=]() { return docStr("Play/Pause animation if any"); });
-  this->addBinding({mod_t::NONE, "Drop"}, "add_files", "Others", [=]() { return docStr("Add files to the scene"); });
+  this->addBinding({mod_t::NONE, "V"}, "toggle_volume_rendering","Scene", std::bind(docStrVal, "Toggle volume rendering",
+    [&]() -> std::string { return docTgl(opts.model.volume.enable);}));
+  this->addBinding({mod_t::NONE, "I"}, "toggle model.volume.inverse","Scene", std::bind(docStrVal, "Toggle inverse volume opacity",
+    [&]() -> std::string { return docTgl(opts.model.volume.inverse);}));
+  this->addBinding({mod_t::NONE, "O"}, "toggle model.point_sprites.enable","Scene", std::bind(docStrVal, "Toggle point sprites rendering",
+    [&]() -> std::string { return docTgl(opts.model.point_sprites.enable);}));
+  this->addBinding({mod_t::NONE, "U"}, "toggle render.background.blur","Scene", std::bind(docStrVal, "Toggle blur background",
+    [&]() -> std::string { return docTgl(opts.render.background.blur);}));
+  this->addBinding({mod_t::NONE, "K"}, "toggle interactor.trackball","Scene", std::bind(docStrVal, "Toggle trackball interaction",
+    [&]() -> std::string { return docTgl(opts.interactor.trackball);}));
+  this->addBinding({mod_t::NONE, "F"}, "toggle render.hdri.ambient","Scene", std::bind(docStrVal, "Toggle HDRI ambient lighting",
+    [&]() -> std::string { return docTgl(opts.render.hdri.ambient);}));
+  this->addBinding({mod_t::NONE, "J"}, "toggle render.background.skybox","Scene", std::bind(docStrVal, "Toggle HDRI skybox",
+    [&]() -> std::string { return docTgl(opts.render.background.skybox);}));
+  this->addBinding({mod_t::NONE, "L"}, "increase_light_intensity", "Scene", std::bind(docStrVal, "Increase lights intensity",
+    [&]() -> std::string { return docDbl(opts.render.light.intensity, 2);}));
+  this->addBinding({mod_t::SHIFT, "L"}, "decrease_light_intensity", "Scene", std::bind(docStrVal, "Decrease lights intensity",
+    [&]() -> std::string { return docDbl(opts.render.light.intensity, 2);}));
+  this->addBinding({mod_t::ANY, "1"}, "set_camera front", "Camera", std::bind(docStr, "Front View camera"));
+  this->addBinding({mod_t::ANY, "3"}, "set_camera right", "Camera", std::bind(docStr, "Right View camera"));
+  this->addBinding({mod_t::ANY, "4"}, "roll_camera -90", "Camera", std::bind(docStr, "Rotate camera right"));
+  this->addBinding({mod_t::ANY, "5"}, "toggle scene.camera.orthographic", "Camera", std::bind(docStrVal, "Toggle Orthographic Projection",
+    [&]() -> std::string { return docTglOpt(opts.scene.camera.orthographic);}));
+  this->addBinding({mod_t::ANY, "6"}, "roll_camera 90", "Camera", std::bind(docStr, "Rotate camera left"));
+  this->addBinding({mod_t::ANY, "7"}, "set_camera top", "Camera", std::bind(docStr, "Top View camera"));
+  this->addBinding({mod_t::ANY, "9"}, "set_camera isometric", "Camera", std::bind(docStr, "Isometric View camera"));
+  this->addBinding({mod_t::NONE, "H"}, "toggle ui.cheatsheet", "Others", std::bind(docStr, "Toggle cheatsheet display"));
+  this->addBinding({mod_t::ANY, "Question"}, "print_scene_info", "Others", std::bind(docStr, "Print scene descr to terminal"));
+  this->addBinding({mod_t::NONE, F3D_EXIT_HOTKEY_SYM}, "stop_interactor", "Others", std::bind(docStr, "Quit"));
+  this->addBinding({mod_t::NONE, "Return"}, "reset_camera", "Others", std::bind(docStr, "Reset camera to initial parameters"));
+  this->addBinding({mod_t::NONE, "Space"}, "toggle_animation", "Others", std::bind(docStr, "Play/Pause animation if any"));
+  this->addBinding({mod_t::NONE, "Drop"}, "add_files", "Others", std::bind(docStr, "Add files to the scene"));
   // clang-format on
 
   return *this;
