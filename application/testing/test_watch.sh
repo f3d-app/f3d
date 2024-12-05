62819e72f29db92f44a6f19d3f8cdec92d71d93e
Michael Migliore <mcmigliore@gmail.com>
2024-12-05 08:51:44 +0100

    try sigterm...

--- application/testing/test_watch.sh
+++ application/testing/test_watch.sh
@@ -21,13 +21,7 @@ pid=$!
 
 function cleanup()
 {
-  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    wmctrl -c :ACTIVE:
-  elif [[ "$OSTYPE" == "darwin"* ]]; then
-    osascript -e 'quit app "f3d"'
-  else
-    exit 1 # not supported
-  fi
+  kill -SIGTERM $pid
 }
 trap "cleanup" EXIT
 
