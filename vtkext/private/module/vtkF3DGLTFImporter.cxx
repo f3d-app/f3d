0aebd1407de04c39a201e23bb728f51a4bfda503
Michael Migliore <mcmigliore@gmail.com>
2024-12-17 07:45:54 +0100

    fix

--- vtkext/private/module/vtkF3DGLTFImporter.cxx
+++ vtkext/private/module/vtkF3DGLTFImporter.cxx
@@ -8,10 +8,14 @@
 vtkStandardNewMacro(vtkF3DGLTFImporter);
 
 //----------------------------------------------------------------------------
+#if VTK_VERSION_NUMBER >= VTK_VERSION_CHECK(9, 4, 20241220) // To update when merged
 vtkF3DGLTFImporter::vtkF3DGLTFImporter()
 {
   this->ImportArmature = true;
 }
+#else
+vtkF3DGLTFImporter::vtkF3DGLTFImporter() = default;
+#endif
 
 //----------------------------------------------------------------------------
 #if VTK_VERSION_NUMBER >= VTK_VERSION_CHECK(9, 4, 20241220) // To update when merged
@@ -23,7 +27,7 @@ void vtkF3DGLTFImporter::ApplyArmatureProperties(vtkActor* actor)
   vtkOpenGLShaderProperty* sp = vtkOpenGLShaderProperty::SafeDownCast(actor->GetShaderProperty());
   if (sp)
   {
-    // compress the depth value to the first thousand slice in front of the camera to make sure it's displayed on top
+    // compress the depth value to the first thousandth slice in front of the camera to make sure it's displayed on top
     sp->AddFragmentShaderReplacement("//VTK::Depth::Impl", false, "gl_FragDepth = gl_FragDepth * 0.001;\n", false);
   }
 }
