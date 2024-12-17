6fb2e973f93533db414960222f12e045d7f42f1b
Youva Boutora <48061964+Youva@users.noreply.github.com>
2024-12-17 11:48:49 -0500

    Added unit test

---
+++ plugins/native/module/vtkF3DQuakeMDLImporterConstants.h
@@ -0,0 +1,7 @@
+#ifndef vtkF3DQuakeMDLImporterConstants_h
+#define vtkF3DQuakeMDLImporterConstants_h
+
+extern const unsigned char F3DMDLDefaultColorMap[256][3];
+extern const float F3DMDLNormalVectors[162][3];
+
+#endif
