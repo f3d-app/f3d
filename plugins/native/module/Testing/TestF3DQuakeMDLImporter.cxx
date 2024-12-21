db137173cf19faa8f991652568b9f76e10dbef24
Youva Boutora <48061964+Youva@users.noreply.github.com>
2024-12-21 11:00:33 -0500

    Added asset file for coverage

--- plugins/native/module/Testing/TestF3DQuakeMDLImporter.cxx
+++ plugins/native/module/Testing/TestF3DQuakeMDLImporter.cxx
@@ -7,7 +7,7 @@
 
 int TestF3DQuakeMDLImporter(int vtkNotUsed(argc), char* argv[])
 {
-  std::string filename = std::string(argv[1]) + "data/glaunch.mdl";
+  std::string filename = std::string(argv[1]) + "data/glaunch_2.mdl"; // File was modified to add coverage.
   vtkNew<vtkF3DQuakeMDLImporter> importer;
   importer->SetFileName(filename);
   importer->Update();
@@ -20,6 +20,8 @@ int TestF3DQuakeMDLImporter(int vtkNotUsed(argc), char* argv[])
   vtkIdType selectedAnimationIndex = 1;
   importer->EnableAnimation(selectedAnimationIndex);
   std::string animationName = importer->GetAnimationName(2);
+  std::string cameraName = importer->GetCameraName(0);
   importer->UpdateTimeStep(0.0);
-  return numAnimations == 2 && animationName == "" ? EXIT_SUCCESS : EXIT_FAILURE;
+  return numAnimations == 2 && animationName == "" && cameraName == "Camera" ? EXIT_SUCCESS
+                                                                       : EXIT_FAILURE;
 }
