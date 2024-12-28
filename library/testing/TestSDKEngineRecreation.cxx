a8e80cb5bacec8c9840771ee894b01b6fd2e89a9
Elijah Hantman <elihantman@gmail.com>
2024-12-28 11:07:03 -0800

    formatting fix for test

--- library/testing/TestSDKEngineRecreation.cxx
+++ library/testing/TestSDKEngineRecreation.cxx
@@ -7,19 +7,16 @@
 
 #include <iostream>
 
-int TestSDKEngineRecreation (int argc, char* argv[]) {
-
-    std::string filename = "TestSDKEngineRecreation";
-    std::string interactionFilePath = std::string(argv[2]) + "../../" + filename + ".log";
-    f3d::engine* eng = new f3d::engine(f3d::engine::create(false));
-    eng->getInteractor().start(1/30, [eng](){
-            eng->getInteractor().stop();
-            });
-    delete eng;
-    for (int i = 0; i < 5; i++) {
-        f3d::engine* eng2 = new f3d::engine(f3d::engine::create(false));
-        eng2->getInteractor().playInteraction(interactionFilePath);
-    }
-
-    return EXIT_SUCCESS;
+int TestSDKEngineRecreation(int argc, char* argv[]) {
+  std::string filename = "TestSDKEngineRecreation";
+  std::string interactionFilePath = std::string(argv[2]) + "../../" + filename + ".log";
+  f3d::engine* eng = new f3d::engine(f3d::engine::create(false));
+  eng->getInteractor().start(1/30, [eng](){ eng->getInteractor().stop(); });
+  delete eng;
+  for (int i = 0; i < 5; i++) {
+    f3d::engine* eng2 = new f3d::engine(f3d::engine::create(false));
+    eng2->getInteractor().playInteraction(interactionFilePath);
+  }
+  
+  return EXIT_SUCCESS;
 }
