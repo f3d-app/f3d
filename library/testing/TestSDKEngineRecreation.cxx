dc250312b2ca04f2de3193d5d0d30fc204c70a87
Elijah Hantman <elihantman@gmail.com>
2024-12-31 12:56:30 -0800

    modifications to simplify and improve testing

--- library/testing/TestSDKEngineRecreation.cxx
+++ library/testing/TestSDKEngineRecreation.cxx
@@ -1,16 +1,12 @@
 #include <engine.h>
 #include <interactor.h>
-#include <log.h>
-#include <options.h>
-#include <scene.h>
-#include <window.h>
 
 #include <iostream>
 
 int TestSDKEngineRecreation(int argc, char* argv[])
 {
   std::string filename = "TestSDKEngineRecreation";
-  std::string interactionFilePath = std::string(argv[2]) + "../../" + filename + ".log";
+  std::string interactionFilePath = std::string(argv[1]) + "/recordings/" +filename + ".log";
   f3d::engine* eng = new f3d::engine(f3d::engine::create(false));
   eng->getInteractor().start(1 / 30, [eng]() { eng->getInteractor().stop(); });
   delete eng;
