025ec3ab27a85b9d9f395021918f56a28fccfbfe
Mathieu Westphal <mathieu.westphal@gmail.com>
2025-01-03 19:47:22 +0100

    Improve python typing and examples (#1867)

--- python/testing/test_interactor_start.py
+++ python/testing/test_interactor_start.py
@@ -1,10 +1,9 @@
 from functools import partial
-import pytest
 
 import f3d
 
 
-def stop_fn(inter):
+def stop_fn(inter: f3d.Interactor):
     inter.stop()
 
 
@@ -14,7 +13,7 @@ def test_start_stop():
     inter.start(1 / 30, partial(stop_fn, inter))
 
 
-def test_request_render(capfd):
+def test_request_render():
     engine = f3d.Engine.create(True)
     inter = engine.interactor
     inter.request_render()
