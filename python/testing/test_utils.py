3807e631917938f2bba964b9cf957a0d14070c7c
Mathieu Westphal <mathieu.westphal@kitware.com>
2025-01-03 11:47:27 +0100

    fixup python

--- python/testing/test_utils.py
+++ python/testing/test_utils.py
@@ -1,9 +1,11 @@
 import f3d
 
+from pathlib import Path
+
 
 def test_text_distance():
     assert f3d.Utils.text_distance("modle", "model") == 2
 
 
 def test_collapse_path():
-    assert f3d.Utils.collapse_path("/folder/../file.ext", ".") == "/file.ext"
+    assert f3d.Utils.collapse_path("/folder/../file.ext", ".") == Path("/file.ext")
