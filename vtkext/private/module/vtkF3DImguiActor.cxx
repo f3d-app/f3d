44f56317cf084720918318f99ec10704a94a7bd5
Nig3l <nig3lpro@gmail.com>
2025-02-16 18:45:34 +0100

    application: store tickHalfThickNess is const

--- vtkext/private/module/vtkF3DImguiActor.cxx
+++ vtkext/private/module/vtkF3DImguiActor.cxx
@@ -297,6 +297,7 @@ void vtkF3DImguiActor::RenderDropZone()
     ImGuiViewport* viewport = ImGui::GetMainViewport();
     constexpr ImU32 color = IM_COL32(255, 255, 255, 255);
     constexpr float tickThickness = 3.0f;
+    constexpr float tickHalfThickness = tickThickness * 0.5f;
     constexpr float tickLength = 10.0f;
 
     int dropzonePad = static_cast<int>(std::min(viewport->WorkSize.x, viewport->WorkSize.y) * 0.1);
@@ -324,27 +325,27 @@ void vtkF3DImguiActor::RenderDropZone()
     double tickBBSizeH = tickLength + tickSpaceH;
 
     // Draw top and bottom line
-    for (float x = p0.x - tickThickness * 0.5f; x < p1.x - tickBBSizeW; x += tickBBSizeW)
+    for (float x = p0.x - tickHalfThickness; x < p1.x - tickBBSizeW; x += tickBBSizeW)
     {
       draw_list->AddLine(ImVec2(x, p0.y), ImVec2(x + tickLength, p0.y), color, tickThickness);
       draw_list->AddLine(ImVec2(x, p1.y), ImVec2(x + tickLength, p1.y), color, tickThickness);
     }
 
-    draw_list->AddLine(ImVec2(p1.x - tickLength, p0.y), ImVec2(p1.x + tickThickness * 0.5f, p0.y),
+    draw_list->AddLine(ImVec2(p1.x - tickLength, p0.y), ImVec2(p1.x + tickHalfThickness, p0.y),
       color, tickThickness);
-    draw_list->AddLine(ImVec2(p1.x - tickLength, p1.y), ImVec2(p1.x + tickThickness * 0.5f, p1.y),
+    draw_list->AddLine(ImVec2(p1.x - tickLength, p1.y), ImVec2(p1.x + tickHalfThickness, p1.y),
       color, tickThickness);
 
     // Draw left and right line
-    for (float y = p0.y - tickThickness * 0.5f; y < p1.y - tickBBSizeH; y += tickBBSizeH)
+    for (float y = p0.y - tickHalfThickness; y < p1.y - tickBBSizeH; y += tickBBSizeH)
     {
       draw_list->AddLine(ImVec2(p0.x, y), ImVec2(p0.x, y + tickLength), color, tickThickness);
       draw_list->AddLine(ImVec2(p1.x, y), ImVec2(p1.x, y + tickLength), color, tickThickness);
     }
 
-    draw_list->AddLine(ImVec2(p0.x, p1.y - tickLength), ImVec2(p0.x, p1.y + tickThickness * 0.5f),
+    draw_list->AddLine(ImVec2(p0.x, p1.y - tickLength), ImVec2(p0.x, p1.y + tickHalfThickness),
       color, tickThickness);
-    draw_list->AddLine(ImVec2(p1.x, p1.y - tickLength), ImVec2(p1.x, p1.y + tickThickness * 0.5f),
+    draw_list->AddLine(ImVec2(p1.x, p1.y - tickLength), ImVec2(p1.x, p1.y + tickHalfThickness),
       color, tickThickness);
 
     ImGui::End();
