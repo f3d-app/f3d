81c16d45b9df71af9b7d8004e1370fffd2909e07
Void Strike <kaydzyu1508@gmail.com>
2025-02-18 22:46:05 +0500

    std::accumulate fixed

--- library/src/interactor_impl.cxx
+++ library/src/interactor_impl.cxx
@@ -819,11 +819,14 @@ interactor& interactor_impl::initCommands()
       const std::string& aliasName = args[0];
       // Combine all remaining arguments into the alias command
       // Add alias command to the map
-      this->Internals->AliasMap[aliasName] =
-        std::accumulate(args.begin() + 1, args.end(), std::string(" "));
-      log::info(
-        "Alias" + aliasName + " added with command " + this->Internals->AliasMap[aliasName]);
-    });
+      this->Internals->AliasMap[aliasName] = 
+        std::accumulate(args.begin() + 2, args.end(), 
+          args[1],  // Start with first command argument
+          [](std::string a, std::string b) { return a + " " + b; });
+    
+    log::info(
+      "Alias " + aliasName + " added with command " + this->Internals->AliasMap[aliasName]);
+  });
   return *this;
 }
 
