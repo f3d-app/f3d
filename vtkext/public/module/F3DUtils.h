8d289825c4020d0a782f24d77be42b1c421dd0a0
Mathieu Westphal <mathieu.westphal@kitware.com>
2025-03-14 08:46:31 +0100

    export

--- vtkext/public/module/F3DUtils.h
+++ vtkext/public/module/F3DUtils.h
@@ -8,9 +8,11 @@
 #ifndef F3DUtils_h
 #define F3DUtils_h
 
+#include "vtkextModule.h"
+
 #include <string>
 
-namespace F3DUtils
+namespace VTKEXT_EXPORT F3DUtils
 {
 double ParseToDouble(const std::string& str, double def, const std::string& nameError);
 };
