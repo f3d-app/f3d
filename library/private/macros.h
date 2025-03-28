78a1aff17573cfa9865d89e640cb29967bdd64c6
Michael Migliore <mcmigliore@gmail.com>
2025-03-28 10:59:49 +0100

    fix

--- library/private/macros.h
+++ library/private/macros.h
@@ -11,12 +11,12 @@
 #elif defined(__clang__)
 #define F3D_SILENT_WARNING_PUSH() _Pragma("clang diagnostic push")
 #define F3D_SILENT_WARNING_DECL(MSVC_CODE, GNU_STRING)                                             \
-  _Pragma("clang diagnostic ignored \"-W" #GNU_STRING "\"")
+  _Pragma("clang diagnostic ignored \"-W" GNU_STRING "\"")
 #define F3D_SILENT_WARNING_POP() __pragma("clang diagnostic pop")
 #elif defined(__GNUC__)
 #define F3D_SILENT_WARNING_PUSH() _Pragma("GCC diagnostic push")
 #define F3D_SILENT_WARNING_DECL(MSVC_CODE, GNU_STRING)                                             \
-  _Pragma("GCC diagnostic ignored \"-W" #GNU_STRING "\"")
+  _Pragma("GCC diagnostic ignored \"-W" GNU_STRING "\"")
 #define F3D_SILENT_WARNING_POP() __pragma("GCC diagnostic pop")
 #else
 #define F3D_SILENT_WARNING_PUSH()
