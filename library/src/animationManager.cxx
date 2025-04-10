9eb9d1eab80a8dfc9a2f2b20b7ec66adc437bf72
Mathieu Westphal <mathieu.westphal@kitware.com>
2025-04-10 09:06:05 +0200

    clang format

--- library/src/animationManager.cxx
+++ library/src/animationManager.cxx
@@ -280,7 +280,8 @@ void animationManager::UpdateForAnimationIndex()
 {
   assert(this->Importer);
 
-  if (this->Options.scene.animation.index <= -2 || this->AnimationIndex == this->Options.scene.animation.index || this->AvailAnimations <= 0)
+  if (this->Options.scene.animation.index <= -2 ||
+    this->AnimationIndex == this->Options.scene.animation.index || this->AvailAnimations <= 0)
   {
     // Already updated or no animation available
     return;
