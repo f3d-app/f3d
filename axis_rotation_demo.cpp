b714d54182571a39ce2a374931e85e9a4f3d1944
ATHA767 <thapaaakrista@gmail.com>
2025-04-23 20:40:13 -0400

    Simulate axis-lock camera rotation

---
+++ axis_rotation_demo.cpp
@@ -0,0 +1,8 @@
+// axis_rotation_demo.cpp
+// Simulate axis-lock camera rotation
+#include <iostream>
+
+int main() {
+    std::cout << "Axis-lock camera rotation debug mode." << std::endl;
+    return 0;
+}
