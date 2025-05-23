f99676f21d367ade1e25d9842fb5741884be29a3
Elijah Hantman <elihantman@gmail.com>
2025-05-22 20:46:22 -0700

    added mapping from colormap-steps to dicretize value

--- application/F3DOptionsTools.h
+++ application/F3DOptionsTools.h
@@ -118,6 +118,7 @@ static inline const std::map<std::string_view, std::string_view> LibOptionsNames
   { "coloring-range", "model.scivis.range" },
   { "coloring-scalar-bar", "ui.scalar_bar" },
   { "colormap", "model.scivis.colormap" },
+  { "colormap-steps", "model.scivis.discretize" },
   { "volume", "model.volume.enable" },
   { "volume-inverse", "model.volume.inverse" },
   { "camera-orthographic", "scene.camera.orthographic" },
