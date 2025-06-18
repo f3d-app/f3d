5d2e36019a627548d668d49b4f6c5d24c1e47984
Mathieu Westphal <mathieu.westphal@kitware.com>
2025-06-18 08:56:14 +0200

    try creating render window

--- plugins/assimp/module/Testing/TestF3DAssimpImporter.cxx
+++ plugins/assimp/module/Testing/TestF3DAssimpImporter.cxx
@@ -1,5 +1,7 @@
 #include <vtkNew.h>
 #include <vtkTestUtilities.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderer.h>
 
 #include "vtkF3DAssimpImporter.h"
 
@@ -7,9 +9,14 @@
 
 int TestF3DAssimpImporter(int vtkNotUsed(argc), char* argv[])
 {
+  vtkNew<vtkRenderWindow> window;
+  vtkNew<vtkRenderer> renderer;
+  window->AddRenderer(renderer);
+
   std::string filename = std::string(argv[1]) + "data/animatedWorld.fbx";
   vtkNew<vtkF3DAssimpImporter> importer;
   importer->SetFileName(filename);
+  importer->SetRenderWindow(window);
   importer->Update();
   importer->Print(cout);
   return importer->GetNumberOfAnimations() == 1 ? EXIT_SUCCESS : EXIT_FAILURE;
