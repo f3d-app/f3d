acbabf8d9bf44453fd2a9056e60cbead9a837c81
Medyan Naser <medyan7.naser@gmail.com>
2025-07-07 17:06:14 -0400

    Add logo support in drop zone

--- vtkext/private/module/vtkF3DRenderer.cxx
+++ vtkext/private/module/vtkF3DRenderer.cxx
@@ -1649,6 +1649,16 @@ void vtkF3DRenderer::ShowDropZone(bool show)
 }
 
 //----------------------------------------------------------------------------
+void vtkF3DRenderer::ShowDropZoneLogo(bool show)
+{
+  if (this->DropZoneLogoVisible != show)
+  {
+    this->DropZoneLogoVisible = show;
+    this->UIActor->SetDropZoneLogoVisibility(show);
+  }
+}
+
+//----------------------------------------------------------------------------
 void vtkF3DRenderer::ShowHDRISkybox(bool show)
 {
   if (this->HDRISkyboxVisible != show)
