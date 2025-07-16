01a36e61fc45ad5624dc94303585d9fc252e5b04
Nig3l <nig3lpro@gmail.com>
2025-07-16 13:06:47 +0200

    Set default value to 1.0 to opacity option

--- library/src/interactor_impl.cxx
+++ library/src/interactor_impl.cxx
@@ -243,8 +243,8 @@ public:
   // Increase/Decrease opacity
   void IncreaseOpacity(bool negative)
   {
-    // current opacity, interpreted as 1 if it does not exist
-    const double currentOpacity = this->Options.model.color.opacity.value_or(1.0);
+    // current opacity, limited to 1.0 because we can't have more thant full opacity (1.0)
+    const double currentOpacity = std::min(this->Options.model.color.opacity, 1.0);
 
     // new opacity, clamped between 0 and 1 if not already set outside that range
     const double increment = negative ? -0.05 : 0.05;
@@ -1209,8 +1209,8 @@ interactor& interactor_impl::initBindings()
   this->addBinding({mod_t::NONE, "J"}, "toggle render.background.skybox","Scene", std::bind(docTgl, "Toggle HDRI skybox", std::cref(opts.render.background.skybox)));
   this->addBinding({mod_t::NONE, "L"}, "increase_light_intensity", "Scene", std::bind(docDbl, "Increase lights intensity", std::cref(opts.render.light.intensity)));
   this->addBinding({mod_t::SHIFT, "L"}, "decrease_light_intensity", "Scene", std::bind(docDbl, "Decrease lights intensity", std::cref(opts.render.light.intensity)));
-  this->addBinding({mod_t::CTRL, "P"}, "increase_opacity", "Scene", std::bind(docDblOpt, "Increase opacity", std::cref(opts.model.color.opacity)));
-  this->addBinding({mod_t::SHIFT, "P"}, "decrease_opacity", "Scene", std::bind(docDblOpt, "Decrease opacity", std::cref(opts.model.color.opacity)));
+  this->addBinding({mod_t::CTRL, "P"}, "increase_opacity", "Scene", std::bind(docDbl, "Increase opacity", std::cref(opts.model.color.opacity)));
+  this->addBinding({mod_t::SHIFT, "P"}, "decrease_opacity", "Scene", std::bind(docDbl, "Decrease opacity", std::cref(opts.model.color.opacity)));
   this->addBinding({mod_t::SHIFT, "A"}, "toggle render.armature.enable","Scene", std::bind(docTgl, "Toggle armature", std::cref(opts.render.armature.enable)));
   this->addBinding({mod_t::ANY, "1"}, "set_camera front", "Camera", std::bind(docStr, "Front View camera"));
   this->addBinding({mod_t::ANY, "2"}, "elevation_camera -90", "Camera", std::bind(docStr, "Rotate camera down"));
