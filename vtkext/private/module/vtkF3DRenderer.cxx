b596d536b5379d6eef3162cf300017453019d0ef
rrhxde <133144054+rrhxde@users.noreply.github.com>
2025-07-25 08:30:43 -0400

    Scalar bar continuity improvement (#2321)
    * maximum number of color scalar bar

--- vtkext/private/module/vtkF3DRenderer.cxx
+++ vtkext/private/module/vtkF3DRenderer.cxx
@@ -2757,6 +2757,7 @@ void vtkF3DRenderer::ConfigureScalarBarActorForColoring(
   scalarBar->SetWidth(0.8);
   scalarBar->SetHeight(0.07);
   scalarBar->SetPosition(0.1, 0.01);
+  scalarBar->SetMaximumNumberOfColors(512);
 }
 
 //----------------------------------------------------------------------------
