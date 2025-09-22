94b6e7ef7332c065c444a1f13210a2b3d41a5cc3
Michael Migliore <mcmigliore@gmail.com>
2025-09-22 11:14:30 +0200

    cov

---
+++ vtkext/private/module/Testing/TestF3DWebPReaderInvalid.cxx
@@ -0,0 +1,21 @@
+#include <vtkNew.h>
+
+#include "vtkF3DWebPReader.h"
+
+#include <iostream>
+
+int TestF3DWebPReaderInvalid(int argc, char* argv[])
+{
+  vtkNew<vtkF3DWebPReader> reader;
+  reader->Update();
+
+  // Do not create a dummy.webp
+  std::string filename = std::string(argv[1]) + "data/dummy.webp";
+  reader->CanReadFile(filename.c_str());
+
+  filename = std::string(argv[1]) + "data/invalid.webp";
+  reader->SetFileName(filename.c_str());
+  reader->Update();
+
+  return EXIT_SUCCESS;
+}
