6afc8674db1cd26e12ef1950a24f9350f85fc92f
Medyan Naser <medyan7.naser@gmail.com>
2025-10-12 11:29:30 -0400

    remove hierarchy from options.json

--- application/F3DStarter.cxx
+++ application/F3DStarter.cxx
@@ -1904,7 +1904,9 @@ void F3DStarter::LoadFileGroupInternal(
     this->Internals->LoadedFiles.empty();
   options.ui.filename_info = filenameInfo;
   // options.ui.hierarchy = f3d::scene& scene = this->Internals->Engine->getScene();
-  options.ui.hierarchy = scene.getSceneHierarchy();
+  // options.ui.hierarchy = scene.getSceneHierarchy();
+  auto hierarchy = scene.getSceneHierarchy();
+  this->Internals->Engine->getWindow().SetSceneHierarchy(hierarchy);
 }
 
 //----------------------------------------------------------------------------
