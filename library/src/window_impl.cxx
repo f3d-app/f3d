64b7c28c1555bced98ea71a7a1d402c34578ae80
Evgenii Startcev <slayer-gurgen@yandex.ru>
2025-10-23 21:48:44 +0200

    Reset camera after enabling volume rendering
    Co-authored-by: Mathieu Westphal <mathieu.westphal@gmail.com>

--- library/src/window_impl.cxx
+++ library/src/window_impl.cxx
@@ -653,6 +653,13 @@ vtkRenderWindow* window_impl::GetRenderWindow()
 bool window_impl::render()
 {
   this->UpdateDynamicOptions();
+  const options& opt = this->Internals->Options;
+  if ((!opt.scene.camera.index.has_value()) && (!this->Internals->Camera->GetSuccessfullyReset()))
+  {
+    // Camera wasn't successfully reset last time, it could be a chance that update of dynamic
+    // options will enable successful reset of camera
+    this->Internals->Camera->resetToBounds();
+  }
   this->Internals->RenWin->Render();
   return true;
 }
