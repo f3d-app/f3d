57666478b42734218dd2591a9afec863512cb2c5
Mathieu Westphal <mathieu.westphal@kitware.com>
2025-10-25 11:07:30 +0200

    Licensing: Update copyright to F3D-APP Foundation

--- library/src/engine.cxx
+++ library/src/engine.cxx
@@ -502,6 +502,7 @@ engine::libInformation engine::getLibInfo()
 
   libInfo.Copyrights.emplace_back("2019-2021 Kitware SAS");
   libInfo.Copyrights.emplace_back("2021-2025 Michael Migliore, Mathieu Westphal");
+  libInfo.Copyrights.emplace_back("2025 F3D-APP Foundation");
   libInfo.License = "BSD-3-Clause";
 
   return libInfo;
