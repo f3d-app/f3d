838902780dae2c9c7615ac5f14ac7aeeb2767179
Michael Migliore <mcmigliore@gmail.com>
2025-10-27 09:41:49 +0100

    ts

--- webassembly/build.sh
+++ webassembly/build.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+# todo: install in docker image directly
+npm install -g typescript
+
 cmake -S /src -B /src/_wasm_build \
     -DBUILD_SHARED_LIBS=OFF \
     -DCMAKE_BUILD_TYPE=$1 \
