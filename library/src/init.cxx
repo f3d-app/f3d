656fe708def6c050bfaeeaba7201e9c741a8f031
Michael Migliore <mcmigliore@gmail.com>
2025-11-21 23:11:07 +0100

    clean

--- library/src/init.cxx
+++ library/src/init.cxx
@@ -46,7 +46,6 @@ init::init()
   vtkNew<vtkF3DObjectFactory> factory;
   vtkObjectFactory::RegisterFactory(factory);
   vtkObjectFactory::SetAllEnableFlags(0, "vtkPolyDataMapper", "vtkOpenGLPolyDataMapper");
-  vtkObjectFactory::SetAllEnableFlags(0, "vtkPolyDataMapper", "vtkOpenGLLowMemoryPolyDataMapper");
 
 #if VTK_VERSION_NUMBER >= VTK_VERSION_CHECK(9, 3, 20240203)
   vtkObjectFactory::SetAllEnableFlags(0, "vtkPointGaussianMapper", "vtkOpenGLPointGaussianMapper");
