4ba4a8c40bd65ce0da81c4208b6ffe392775b2e8
Mathieu Westphal <mathieu.westphal@kitware.com>
2025-11-28 08:39:18 +0100

    interactor: small event loop fixups
    - Log when starting already started interactor
    - Log when stoping not started interactor
    - Avoid issues in ImGUI by checking deltaTime and logging if invalid
    - Add testing and docs

--- library/testing/TestSDKStartInteractor.cxx
+++ library/testing/TestSDKStartInteractor.cxx
@@ -9,6 +9,15 @@ int TestSDKStartInteractor([[maybe_unused]] int argc, [[maybe_unused]] char* arg
   f3d::scene& sce = eng.getScene();
   sce.add(std::string(argv[1]) + "/data/cow.vtp");
   f3d::interactor& inter = eng.getInteractor();
-  inter.start(1, [&inter]() { inter.stop(); });
+  inter.start(0.1, [&inter]() { inter.stop(); });
+
+  // Call start multiple times
+  inter.start(0.1, [&inter]() { inter.start(1, [&inter]() {}); inter.stop(); });
+
+  // Call stop without loop running
+  inter.stop();
+
+  // Event loop with non positive deltaTime
+  inter.triggerEventLoop(0);
   return EXIT_SUCCESS;
 }
