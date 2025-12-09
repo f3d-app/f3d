7c7a5b3c84fbcf46079bdbd102d07887b767dbfc
Samarthi <samarthi.lahiri@gmail.com>
2025-12-09 23:50:41 +0530

    Add minimal test cases for full coverage of vtkImguiObservers

--- library/testing/TestSDKTriggerInteractionsWithoutRender.cxx
+++ library/testing/TestSDKTriggerInteractionsWithoutRender.cxx
@@ -15,56 +15,30 @@ int TestSDKTriggerInteractionsWithoutRender([[maybe_unused]] int argc, [[maybe_u
   eng.getScene().add(DataPath);
   f3d::interactor& inter = eng.getInteractor();
   
-  // Trigger mouse wheel backward and check if it zoomed out
+  // Trigger mouse wheel 
   inter.triggerMouseWheel(f3d::interactor::WheelDirection::BACKWARD);
-  
-  // Trigger mouse wheel forward and check if it zoomed in
   inter.triggerMouseWheel(f3d::interactor::WheelDirection::FORWARD);
-  
-  // Trigger edge rendering with key press "E"
+
+  // Trigger keyboard key
   inter.triggerKeyboardKey(f3d::interactor::InputAction::PRESS, "E");
   inter.triggerKeyboardKey(f3d::interactor::InputAction::RELEASE, "E");
 
-  // Trigger decreased light rendering with key press "SHIFT+L"
+
+  // Trigger modifier update
   inter.triggerModUpdate(f3d::interactor::InputModifier::SHIFT);
-  inter.triggerKeyboardKey(f3d::interactor::InputAction::PRESS, "L");
-
-  // Trigger character inputs
   
-  inter.triggerKeyboardKey(f3d::interactor::InputAction::PRESS, "Escape"); // show console
-  inter.triggerKeyboardKey(f3d::interactor::InputAction::RELEASE, "Escape");
+  // Trigger character inputs 
   inter.triggerTextCharacter('f');
-  inter.triggerTextCharacter('3');
-  inter.triggerTextCharacter('d');
-  inter.triggerKeyboardKey(f3d::interactor::InputAction::PRESS, "Return");
-  inter.triggerKeyboardKey(f3d::interactor::InputAction::RELEASE, "Return");
-
+ 
   // Trigger rotate camera with mouse left button
   inter.triggerMousePosition(150, 100);
-  inter.triggerMouseButton(f3d::interactor::InputAction::PRESS, f3d::interactor::MouseButton::LEFT);
-  inter.triggerMousePosition(150, 200);
-  inter.triggerMouseButton(f3d::interactor::InputAction::RELEASE, f3d::interactor::MouseButton::LEFT);
   
-  // Trigger pan camera with mouse middle button
-  inter.triggerMousePosition(150, 100);
-  inter.triggerMouseButton(
-      f3d::interactor::InputAction::PRESS, f3d::interactor::MouseButton::MIDDLE);
-  inter.triggerMousePosition(150, 200);
-  inter.triggerMouseButton(
-      f3d::interactor::InputAction::RELEASE, f3d::interactor::MouseButton::MIDDLE);
+  // Trigger mouse button
+  inter.triggerMouseButton(f3d::interactor::InputAction::PRESS, f3d::interactor::MouseButton::LEFT);
+  inter.triggerMouseButton(f3d::interactor::InputAction::RELEASE, f3d::interactor::MouseButton::LEFT);
+  inter.triggerMouseButton(f3d::interactor::InputAction::PRESS, f3d::interactor::MouseButton::RIGHT);
+  inter.triggerMouseButton(f3d::interactor::InputAction::RELEASE, f3d::interactor::MouseButton::RIGHT);
 
-  // Trigger zoom camera with mouse right button
-  inter.triggerMousePosition(150, 100);
-  inter.triggerMouseButton(
-      f3d::interactor::InputAction::PRESS, f3d::interactor::MouseButton::RIGHT);
-  inter.triggerMousePosition(150, 200);
-  inter.triggerMouseButton(
-      f3d::interactor::InputAction::RELEASE, f3d::interactor::MouseButton::RIGHT);
-
-  // following calls are only there for coverage
-  // there is no interactor style action associated with these yet
-  inter.triggerMouseWheel(f3d::interactor::WheelDirection::LEFT);
-  inter.triggerMouseWheel(f3d::interactor::WheelDirection::RIGHT);
-
+  
   return EXIT_SUCCESS;
 }
