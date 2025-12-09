cd7e5446260cb728b4054d6c936531bf417a4895
Samarthi <samarthi.lahiri@gmail.com>
2025-12-09 23:31:12 +0530

    Modify interactor tests to make them simpler

--- library/testing/TestSDKTriggerInteractionsWithoutRender.cxx
+++ library/testing/TestSDKTriggerInteractionsWithoutRender.cxx
@@ -6,123 +6,65 @@
 
 #include <iostream>
 
-struct TestTriggerHelper
-{
-  TestTriggerHelper(std::string dataPath, std::string baselinePath, std::string outputPath)
-    : DataPath(std::move(dataPath))
-    , BaselinePath(std::move(baselinePath))
-    , OutputPath(std::move(outputPath))
-  {
-  }
-
-  template<typename Func>
-  void operator()(Func&& func) const
-  {
-    f3d::engine engine = f3d::engine::create();
-    engine.getWindow().setSize(300, 300);
-    engine.getScene().add(this->DataPath);
-
-    func(engine);
-  }
-
-  // Run with baseline test
-  template<typename Func>
-  void operator()(const std::string& name, Func&& func) const
-  {
-    (*this)([&](f3d::engine& engine) {
-      func(engine);
-
-      if (!TestSDKHelpers::RenderTest(
-            engine.getWindow(), this->BaselinePath, this->OutputPath, name))
-      {
-        throw std::runtime_error("Render test failed for " + name);
-      }
-    });
-  }
-
-  std::string DataPath;
-  std::string BaselinePath;
-  std::string OutputPath;
-};
-
 int TestSDKTriggerInteractionsWithoutRender([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
-  TestTriggerHelper helper(
-    std::string(argv[1]) + "/data/cow.vtp", std::string(argv[1]) + "baselines/", argv[2]);
-
+  f3d::log::setVerboseLevel(f3d::log::VerboseLevel::DEBUG);
+  f3d::engine eng = f3d::engine::create(true);
+  std::string DataPath = std::string(argv[1]) + "/data/cow.vtp";
+  eng.getWindow().setSize(300, 300);
+  eng.getScene().add(DataPath);
+  f3d::interactor& inter = eng.getInteractor();
+  
   // Trigger mouse wheel backward and check if it zoomed out
-  helper("TestSDKTriggerInteractionsWheelBackward", [](f3d::engine& engine) {
-    engine.getInteractor().triggerMouseWheel(f3d::interactor::WheelDirection::BACKWARD);
-  });
-
+  inter.triggerMouseWheel(f3d::interactor::WheelDirection::BACKWARD);
+  
   // Trigger mouse wheel forward and check if it zoomed in
-  helper("TestSDKTriggerInteractionsWheelForward", [](f3d::engine& engine) {
-    engine.getInteractor().triggerMouseWheel(f3d::interactor::WheelDirection::FORWARD);
-  });
-
+  inter.triggerMouseWheel(f3d::interactor::WheelDirection::FORWARD);
+  
   // Trigger edge rendering with key press "E"
-  helper("TestSDKTriggerInteractionsKeyPress", [](f3d::engine& engine) {
-    engine.getInteractor().triggerKeyboardKey(f3d::interactor::InputAction::PRESS, "E");
-    engine.getInteractor().triggerKeyboardKey(f3d::interactor::InputAction::RELEASE, "E");
-  });
+  inter.triggerKeyboardKey(f3d::interactor::InputAction::PRESS, "E");
+  inter.triggerKeyboardKey(f3d::interactor::InputAction::RELEASE, "E");
 
   // Trigger decreased light rendering with key press "SHIFT+L"
-  helper("TestSDKTriggerInteractionsKeyPressMod", [](f3d::engine& engine) {
-    for (int i = 0; i < 30; ++i)
-    {
-      engine.getInteractor().triggerModUpdate(f3d::interactor::InputModifier::SHIFT);
-      engine.getInteractor().triggerKeyboardKey(f3d::interactor::InputAction::PRESS, "L");
-    }
-  });
+  inter.triggerModUpdate(f3d::interactor::InputModifier::SHIFT);
+  inter.triggerKeyboardKey(f3d::interactor::InputAction::PRESS, "L");
 
   // Trigger character inputs
-  helper("TestSDKTriggerInteractionsTextInput", [](f3d::engine& engine) {
-    engine.getInteractor().triggerKeyboardKey(
-      f3d::interactor::InputAction::PRESS, "Escape"); // show console
-    engine.getInteractor().triggerKeyboardKey(f3d::interactor::InputAction::RELEASE, "Escape");
-    engine.getInteractor().triggerTextCharacter('f');
-    engine.getInteractor().triggerTextCharacter('3');
-    engine.getInteractor().triggerTextCharacter('d');
-    engine.getInteractor().triggerKeyboardKey(f3d::interactor::InputAction::PRESS, "Return");
-    engine.getInteractor().triggerKeyboardKey(f3d::interactor::InputAction::RELEASE, "Return");
-  });
+  
+  inter.triggerKeyboardKey(f3d::interactor::InputAction::PRESS, "Escape"); // show console
+  inter.triggerKeyboardKey(f3d::interactor::InputAction::RELEASE, "Escape");
+  inter.triggerTextCharacter('f');
+  inter.triggerTextCharacter('3');
+  inter.triggerTextCharacter('d');
+  inter.triggerKeyboardKey(f3d::interactor::InputAction::PRESS, "Return");
+  inter.triggerKeyboardKey(f3d::interactor::InputAction::RELEASE, "Return");
 
   // Trigger rotate camera with mouse left button
-  helper("TestSDKTriggerInteractionsMouseRotate", [](f3d::engine& engine) {
-    engine.getInteractor().triggerMousePosition(150, 100);
-    engine.getInteractor().triggerMouseButton(
-      f3d::interactor::InputAction::PRESS, f3d::interactor::MouseButton::LEFT);
-    engine.getInteractor().triggerMousePosition(150, 200);
-    engine.getInteractor().triggerMouseButton(
-      f3d::interactor::InputAction::RELEASE, f3d::interactor::MouseButton::LEFT);
-  });
-
+  inter.triggerMousePosition(150, 100);
+  inter.triggerMouseButton(f3d::interactor::InputAction::PRESS, f3d::interactor::MouseButton::LEFT);
+  inter.triggerMousePosition(150, 200);
+  inter.triggerMouseButton(f3d::interactor::InputAction::RELEASE, f3d::interactor::MouseButton::LEFT);
+  
   // Trigger pan camera with mouse middle button
-  helper("TestSDKTriggerInteractionsMousePan", [](f3d::engine& engine) {
-    engine.getInteractor().triggerMousePosition(150, 100);
-    engine.getInteractor().triggerMouseButton(
+  inter.triggerMousePosition(150, 100);
+  inter.triggerMouseButton(
       f3d::interactor::InputAction::PRESS, f3d::interactor::MouseButton::MIDDLE);
-    engine.getInteractor().triggerMousePosition(150, 200);
-    engine.getInteractor().triggerMouseButton(
+  inter.triggerMousePosition(150, 200);
+  inter.triggerMouseButton(
       f3d::interactor::InputAction::RELEASE, f3d::interactor::MouseButton::MIDDLE);
-  });
 
   // Trigger zoom camera with mouse right button
-  helper("TestSDKTriggerInteractionsMouseZoom", [](f3d::engine& engine) {
-    engine.getInteractor().triggerMousePosition(150, 100);
-    engine.getInteractor().triggerMouseButton(
+  inter.triggerMousePosition(150, 100);
+  inter.triggerMouseButton(
       f3d::interactor::InputAction::PRESS, f3d::interactor::MouseButton::RIGHT);
-    engine.getInteractor().triggerMousePosition(150, 200);
-    engine.getInteractor().triggerMouseButton(
+  inter.triggerMousePosition(150, 200);
+  inter.triggerMouseButton(
       f3d::interactor::InputAction::RELEASE, f3d::interactor::MouseButton::RIGHT);
-  });
 
   // following calls are only there for coverage
   // there is no interactor style action associated with these yet
-  helper([](f3d::engine& engine) {
-    engine.getInteractor().triggerMouseWheel(f3d::interactor::WheelDirection::LEFT);
-    engine.getInteractor().triggerMouseWheel(f3d::interactor::WheelDirection::RIGHT);
-  });
+  inter.triggerMouseWheel(f3d::interactor::WheelDirection::LEFT);
+  inter.triggerMouseWheel(f3d::interactor::WheelDirection::RIGHT);
 
   return EXIT_SUCCESS;
 }
