33215ea126f0b7fa59cdad06ec3ab43c9905c5b5
Vincenzo <vincenzontt02@gmail.com>
2025-12-19 17:44:51 +0100

    fix: made grid mapper an attribute after the merge

--- vtkext/private/module/vtkF3DRenderer.cxx
+++ vtkext/private/module/vtkF3DRenderer.cxx
@@ -832,11 +832,11 @@ void vtkF3DRenderer::ConfigureGridUsingCurrentActors()
       const double f1 = fX1 + fY1 + fZ1;
       const double f2 = fX2 + fY2 + fZ2;
 
-      gridMapper->SetAxis1Color( //
+      this->GridMapper->SetAxis1Color( //
         (colorX[0] * fX1 + colorY[0] * fY1 + colorZ[0] * fZ1) / f1,
         (colorX[1] * fX1 + colorY[1] * fY1 + colorZ[1] * fZ1) / f1,
         (colorX[2] * fX1 + colorY[2] * fY1 + colorZ[2] * fZ1) / f1, 1);
-      gridMapper->SetAxis2Color( //
+      this->GridMapper->SetAxis2Color( //
         (colorX[0] * fX2 + colorY[0] * fY2 + colorZ[0] * fZ2) / f2,
         (colorX[1] * fX2 + colorY[1] * fY2 + colorZ[1] * fZ2) / f2,
         (colorX[2] * fX2 + colorY[2] * fY2 + colorZ[2] * fZ2) / f2, 1);
