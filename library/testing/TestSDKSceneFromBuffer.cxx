4a5be73de3a787b0030b44487f37c2082ce409c9
Mathieu Westphal <mathieu.westphal@kitware.com>
2025-12-30 21:36:48 +0100

    more fixups

--- library/testing/TestSDKSceneFromBuffer.cxx
+++ library/testing/TestSDKSceneFromBuffer.cxx
@@ -16,28 +16,23 @@ int TestSDKSceneFromBuffer([[maybe_unused]] int argc, [[maybe_unused]] char* arg
   f3d::options& opt = eng.getOptions();
 
   // Add empty buffer
-  test("Add empty buffer", [&]() {
-    sce.add(nullptr, 0);
-  });
+  test("Add empty buffer", [&]() { sce.add(nullptr, 0); });
 
-  char c='\0';
+  char c = '\0';
 
   // Add buffer without setting reader
-  test.expect<f3d::scene::load_failure_exception>("add buffer without setting reader", [&]() {
-    sce.add(&c, 1);
-  });
+  test.expect<f3d::scene::load_failure_exception>(
+    "add buffer without setting reader", [&]() { sce.add(&c, 1); });
 
   // Add buffer with invalid reader
   opt.scene.force_reader = "INVALID";
-  test.expect<f3d::scene::load_failure_exception>("add buffer with invalid reader", [&]() {
-    sce.add(&c, 1);
-  });
+  test.expect<f3d::scene::load_failure_exception>(
+    "add buffer with invalid reader", [&]() { sce.add(&c, 1); });
 
   // Add buffer with reader that doesn't support streams
   opt.scene.force_reader = "Nrrd";
-  test.expect<f3d::scene::load_failure_exception>("add buffer with reader that doesn't support streams", [&]() {
-    sce.add(&c, 1);
-  });
+  test.expect<f3d::scene::load_failure_exception>(
+    "add buffer with reader that doesn't support streams", [&]() { sce.add(&c, 1); });
 
   return test.result();
 }
