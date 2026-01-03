b03a69aa633cccb001f3f56c9235217be8f35663
Mathieu Westphal <mathieu.westphal@kitware.com>
2026-01-03 09:06:13 +0100

    TestSDKExternalWindowQT: Use Qt6 instead of Qt5 (#2764)

--- library/testing/TestSDKExternalWindowQT.cxx
+++ library/testing/TestSDKExternalWindowQT.cxx
@@ -1,39 +1,39 @@
-#include <engine.h>
-#include <scene.h>
-#include <window.h>
-
-#include <QGuiApplication>
-#include <QKeyEvent>
-#include <QOpenGLWindow>
-
 #include "TestSDKHelpers.h"
 
-#include <utility>
+#include "engine.h"
+#include "interactor.h"
+#include "options.h"
+#include "scene.h"
+#include "window.h"
 
-class F3DWindow : public QOpenGLWindow
+#include <QApplication>
+#include <QCoreApplication>
+#include <QMainWindow>
+#include <QOpenGLContext>
+#include <QOpenGLWidget>
+#include <QTimer>
+#include <QtGlobal>
+
+class F3DWidget : public QOpenGLWidget
 {
 public:
-  F3DWindow(std::string filePath, std::string baselinePath, std::string outputPath)
-    : QOpenGLWindow()
+  explicit F3DWidget(std::string filePath, std::string baselinePath, std::string outputPath,
+    QWidget* parent = nullptr)
+    : QOpenGLWidget(parent)
     , mFilePath(std::move(filePath))
     , mBaselinePath(std::move(baselinePath))
     , mOutputPath(std::move(outputPath))
   {
   }
 
-protected:
-  void keyPressEvent(QKeyEvent* event) override
-  {
-    if (event->key() == Qt::Key_Escape)
-    {
-      this->close();
-    }
-  }
-
-  void timerEvent(QTimerEvent* event) override
+  bool checkFrameBuffer()
   {
     // before closing, compare the content of the framebuffer with the baseline
+#if QT_VERSION >= QT_VERSION_CHECK(6, 9, 0)
+    QImage frameBuffer = grabFramebuffer().flipped().convertToFormat(QImage::Format_RGB888);
+#else
     QImage frameBuffer = grabFramebuffer().mirrored().convertToFormat(QImage::Format_RGB888);
+#endif
 
     f3d::image img(frameBuffer.width(), frameBuffer.height(), 3);
     img.setContent(frameBuffer.bits());
@@ -41,55 +41,112 @@ protected:
     if (!TestSDKHelpers::RenderTest(
           img, this->mBaselinePath, this->mOutputPath, "TestSDKExternalWindowQT"))
     {
+      return false;
+    }
+    return true;
+  }
+
+protected:
+  void initializeGL() override
+  {
+    f3d::engine::autoloadPlugins();
+
+    f3d::context::function loadFunc = [this](const char* name) {
+      return this->context()->getProcAddress(name);
+    };
+    this->mEngine = std::make_unique<f3d::engine>(f3d::engine::createExternal(loadFunc));
+
+    if (!this->mEngine)
+    {
+      std::cerr << "Failed to create f3d::engine" << '\n';
+      return;
+    }
+
+    this->mInteractor = &this->mEngine->getInteractor();
+
+    this->mTimer = new QTimer(this);
+    this->mTimer->setInterval(30);
+    connect(this->mTimer, &QTimer::timeout, this, &F3DWidget::onTick);
+    this->mTimer->start();
+
+    this->mEngine->getScene().add(this->mFilePath);
+
+    auto& win = this->mEngine->getWindow();
+    win.setSize(this->width(), this->height());
+
+    auto& opt = this->mEngine->getOptions();
+
+    // Test setting an option works
+    opt.render.grid.enable = true;
+    opt.ui.axis = true;
+  }
+
+  void paintGL() override
+  {
+    this->mEngine->getWindow().render();
+  }
+
+private slots:
+  void onTick()
+  {
+    if (!this->mEngine || !this->mInteractor)
+    {
+      return;
+    }
+
+    this->makeCurrent();
+    this->mInteractor->triggerEventLoop(1.0 / 30.0);
+    this->update();
+    this->doneCurrent();
+  }
+
+private:
+  std::string mFilePath;
+  std::string mBaselinePath;
+  std::string mOutputPath;
+  std::unique_ptr<f3d::engine> mEngine;
+  f3d::interactor* mInteractor = nullptr;
+  QTimer* mTimer = nullptr;
+};
+
+class MainWindow : public QMainWindow
+{
+public:
+  explicit MainWindow(std::string filePath, std::string baselinePath, std::string outputPath,
+    QWidget* parent = nullptr)
+    : QMainWindow(parent)
+  {
+    this->setWindowTitle(QStringLiteral("F3D QT External Window"));
+    this->mViewer =
+      new F3DWidget(std::move(filePath), std::move(baselinePath), std::move(outputPath), this);
+    this->setCentralWidget(this->mViewer);
+    this->resize(300, 300);
+  }
+
+public
+  Q_SLOT : void onClose()
+  {
+    if (!this->mViewer->checkFrameBuffer())
+    {
       QCoreApplication::exit(EXIT_FAILURE);
     }
 
     this->close();
   }
 
-  void initializeGL() override
-  {
-    this->QOpenGLWindow::initializeGL();
-
-    f3d::context::function loadFunc = [this](const char* name) {
-      return this->context()->getProcAddress(name);
-    };
-
-    mEngine = std::make_unique<f3d::engine>(f3d::engine::createExternal(loadFunc));
-    mEngine->getWindow().setSize(width(), height());
-    mEngine->getScene().add(mFilePath);
-    auto& opt = mEngine->getOptions();
-
-    // Test setting an option works
-    opt.render.grid.enable = true;
-
-    // XXX: This has no effect as external window does not create an interactor
-    opt.ui.axis = true;
-  }
-
-  void paintGL() override
-  {
-    mEngine->getWindow().render();
-  }
-
 private:
-  std::unique_ptr<f3d::engine> mEngine;
-  std::string mFilePath;
-  std::string mBaselinePath;
-  std::string mOutputPath;
+  F3DWidget* mViewer;
 };
 
-int TestSDKExternalWindowQT([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
+int TestSDKExternalWindowQT(int argc, char** argv)
 {
-  QGuiApplication a(argc, argv);
+  QApplication app(argc, argv);
 
-  F3DWindow w(
+  MainWindow win(
     std::string(argv[1]) + "/data/cow.vtp", std::string(argv[1]) + "/baselines/", argv[2]);
-  w.setTitle("F3D QT External Window");
-  w.resize(300, 300);
-  w.show();
+  win.show();
 
-  w.startTimer(1000);
+  QTimer::singleShot(1000, &win, &MainWindow::onClose);
 
-  return a.exec();
+  return app.exec();
 }
