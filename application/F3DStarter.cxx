baef9dcd09e4e6e1e02f337d1495906de615bf5c
atharv sharma <atharv1908@gmail.com>
2026-01-07 12:24:58 +0530

    properly formatted now

--- application/F3DStarter.cxx
+++ application/F3DStarter.cxx
@@ -2153,7 +2153,8 @@ void F3DStarter::AddCommands()
 
   interactor.addCommand(
     "load_previous_file_group",
-    [this](const std::vector<std::string>& args) {
+    [this](const std::vector<std::string>& args)
+    {
       this->LoadRelativeFileGroup(
         -1, parse_optional_bool_flag(args, "load_previous_file_group", false));
     },
@@ -2162,8 +2163,7 @@ void F3DStarter::AddCommands()
 
   interactor.addCommand(
     "load_next_file_group",
-    [this](const std::vector<std::string>& args)
-    {
+    [this](const std::vector<std::string>& args) {
       this->LoadRelativeFileGroup(
         +1, parse_optional_bool_flag(args, "load_next_file_group", false));
     },
