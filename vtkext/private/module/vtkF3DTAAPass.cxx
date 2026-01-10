c1904004b0594004441d42cb81dde84b2c68a011
Ao Gao <ao.gao@tum.de>
2026-01-11 00:07:33 +0100

    CHORE: Shader replacement improvement

--- vtkext/private/module/vtkF3DTAAPass.cxx
+++ vtkext/private/module/vtkF3DTAAPass.cxx
@@ -159,8 +159,8 @@ bool vtkF3DTAAPass::PreReplaceShaderValues(std::string& vertexShader,
   {
     vtkShaderProgram::Substitute(
       vertexShader, "//VTK::Clip::Dec", "uniform vec2 jitter;\n//VTK::Clip::Dec", false);
-    vtkShaderProgram::Substitute(
-      vertexShader, "//VTK::CustomEnd::Impl", "  gl_Position.xy += jitter;\n", false);
+    vtkShaderProgram::Substitute(vertexShader, "//VTK::CustomEnd::Impl",
+      "  gl_Position.xy += jitter;\n//VTK::CustomEnd::Impl", false);
   }
   return true;
 }
