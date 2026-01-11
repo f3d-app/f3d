be657d62d2b681a8b255ee00f3bd5aa93ff1314f
Archie Payne <115465107+ArchiePayne@users.noreply.github.com>
2026-01-11 16:59:02 +0000

    Fix CI styling errors.

--- library/src/interactor_impl.cxx
+++ library/src/interactor_impl.cxx
@@ -1242,7 +1242,7 @@ interactor& interactor_impl::initCommands()
     std::bind(complNames, std::placeholders::_1,
       std::vector<std::string>{ "front", "top", "right", "isometric" }));
 
-   this->addCommand(
+  this->addCommand(
     "toggle_normal_glyphs",
     [&](const std::vector<std::string>&)
     {
@@ -1250,8 +1250,7 @@ interactor& interactor_impl::initCommands()
         !this->Internals->Options.model.normal_glyphs.enable;
       this->Internals->Window.render();
     },
-    command_documentation_t{
-      "toggle_normal_glyphs", "toggle model.normal_glyphs.enable" });
+    command_documentation_t{ "toggle_normal_glyphs", "toggle model.normal_glyphs.enable" });
 
   this->addCommand(
     "toggle_volume_rendering",
