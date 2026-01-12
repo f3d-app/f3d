152fc9b18f3b0d93200c8a4e425eec8e7ba3d5bb
Nig3l <cots.max@gmail.com>
2026-01-12 07:17:47 +0100

    Add JS Bindings to animationKeyFrames

--- webassembly/F3DEmscriptenBindings.cxx
+++ webassembly/F3DEmscriptenBindings.cxx
@@ -167,8 +167,11 @@ EMSCRIPTEN_BINDINGS(f3d)
     .function("loadAnimationTime", &f3d::scene::loadAnimationTime,
       emscripten::return_value_policy::reference())
     .function(
-      "animationTimeRange",
-      +[](f3d::scene& o) -> emscripten::val { return pairToJSArray(o.animationTimeRange()); })
+      "animationTimeRange", +[](f3d::scene& scene) -> emscripten::val
+      { return pairToJSArray(scene.animationTimeRange()); })
+    .function(
+      "animationKeyFrames", +[](f3d::scene& scene) -> emscripten::val
+      { return containerToJSArray(scene.animationKeyFrames()); })
     .function("availableAnimations", &f3d::scene::availableAnimations)
     .function("getAnimationName", &f3d::scene::getAnimationName)
     .function(
