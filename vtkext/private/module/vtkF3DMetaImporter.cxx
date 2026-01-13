f9adb541a461d7cffaa5aac34be84668e987ee67
Archie Payne <115465107+ArchiePayne@users.noreply.github.com>
2026-01-13 19:24:09 +0000

    Change Glyph3D to Glyph3DMapper

--- vtkext/private/module/vtkF3DMetaImporter.cxx
+++ vtkext/private/module/vtkF3DMetaImporter.cxx
@@ -15,6 +15,7 @@
 #include <vtkObjectFactory.h>
 #include <vtkPointData.h>
 #include <vtkPolyData.h>
+#include <vtkPolyDataNormals.h>
 #include <vtkRenderWindow.h>
 #include <vtkRendererCollection.h>
 #include <vtkSmartPointer.h>
