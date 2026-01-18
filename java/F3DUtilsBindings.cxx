19847b04f5b47a1574129cc55f15322fe026cf5b
David Tang <122414668+davidwuluetang@users.noreply.github.com>
2026-01-17 21:16:00 -0500

    try to fix java binding error

--- java/F3DUtilsBindings.cxx
+++ java/F3DUtilsBindings.cxx
@@ -129,7 +129,7 @@ extern "C"
     return result.has_value() ? env->NewStringUTF(result.value().c_str()) : nullptr;
   }
 
-  JNIEXPORT jdouble JAVA_BIND(Utils, getDPIScale)(jclass)
+  JNIEXPORT jdouble JAVA_BIND(Utils, getDPIScale)(JNIEnv* env, jclass)
   {
     return static_cast<jdouble>(f3d::utils::getDPIScale());
   }
