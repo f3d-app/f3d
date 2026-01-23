523fed24ad5235bc93165b524d9a4579458676f5
Jean Fechter <jean.fechter@gmail.com>
2026-01-23 22:30:03 +0100

    New formalism for tests

---
+++ application/testing/tests.ifc.cmake
@@ -0,0 +1,12 @@
+## Tests that needs ifc plugin
+## This file is only added if ifc is enabled
+f3d_test(NAME TestIFC DATA ifc/IfcOpenHouse.ifc ARGS --load-plugins=ifc)
+f3d_test(NAME TestIFCDefines DATA ifc/IfcOpenHouse.ifc ARGS --load-plugins=ifc -DIFC.circle_segments=24 -DIFC.read_openings=0 -DIFC.read_spaces=0)
+
+if(NOT F3D_MACOS_BUNDLE)
+  file(COPY "${F3D_SOURCE_DIR}/plugins/ifc/configs/config.d/" DESTINATION "${CMAKE_BINARY_DIR}/share/f3d/configs/config_build.d")
+  f3d_test(NAME TestDefaultConfigFileIFC DATA ifc/IfcOpenHouse.ifc CONFIG config_build LONG_TIMEOUT TONE_MAPPING)
+
+  file(COPY "${F3D_SOURCE_DIR}/plugins/ifc/configs/thumbnail.d/" DESTINATION "${CMAKE_BINARY_DIR}/share/f3d/configs/thumbnail_build.d")
+  f3d_test(NAME TestThumbnailConfigFileIFC DATA ifc/IfcOpenHouse.ifc CONFIG thumbnail_build LONG_TIMEOUT TONE_MAPPING)
+endif()
