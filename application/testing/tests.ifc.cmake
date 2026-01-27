ea141283be7b69d155d98d02ebf79c17b8bbe899
Jean Fechter <jean.fechter@gmail.com>
2026-01-27 11:01:57 +0100

    Add piped test

--- application/testing/tests.ifc.cmake
+++ application/testing/tests.ifc.cmake
@@ -2,6 +2,7 @@
 ## This file is only added if ifc is enabled
 f3d_test(NAME TestIFC DATA ifc/IfcOpenHouse_IFC4.ifc ARGS --load-plugins=ifc)
 f3d_test(NAME TestIFCDefines DATA ifc/IfcOpenHouse_IFC4.ifc ARGS --load-plugins=ifc -DIFC.circle_segments=24 -DIFC.read_openings=0 -DIFC.read_spaces=0)
+f3d_test(NAME TestPipedIFC DATA ifc/IfcOpenHouse_IFC4.ifc ARGS --load-plugins=ifc --force-reader=IFC PIPED)
 
 if(NOT F3D_MACOS_BUNDLE)
   file(COPY "${F3D_SOURCE_DIR}/plugins/ifc/configs/config.d/" DESTINATION "${CMAKE_BINARY_DIR}/share/f3d/configs/config_build.d")
