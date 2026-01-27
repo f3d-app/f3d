44b529f767e8e4a48f1be3b89d44ce3c3726361a
Jean Fechter <jean.fechter@gmail.com>
2026-01-27 19:54:05 +0100

    try fix vtk version check

--- application/testing/tests.ifc.cmake
+++ application/testing/tests.ifc.cmake
@@ -6,8 +6,12 @@ f3d_test(NAME TestPipedIFC DATA ifc/IfcOpenHouse_IFC4.ifc ARGS --load-plugins=if
 
 if(NOT F3D_MACOS_BUNDLE)
   file(COPY "${F3D_SOURCE_DIR}/plugins/ifc/configs/config.d/" DESTINATION "${CMAKE_BINARY_DIR}/share/f3d/configs/config_build.d")
-  f3d_test(NAME TestDefaultConfigFileIFC DATA ifc/IfcOpenHouse_IFC4.ifc CONFIG config_build LONG_TIMEOUT TONE_MAPPING)
+  if(VTK_VERSION VERSION_GREATER_EQUAL 9.5.20251001)
+    f3d_test(NAME TestDefaultConfigFileIFC DATA ifc/IfcOpenHouse_IFC4.ifc CONFIG config_build LONG_TIMEOUT TONE_MAPPING)
+  endif()
 
   file(COPY "${F3D_SOURCE_DIR}/plugins/ifc/configs/thumbnail.d/" DESTINATION "${CMAKE_BINARY_DIR}/share/f3d/configs/thumbnail_build.d")
-  f3d_test(NAME TestThumbnailConfigFileIFC DATA ifc/IfcOpenHouse_IFC4.ifc CONFIG thumbnail_build LONG_TIMEOUT TONE_MAPPING)
+  if(VTK_VERSION VERSION_GREATER_EQUAL 9.5.20251001)
+    f3d_test(NAME TestThumbnailConfigFileIFC DATA ifc/IfcOpenHouse_IFC4.ifc CONFIG thumbnail_build LONG_TIMEOUT TONE_MAPPING)
+  endif()
 endif()
