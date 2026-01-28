0bb3584df4e188bd1fe37b31fdc5de2e54ecaf89
Dominik Rau <rau@realtimevisions.de>
2026-01-28 18:00:49 +0100

    Re-added timeout parameter / Commandline parser

--- examples/libf3d/cpp/qml/main.cxx
+++ examples/libf3d/cpp/qml/main.cxx
@@ -3,24 +3,51 @@
 #include <QQmlContext>
 #include <QDebug>
 #include <QQuickWindow>
+#include <QCommandLineParser>
+#include <QFileInfo>
+#include <QTimer>
 
 int main(int argc, char* argv[])
 {
   QGuiApplication app(argc, argv);
 
-  if(argc < 2){
-    qWarning()<<"File argument required";
-    exit(1);
-  }
-  QString fileArg(argv[1]);
-
   // F3D requires OpenGL backend!
   QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenGL);
 
-  QQmlApplicationEngine engine;
+  QCommandLineParser parser;
+  parser.setApplicationDescription("libf3d + QML example");
+  parser.addHelpOption();
 
-  engine.rootContext()->setContextProperty("fileArgument", fileArg);
+  QCommandLineOption timeoutOpt("timeout", "Optional timeout (in seconds) before closing the viewer.", "seconds");
+  parser.addOption(timeoutOpt);
+  parser.addPositionalArgument("file", "3D model file to open");
+  parser.process(app);
+
+  const QStringList pos = parser.positionalArguments();
+  if (pos.isEmpty())
+  {
+    qWarning("No model file provided");
+    return 1;
+  }
+
+  QFileInfo fi(pos.first());
+  if (!fi.exists())
+  {
+    qWarning() << "File not found:" << fi.absoluteFilePath();
+    return 1;
+  }
+
+  bool okTimeout = false;
+  int timeout = parser.value(timeoutOpt).toInt(&okTimeout);
+
+  QQmlApplicationEngine engine;
+  engine.rootContext()->setContextProperty("fileArgument", fi.absoluteFilePath());
   engine.load(QUrl(QStringLiteral("qrc:/Main.qml")));
+
+  if (okTimeout && timeout > 0)
+  {
+    QTimer::singleShot(timeout * 1000, &app, &QCoreApplication::quit);
+  }
 
   return app.exec();
 }
