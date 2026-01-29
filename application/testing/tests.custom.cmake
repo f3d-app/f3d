38bf725bb07c6262aa6cfd819c0cd46bcd81cf08
Michael MIGLIORE <mcmigliore@gmail.com>
2026-01-29 10:02:32 +0100

    Update application/testing/tests.custom.cmake
    Co-authored-by: Mathieu Westphal <mathieu.westphal@gmail.com>

--- application/testing/tests.custom.cmake
+++ application/testing/tests.custom.cmake
@@ -24,5 +24,5 @@ add_test(NAME f3d::TestNoFileFileNameTemplate COMMAND $<TARGET_FILE:f3d> --outpu
 set_tests_properties(f3d::TestNoFileFileNameTemplate PROPERTIES PASS_REGULAR_EXPRESSION "no_file.png")
 set_tests_properties(f3d::TestNoFileFileNameTemplate PROPERTIES ENVIRONMENT "CTEST_F3D_NO_DATA_FORCE_RENDER=1")
 
-# Test that f3d can read DPI scaling from system
+# Test that f3d can read DPI scaling from system, note that this test has no reference baseline.
 add_test(NAME f3d::TestSystemDPIScaling COMMAND $<TARGET_FILE:f3d> ${F3D_SOURCE_DIR}/testing/data/suzanne.stl --dpi-aware --output=${CMAKE_BINARY_DIR}/Testing/Temporary/TestSystemDPIScaling.png)
