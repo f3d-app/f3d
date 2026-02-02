43efa84a54682863b8d7e37c9a740804013763ce
Larry Skuse <larry.skuse@gmail.com>
2026-02-02 18:40:22 -0500

    Define CLI options in JSON

--- application/F3DOptionsTools.cxx
+++ application/F3DOptionsTools.cxx
@@ -1,5 +1,6 @@
 #include "F3DOptionsTools.h"
 
+#include "F3DCLIOptions.h"
 #include "F3DConfig.h"
 #include "F3DException.h"
 #include "F3DPluginsTools.h"
@@ -18,12 +19,12 @@
 #include <sstream>
 
 #include "cxxopts.hpp"
+#include "nlohmann/json.hpp"
 
 namespace fs = std::filesystem;
 
 namespace
 {
-
 /**
  * A struct to configure a complete cxxopts CLI options
  * F3D uses exclusively string options except for a few true boolean option (eg: `--version`)
@@ -35,11 +36,11 @@ namespace
  */
 struct CLIOption
 {
-  std::string_view LongName;
-  std::string_view ShortName;
-  std::string_view HelpText;
-  std::string_view ValueHelper;
-  std::string_view ImplicitValue;
+  std::string LongName;
+  std::string ShortName;
+  std::string HelpText;
+  std::string ValueHelper;
+  std::string ImplicitValue;
 };
 
 /**
@@ -47,157 +48,72 @@ struct CLIOption
  */
 struct CLIGroup
 {
-  std::string_view GroupName;
+  std::string GroupName;
   std::vector<CLIOption> Options;
 };
 
+//----------------------------------------------------------------------------
 /**
- * Declaration of all F3D CLI options except `--input` using above structs
- * Order of groups matters in the context of `--help`
+ * Get all F3D CLI options except `--input`, `--define`, and `--reset`.
+ * Order of groups (in JSON) matters in the context of `--help`.
  */
-// clang-format off
-#if F3D_MODULE_RAYTRACING
-static inline const std::array<CLIGroup, 9> CLIOptions = {{
-#else
-static inline const std::array<CLIGroup, 8> CLIOptions = {{
-#endif
-  { "Applicative",
-    { { "output", "", "Render to file", "<png file>", "" },
-      { "no-background", "", "No background when render to file", "<bool>", "1" },
-      { "help", "h", "Print help", "", "" }, { "version", "", "Print version details", "", "" },
-      { "list-readers", "", "Print the list of readers", "", "" },
-      { "force-reader", "", "Force a specific reader to be used, disregarding the file extension", "<reader>", "1"},
-      { "list-bindings", "", "Print the list of interaction bindings and exits, ignored with `--no-render`, only considers the first file group.", "<bool>", "1" },
-      { "config", "", "Specify the configuration file to use. absolute/relative path or filename/filestem to search in configuration file locations", "<filePath/filename/fileStem>", "" },
-      { "no-config", "", "Do not read the configuration file", "<bool>", "1" },
-      { "no-render", "", "Do not render anything and quit right after loading the first file, use with --verbose to recover information about a file.", "<bool>", "1" },
-      { "rendering-backend", "", "Backend to use when rendering (auto|glx|wgl|egl|osmesa)", "<string>", "" },
-      { "list-rendering-backends", "", "Print the list of rendering backends available on this system", "", "" },
-      { "max-size", "", "Maximum size in Mib of a file to load, leave empty for unlimited", "<size in Mib>", "" },
-      { "dpi-aware", "","Scale font and window resolution according to system scale", "<bool>", "1" },
-#if F3D_MODULE_DMON
-      { "watch", "", "Watch current file and automatically reload it whenever it is modified on disk", "<bool>", "1" },
-#endif
-      { "frame-rate", "", "Frame rate used to refresh animation and other repeated tasks (watch, UI). Does not impact rendering frame rate.", "<fps>", "" },
-      { "load-plugins", "", "List of plugins to load separated with a comma", "<paths or names>", "" },
-      { "scan-plugins", "", "Scan standard directories for plugins and display available plugins (result can be incomplete)", "", "" },
-      { "screenshot-filename", "", "Screenshot filename", "<filename>", "" } } },
-  { "General",
-    { { "verbose", "", "Set verbose level, providing more information about the loaded data in the console output", "{debug, info, warning, error, quiet}", "debug" },
-      { "loading-progress", "", "Show loading progress bar", "<bool>", "1" },
-      { "animation-progress", "", "Show animation progress bar", "<bool>", "1" },
-      { "multi-file-mode", "", R"(Choose the behavior when opening multiple files. "single" will show one file at a time, "all" will show all files in a single scene, "dir" will show files from the same directory in the same scene.)", "<single|all|dir>", "" },
-      { "multi-file-regex", "", R"_(Regular expression pattern to group files. Captured groups are replaced with "*" so that, for example, the pattern "part(\d+)" would group files "foo-part1.xyz" and "foo-part2.xyz" together as "foo-part*.xyz")_", "<regex>", "" },
-      { "recursive-dir-add", "", "Add directories recursively", "<bool>", "1" },
-      { "remove-empty-file-groups", "", "Remove file groups that results into an empty scene", "<bool>", "1" },
-      { "up", "", "Up direction", "<direction>", "" },
-      { "axis", "x", "Show axes", "<bool>", "1" }, { "grid", "g", "Show grid", "<bool>", "1" },
-      { "x-color", "", "Color of the X axis", "<color>", "" },
-      { "y-color", "", "Color of the Y axis", "<color>", "" },
-      { "z-color", "", "Color of the Z axis", "<color>", "" },
-      { "grid-absolute", "", "Position grid at the absolute origin instead of below the model", "<bool>", "1" },
-      { "grid-unit", "", "Size of grid unit square, automatically computed by default", "<value>", "" },
-      { "grid-subdivisions", "", "Number of grid subdivisions", "<value>", "" },
-      { "grid-color", "", "Color of main grid lines", "<color>", "" },
-      { "axes-grid", "", "Enable grid axis", "<bool>", "1" },
-      { "edges", "e", "Show cell edges", "<bool>", "1" },
-      { "armature", "", "Enable armature visualization", "<bool>", "1" },
-      { "camera-index", "", "Select the camera to use", "<index>", "" },
-      { "interaction-trackball", "k", "Enable trackball interaction", "<bool>", "1" },
-      { "invert-zoom", "", "Invert zoom direction with right mouse click", "<bool>", "1" },
-      { "animation-autoplay", "", "Automatically start animation", "<bool>", "1" },
-      { "animation-index", "", "Select animation to show (deprecated)", "<index>", "" },
-      { "animation-indices", "", "Select animations to show", "<index,index,index>", "" },
-      { "animation-speed-factor", "", "Set animation speed factor", "<ratio>", "" },
-      { "animation-time", "", "Set animation time to load", "<time>", "" },
-      { "font-file", "", "Path to a FreeType compatible font file", "<file_path>", ""},
-      { "font-scale", "", "Scale fonts", "<ratio>", ""},
-#if F3D_MODULE_UI
-      { "backdrop-opacity", "", "UI backdrop opacity", "<value>", ""},
-#endif
-      { "command-script", "", "Path to a script file containing commands to execute", "<file_path>", "" } } },
-  { "Material",
-    { {"point-sprites", "o", R"(Select point sprites type ("none", "sphere", "gaussian", "circle", "stddev", "bound"))", "<string>", "sphere" },
-      {"point-sprites-type", "", "Point sprites type (deprecated)", "<sphere|gaussian>", ""},
-      {"point-sprites-size", "", "Point sprites sphere size", "<size>", ""},
-      {"point-sprites-absolute-size", "", "Do not scale point sprites size by scene size", "<bool>", "1"},
-      {"point-size", "", "Point size when showing vertices, model specified by default", "<size>", ""},
-      {"line-width", "", "Line width when showing edges, model specified by default", "<width>", ""},
-      {"backface-type", "", "Backface type, can be visible or hidden, model specified by default", "<visible|hidden>", ""},
-      {"color", "", "Solid color", "<color>", ""},
-      {"opacity", "", "Opacity", "<opacity>", ""},
-      {"roughness", "", "Roughness coefficient (0.0-1.0)", "<roughness>", ""},
-      {"metallic", "", "Metallic coefficient (0.0-1.0)", "<metallic>", ""},
-      {"base-ior", "", "Index Of Refraction of the base layer (1.0-2.5)", "<base-ior>", ""},
-      {"hdri-file", "", "Path to an image file that can be used as a light source and skybox", "<file path>", ""},
-      {"hdri-ambient", "f", "Enable HDRI ambient lighting", "<bool>", "1"},
-      {"hdri-skybox", "j", "Enable HDRI skybox background", "<bool>", "1"},
-      {"texture-matcap", "", "Path to a texture file containing a material capture", "<file path>", ""},
-      {"texture-base-color", "", "Path to a texture file that sets the color of the object", "<file path>", ""},
-      {"texture-material", "", "Path to a texture file that sets the Occlusion, Roughness and Metallic values of the object", "<file path>", ""},
-      {"texture-emissive", "", "Path to a texture file that sets the emitted light of the object", "<file path>", ""},
-      {"textures-transform", "", "3x3 matrix to transform textures", "<transform2d>", ""},
-      {"emissive-factor", "", "Emissive factor. This value is multiplied with the emissive color when an emissive texture is present", "<color>", ""},
-      {"texture-normal", "", "Path to a texture file that sets the normal map of the object", "<file path>", ""},
-      {"normal-scale", "", "Normal scale affects the strength of the normal deviation from the normal texture", "<normalScale>", ""} } },
-  {"Window",
-    { {"background-color", "", "Background color", "<color>", ""},
-      {"resolution", "", "Window resolution", "<width,height>", ""},
-      {"position", "", "Window position", "<x,y>", ""},
-      {"fps", "z", "Display rendering frame per second", "<bool>", "1"},
-      {"filename", "n", "Display filename", "<bool>", "1"},
-      {"metadata", "m", "Display file metadata", "<bool>", "1"},
-      {"hdri-filename", "", "Display hdri filename", "<bool>", "1"},
-      {"blur-background", "u", "Blur background", "<bool>", "1" },
-      {"blur-coc", "", "Blur circle of confusion radius", "<value>", ""},
-      {"light-intensity", "", "Light intensity", "<value>", ""} } },
-  {"Scientific visualization",
-    { {"scalar-coloring", "s", "Color by a scalar array", "<bool>", "1" },
-      {"coloring-array", "", "Name of the array to color with", "<array_name>", "" },
-      {"coloring-component", "y", "Component from the array to color with. -1 means magnitude, -2 or the short option, -y, means direct scalars", "<comp_index>", "-2"},
-      {"coloring-by-cells", "c", "Use an array from the cells", "<bool>", "1"},
-      {"coloring-range", "", "Custom range for the coloring by array, automatically computed by default", "<min,max>", ""},
-      {"coloring-scalar-bar", "b", "Show scalar bar", "<bool>", "1" },
-      {"colormap-file", "", "Specify a colormap image", "<filePath/filename/fileStem>", ""},
-      {"colormap-discretization", "", "Specify number of colors in colormap", "<int>", ""},
-      {"colormap", "", "Specify a custom colormap (ignored if \"colormap-file\" is specified)", "<color_list>", ""},
-      {"volume-opacity-map", "", "Specify a custom opacity map for volume rendering", "<opacity_list>", ""},
-      {"volume-opacity-file", "", "Specify a custom opacity map image for volume rendering", "<filePath/filename/fileStem>", ""},
-      {"volume", "v", "Show volume if the file is compatible", "<bool>", "1"},
-      {"volume-inverse", "i", "Inverse opacity function for volume rendering", "<bool>", "1"} } },
-  {"Camera",
-    { {"camera-position", "", "Camera position (overrides camera direction and camera zoom factor if any)", "<X,Y,Z>", ""},
-      {"camera-focal-point", "", "Camera focal point", "<X,Y,Z>", ""},
-      {"camera-view-up", "", "Camera view up", "<direction>", ""},
-      {"camera-view-angle", "", "Camera view angle (non-zero, in degrees)", "<angle>", ""},
-      {"camera-direction", "", "Camera direction", "<direction>", ""},
-      {"camera-zoom-factor", "", "Camera zoom factor (non-zero)", "<factor>", ""},
-      {"camera-azimuth-angle", "", "Camera azimuth angle (in degrees), performed after other camera options", "<angle>", ""},
-      {"camera-elevation-angle", "", "Camera elevation angle (in degrees), performed after other camera options", "<angle>", ""},
-      {"camera-orthographic", "", "Use an orthographic camera", "<bool>", "1"} } },
+[[nodiscard]] std::vector<CLIGroup> GetAllCLIOptions()
+{
+  const auto cliOptionsJson = nlohmann::json::parse(F3D::CLI_OPTIONS_JSON);
 
-#if F3D_MODULE_RAYTRACING
-  {"Raytracing",
-    { {"raytracing", "r", "Enable raytracing", "<bool>", "1"},
-      {"raytracing-samples", "", "Number of samples per pixel", "<samples>", ""},
-      {"raytracing-denoise", "d", "Denoise the image", "<bool>", "1"} } },
+  auto parseCliOptionJson = [](const nlohmann::json& cliOptionJson) -> CLIOption
+  {
+    CLIOption cliOption;
+    cliOption.LongName = cliOptionJson["longName"];
+    cliOption.ShortName = cliOptionJson["shortName"];
+    cliOption.HelpText = cliOptionJson["helpText"];
+    cliOption.ValueHelper = cliOptionJson["valueHelper"];
+    cliOption.ImplicitValue = cliOptionJson["implicitValue"];
+    return cliOption;
+  };
+
+  auto parseCliGroupJson = [&parseCliOptionJson](const nlohmann::json& cliGroupJson) -> CLIGroup
+  {
+    CLIGroup cliGroup;
+    cliGroup.GroupName = cliGroupJson["name"];
+    for (const auto& cliOptionJson : cliGroupJson["options"])
+    {
+#ifndef F3D_MODULE_DMON
+      if (cliOptionJson.value("conditional", "") == "F3D_MODULE_DMON")
+      {
+        continue;
+      }
 #endif
-  {"PostFX (OpenGL)",
-    { {"blending", "p", R"(Select translucency blending mode ("none", "ddp", "sort", "sort_cpu" or "stochastic"))", "<string>", "ddp"},
-      {"translucency-support", "", "Enable translucency blending (deprecated)", "<bool>", "1"},
-      {"ambient-occlusion", "q", "Enable ambient occlusion providing approximate shadows for better depth perception, implemented using SSAO", "<bool>", "1"},
-      {"anti-aliasing", "a", R"(Select anti-aliasing method ("none", "fxaa", "ssaa" or "taa"))", "<string>", "fxaa"},
-      {"anti-aliasing-mode", "", R"(Select anti-aliasing method ("fxaa", "ssaa" or "taa") (deprecated))", "<string>", "fxaa"},
-      {"tone-mapping", "t", "Enable Tone Mapping, providing balanced coloring", "<bool>", "1"},
-      {"final-shader", "", "Execute the final shader at the end of the rendering pipeline", "<GLSL code>", ""},
-      {"display-depth", "", "Display depth buffer as grayscale image or with a colormap if \"--scalar-coloring\" is specified", "<bool>", "1"} } },
-  {"Testing",
-    { {"reference", "", "Reference", "<png file>", ""},
-      {"reference-threshold", "", "Testing threshold", "<threshold>", ""},
-      {"interaction-test-record", "", "Path to an interaction log file to record interactions events to", "<file_path>", ""},
-      {"interaction-test-play", "", "Path to an interaction log file to play interaction events from when loading a file", "<file_path>", ""} } }
-}};
-// clang-format on
+#ifndef F3D_MODULE_UI
+      if (cliGroupJson.value("conditional", "") == "F3D_MODULE_UI")
+      {
+        continue;
+      }
+#endif
+      if (cliGroup.GroupName == "Applicative" &&
+        std::set<std::string>{ "input", "define", "reset" }.count(cliOptionJson["longName"]) != 0)
+      {
+        continue;
+      }
+      cliGroup.Options.push_back(parseCliOptionJson(cliOptionJson));
+    }
+    return cliGroup;
+  };
+
+  std::vector<CLIGroup> cliOptions;
+  for (const auto& cliGroupJson : cliOptionsJson["groups"])
+  {
+#ifndef F3D_MODULE_RAYTRACING
+    if (cliGroupJson.value("conditional", "") == "F3D_MODULE_RAYTRACING")
+    {
+      continue;
+    }
+#endif
+    cliOptions.push_back(parseCliGroupJson(cliGroupJson));
+  }
+
+  return cliOptions;
+}
 
 /**
  * True boolean options need to be filtered out in ParseCLIOptions
@@ -223,7 +139,8 @@ std::string CollapseName(const std::string_view& longName, const std::string_vie
 }
 
 //----------------------------------------------------------------------------
-void PrintHelp(const std::string& execName, const cxxopts::Options& cxxOptions)
+void PrintHelp(const std::string& execName, const cxxopts::Options& cxxOptions,
+  const std::vector<CLIGroup>& cliOptions)
 {
   const std::array<std::pair<std::string, std::string>, 4> examples = { {
     { execName + " file.vtu -xtgans",
@@ -236,8 +153,8 @@ void PrintHelp(const std::string& execName, const cxxopts::Options& cxxOptions)
   } };
 
   f3d::log::setUseColoring(false);
-  std::vector<std::string> orderedCLIGroupNames(CLIOptions.size());
-  std::transform(CLIOptions.cbegin(), CLIOptions.cend(), orderedCLIGroupNames.begin(),
+  std::vector<std::string> orderedCLIGroupNames(cliOptions.size());
+  std::transform(cliOptions.cbegin(), cliOptions.cend(), orderedCLIGroupNames.begin(),
     [](const ::CLIGroup& cliGroup) { return cliGroup.GroupName; });
   f3d::log::info(cxxOptions.help(orderedCLIGroupNames));
   f3d::log::info("\nExamples:");
@@ -456,12 +373,14 @@ F3DOptionsTools::OptionsDict F3DOptionsTools::ParseCLIOptions(
   std::vector<std::string> resets;
   auto cxxoptsResets = cxxopts::value<std::vector<std::string>>(resets);
 
+  auto CLIOptions = GetAllCLIOptions();
+
   try
   {
     cxxopts::Options cxxOptions(execName, F3D::AppTitle);
     cxxOptions.custom_help("[OPTIONS...]");
 
-    for (const ::CLIGroup& optionGroup : ::CLIOptions)
+    for (const ::CLIGroup& optionGroup : CLIOptions)
     {
       auto group = cxxOptions.add_options(std::string(optionGroup.GroupName));
 
@@ -550,7 +469,7 @@ F3DOptionsTools::OptionsDict F3DOptionsTools::ParseCLIOptions(
     // Check boolean options and log them if any
     if (result.count("help") > 0)
     {
-      ::PrintHelp(execName, cxxOptions);
+      ::PrintHelp(execName, cxxOptions, CLIOptions);
       throw F3DExNoProcess("help requested");
     }
     if (result.count("version") > 0)
--- resources/completion.zsh
+++ resources/completion.zsh
@@ -1,16 +1,47 @@
 #compdef f3d
 
-local longopts
-local shortopts
+# Convert /install/path/share/zsh/site-functions/_f3d -> /install/path/share/f3d/cli-options.json
+# The installation directory may vary, so find the json file relative to this script.
+local share_path="${${(%):-%x}:a:h:h:h}"
+local json_file="${share_path}/f3d/cli-options.json"
 local arguments
-local f3dhelp
 
-f3dhelp=$(f3d --help 2>&1 | sed '1,/Examples/!d' | sed 's/\[.*\] *//')
+if [[ -f "$json_file" ]] && command -v python3 &> /dev/null; then
+    # Parse JSON file using Python
+    local opts_data
+    opts_data=$(python3 -c "
+import json
+with open('$json_file') as f:
+    data = json.load(f)
+for group in data.get('groups', []):
+    for opt in group.get('options', []):
+        long_name = opt.get('longName', '')
+        short_name = opt.get('shortName', '')
+        help_text = opt.get('helpText', '')
+        if short_name:
+            print(f'-{short_name}[{help_text}]')
+        if long_name:
+            print(f'--{long_name}[{help_text}]')
+" 2>/dev/null)
+    
+    if [[ -n "$opts_data" ]]; then
+        arguments=("${(f)opts_data}")
+    fi
+fi
 
-shortopts=$(echo $f3dhelp | grep "^\s*[-]., --" | sed "s/^ *\(-.\), *--[^ ]* *\(.*\)$/\1[\2]/")
-longopts=$(echo $f3dhelp | grep "[, ] [-]-" | sed 's/=.*>//g' | sed 's/-.,//g' | sed "s/^ *\([^ ]*\) *\(.*\)$/\1[\2]/g")
-
-arguments=("${(f)shortopts}")
-arguments+=("${(f)longopts}")
+# Fallback to parsing --help if JSON parsing failed
+if [[ -z "$arguments" ]]; then
+    local f3dhelp
+    f3dhelp=$(f3d --help 2>&1 | sed '1,/Examples/!d' | sed 's/\[.*\] *//')
+    
+    local shortopts
+    local longopts
+    
+    shortopts=$(echo $f3dhelp | grep "^\s*[-]., --" | sed "s/^ *\(-.\), *--[^ ]* *\(.*\)$/\1[\2]/")
+    longopts=$(echo $f3dhelp | grep "[, ] [-]-" | sed 's/=.*>//g' | sed 's/-.,//g' | sed "s/^ *\([^ ]*\) *\(.*\)$/\1[\2]/g")
+    
+    arguments=("${(f)shortopts}")
+    arguments+=("${(f)longopts}")
+fi
 
 _arguments "$arguments[@]" "*:file:_files"
