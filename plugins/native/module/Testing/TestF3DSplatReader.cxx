9bbd222d0a05aadbdbf8ca9d79f794c49e833688
Mathieu Westphal <mathieu.westphal@kitware.com>
2026-02-05 20:19:16 +0100

    coverage

--- plugins/native/module/Testing/TestF3DSplatReader.cxx
+++ plugins/native/module/Testing/TestF3DSplatReader.cxx
@@ -30,7 +30,7 @@ int TestF3DSplatReader(int vtkNotUsed(argc), char* argv[])
 
   vtkIdType nbPoints = reader->GetOutput()->GetNumberOfPoints();
 
-  if (nbPoints != 52293) // <-- Change this to expected number of splats
+  if (nbPoints != 52293)
   {
     std::cerr << "Incorrect number of splats: " << nbPoints << "\n";
     return EXIT_FAILURE;
@@ -51,6 +51,27 @@ int TestF3DSplatReader(int vtkNotUsed(argc), char* argv[])
   if (vtkF3DSplatReader::CanReadFile(stream))
   {
     std::cerr << "Unexpected CanReadFile success\n";
+    return EXIT_FAILURE;
+  }
+
+  path = std::string(argv[1]) + "data/empty.splat";
+  if (!stream->Open(path.c_str()))
+  {
+    std::cerr << "Cannot open empty.splat file\n";
+    return EXIT_FAILURE;
+  }
+  if (vtkF3DSplatReader::CanReadFile(stream))
+  {
+    std::cerr << "Unexpected CanReadFile success with empty file\n";
+    return EXIT_FAILURE;
+  }
+
+  reader->SetStream(stream);
+  reader->Update();
+  nbPoints = reader->GetOutput()->GetNumberOfPoints();
+  if (nbPoints != 0)
+  {
+    std::cerr << "Incorrect number of splats: " << nbPoints << " for empty file\n";
     return EXIT_FAILURE;
   }
 
--- plugins/native/module/vtkF3DSplatReader.cxx
+++ plugins/native/module/vtkF3DSplatReader.cxx
@@ -142,5 +142,5 @@ bool vtkF3DSplatReader::CanReadFile(vtkResourceStream* stream)
 
   // Check first splat scales are all positive
   return std::all_of(
-    std::begin(splat.scale), std::end(splat.scale), [](float val){ return val > 0; });
+    std::begin(splat.scale), std::end(splat.scale), [](float val) { return val > 0; });
 }
