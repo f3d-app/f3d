11e708aeada62e4e9b7724b04bc442ece2c9900b
Nig3l <cots.max@gmail.com>
2026-02-24 19:42:16 +0100

    Remove useless comment on TestFinalShader

--- application/testing/tests.features.cmake
+++ application/testing/tests.features.cmake
@@ -418,7 +418,6 @@ endif()
 
 ## Final shader
 
-# Add labels manually because adding thanks to f3d_test methods escape ";}" at the end of the final shader.
 f3d_test(NAME TestFinalShaderNegative DATA cow.vtp ARGS --final-shader "vec4 pixel(vec2 uv){return vec4(vec3(1.0) - texture(source, uv).rgb, 1.0)\\\\\\\;}")
 f3d_test(NAME TestFinalShaderNegativeFileName DATA cow.vtp ARGS --filename --final-shader "vec4 pixel(vec2 uv){return vec4(vec3(1.0) - texture(source, uv).rgb, 1.0)\\\\\\\\;}" UI)
 f3d_test(NAME TestFinalShaderUndefined DATA cow.vtp ARGS --final-shader "undefined" REGEXP "Final shader must define a function" NO_BASELINE)
