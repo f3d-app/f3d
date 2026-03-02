7fac73d53ed1912381fbbc2278f52e467f4172cf
Mathieu Westphal <mathieu.westphal@kitware.com>
2026-03-02 21:18:58 +0100

    proper LAZ support

--- application/testing/tests.pdal.cmake
+++ application/testing/tests.pdal.cmake
@@ -1,1 +1,10 @@
+## Tests that needs pdal plugin
+## This file is only added if pdal is enabled
 f3d_test(NAME TestLAZ DATA simple.laz ARGS --load-plugins=pdal --scalar-coloring --coloring-array=Color --coloring-component=-2)
+
+if(NOT F3D_MACOS_BUNDLE)
+  file(COPY "${F3D_SOURCE_DIR}/plugins/pdal/configs/config.d/" DESTINATION "${CMAKE_BINARY_DIR}/share/f3d/configs/config_build.d")
+  f3d_test(NAME TestDefaultConfigFileLAZ DATA simple.laz CONFIG config_build LONG_TIMEOUT)
+  file(COPY "${F3D_SOURCE_DIR}/plugins/pdal/configs/thumbnail.d/" DESTINATION "${CMAKE_BINARY_DIR}/share/f3d/configs/thumbnail_build.d")
+  f3d_test(NAME TestThumbnailConfigFileLAZ DATA simple.laz CONFIG thumbnail_build LONG_TIMEOUT)
+endif()
---
+++ plugins/pdal/module/vtkF3DPDALReader.h
@@ -0,0 +1,31 @@
+/**
+ * @class   vtkF3DPDALReader
+ * @brief   Specialized version of the PDAL reader to read direct scalars data
+ *
+ * A specialized version of the vtkPDALReader that is able to read colors to be displayed
+ * as direct scalars
+ */
+
+#ifndef vtkF3DPDALReader_h
+#define vtkF3DPDALReader_h
+
+#include <vtkPDALReader.h>
+
+class vtkF3DPDALReader : public vtkPDALReader
+{
+public:
+  static vtkF3DPDALReader* New();
+  vtkTypeMacro(vtkF3DPDALReader, vtkPDALReader);
+
+protected:
+  vtkF3DPDALReader() = default;
+  ~vtkF3DPDALReader() override = default;
+
+  int RequestData(vtkInformation*, vtkInformationVector**, vtkInformationVector*) override;
+
+private:
+  vtkF3DPDALReader(const vtkF3DPDALReader&) = delete;
+  void operator=(const vtkF3DPDALReader&) = delete;
+};
+
+#endif
