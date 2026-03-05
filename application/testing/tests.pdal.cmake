9a9d546c11c364b444d07e70af93da00a020b10a
Mathieu Westphal <mathieu.westphal@kitware.com>
2026-03-05 07:24:14 +0100

    adding qi format

--- application/testing/tests.pdal.cmake
+++ application/testing/tests.pdal.cmake
@@ -12,6 +12,10 @@ f3d_test(NAME TestCSD DATA sample.csd PLUGIN pdal ARGS --scalar-coloring --camer
 f3d_test(NAME TestPCD DATA autzen-utm.pcd PLUGIN pdal ARGS --scalar-coloring --coloring-array=Color --coloring-component=-1)
 f3d_test(NAME TestPTX DATA 1.2-with-color.ptx PLUGIN pdal ARGS --scalar-coloring --coloring-array=Color --coloring-component=-1)
 
+# Default camera position goes wrong because of https://github.com/f3d-app/f3d/issues/2921
+# and the only sample file found was the one from pdal
+f3d_test(NAME TestQI DATA 10-word.qi PLUGIN pdal ARGS --scalar-coloring --camera-direction=-1,-1,-1)
+
 if(NOT F3D_MACOS_BUNDLE)
   file(COPY "${F3D_SOURCE_DIR}/plugins/pdal/configs/config.d/" DESTINATION "${CMAKE_BINARY_DIR}/share/f3d/configs/config_build.d")
   if(VTK_VERSION VERSION_GREATER_EQUAL 9.5.20251001)
@@ -22,6 +26,7 @@ if(NOT F3D_MACOS_BUNDLE)
     f3d_test(NAME TestDefaultConfigFileCSD DATA sample.csd ARGS --camera-view-angle=15 --camera-zoom-factor=1 --camera-position=-10,10,-10 CONFIG config_build LONG_TIMEOUT LABELS "plugin;pdal")
     f3d_test(NAME TestDefaultConfigFilePCD DATA autzen-utm.pcd CONFIG config_build LONG_TIMEOUT LABELS "plugin;pdal" THRESHOLD 0.05)
     f3d_test(NAME TestDefaultConfigFilePTX DATA 1.2-with-color.ptx CONFIG config_build LONG_TIMEOUT LABELS "plugin;pdal")
+    f3d_test(NAME TestDefaultConfigFileQI DATA 10-word.qi ARGS --camera-direction=-1,-1,-1 CONFIG config_build LONG_TIMEOUT LABELS "plugin;pdal")
   endif()
   file(COPY "${F3D_SOURCE_DIR}/plugins/pdal/configs/thumbnail.d/" DESTINATION "${CMAKE_BINARY_DIR}/share/f3d/configs/thumbnail_build.d")
   f3d_test(NAME TestThumbnailConfigFileLAS DATA warsaw_small.las CONFIG thumbnail_build LONG_TIMEOUT LABELS "plugin;pdal")
@@ -31,4 +36,5 @@ if(NOT F3D_MACOS_BUNDLE)
   f3d_test(NAME TestThumbnailConfigFileCSD DATA sample.csd ARGS --camera-view-angle=15 --camera-zoom-factor=1 --camera-position=-10,10,-10 CONFIG thumbnail_build LONG_TIMEOUT LABELS "plugin;pdal")
   f3d_test(NAME TestThumbnailConfigFilePCD DATA autzen-utm.pcd CONFIG thumbnail_build LONG_TIMEOUT LABELS "plugin;pdal")
   f3d_test(NAME TestThumbnailConfigFilePTX DATA 1.2-with-color.ptx CONFIG thumbnail_build LONG_TIMEOUT LABELS "plugin;pdal")
+  f3d_test(NAME TestThumbnailConfigFileQI DATA 10-word.qi ARGS --camera-direction=-1,-1,-1 CONFIG thumbnail_build LONG_TIMEOUT LABELS "plugin;pdal")
 endif()
