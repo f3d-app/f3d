07b7b45d39de3eb8f4c4e25f598092d720b9920e
Edward Shao <shaoe@usc.edu>
2026-06-16 02:28:45 -0400

    Renamed file and removed unncessary comment for TestSDKImageStream

---
+++ library/testing/TestSDKImageStream.cxx
@@ -0,0 +1,41 @@
+#include "PseudoUnitTest.h"
+
+#include <image.h>
+
+#include <algorithm>
+#include <random>
+
+int TestSDKImageStream([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
+{
+  PseudoUnitTest test;
+
+  constexpr unsigned int width = 64;
+  constexpr unsigned int height = 64;
+  constexpr unsigned int channels = 3;
+
+  // fill with deterministic random values
+  std::mt19937 randGenerator;
+
+  f3d::image generated(width, height, channels);
+  std::vector<uint8_t> pixels(width * height * channels);
+  std::ranges::generate(pixels, [&]() { return static_cast<uint8_t>(randGenerator() % 256); });
+  generated.setContent(pixels.data());
+
+  // check reading stream
+  std::vector<unsigned char> generatedBuffer = generated.saveBuffer();
+  std::byte* bufferData = reinterpret_cast<std::byte*>(generatedBuffer.data());
+  f3d::image bufferImage(bufferData, generatedBuffer.size());
+  test("check loading stream from image reader", generated.compare(bufferImage), 0.0);
+
+  // check reading inexistent/null stream
+  test.expect<f3d::image::read_exception>(
+    "read image from invalid/null stream", [&]() { f3d::image nullImgStream(nullptr, 10); });
+
+  // check reading invalid stream
+  std::vector<unsigned char> invalidBuffer = { 0, 1, 2, 3, 4, 5 };
+  std::byte* invalidBufferData = reinterpret_cast<std::byte*>(invalidBuffer.data());
+  test.expect<f3d::image::read_exception>("read image from invalid stream",
+    [&]() { f3d::image invalidImgStream(invalidBufferData, 10); });
+
+  return test.result();
+}
