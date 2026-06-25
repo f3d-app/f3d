27480b3a0e5902bd3743469145a942f0e8328522
Michael Migliore <mcmigliore@gmail.com>
2026-06-25 15:46:32 +0200

    ci

--- application/testing/tests.documentation.cmake
+++ application/testing/tests.documentation.cmake
@@ -134,7 +134,7 @@ f3d_test_doc(NAME TestDocGridOn DATA DamagedHelmet.glb REF_IMAGE grid_on.png ROT
 
 ## --axes-grid
 ### OFF: damaged_helmet_baseline.png
-f3d_test_doc(NAME TestDocAxesGridOn DATA DamagedHelmet.glb REF_IMAGE axes_grid_on.png ROTATE ARGS --axes-grid -f --anti-aliasing=none)
+f3d_test_doc(NAME TestDocAxesGridOn DATA DamagedHelmet.glb REF_IMAGE axes_grid_on.png ROTATE ARGS --axes-grid -f --anti-aliasing=none THRESHOLD 0.08)
 
 ## --edges
 ### OFF: damaged_helmet_baseline.png
