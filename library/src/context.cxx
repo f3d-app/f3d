24d8680908536749cb08e2331583813cb8b6ae58
Michael Migliore <mcmigliore@gmail.com>
2026-06-26 15:53:26 +0200

    Add OSMesa support on all OS

--- library/src/context.cxx
+++ library/src/context.cxx
@@ -16,13 +16,18 @@
 namespace f3d
 {
 //----------------------------------------------------------------------------
-context::function context::getSymbol(std::string_view lib, std::string_view func)
+std::string context::addLibraryDecoration(std::string_view lib)
 {
   std::string libName = vtksys::DynamicLoader::LibPrefix();
   libName += lib;
   libName += vtksys::DynamicLoader::LibExtension();
+  return libName;
+}
 
-  vtksys::DynamicLoader::LibraryHandle handle = vtksys::DynamicLoader::OpenLibrary(libName);
+//----------------------------------------------------------------------------
+context::function context::getSymbol(std::string_view lib, std::string_view func)
+{
+  vtksys::DynamicLoader::LibraryHandle handle = vtksys::DynamicLoader::OpenLibrary(lib.data());
 
   if (!handle)
   {
@@ -46,7 +51,7 @@ context::function context::getSymbol(std::string_view lib, std::string_view func
 context::function context::glx()
 {
 #if defined(VTK_USE_X)
-  return getSymbol("GLX", "glXGetProcAddress");
+  return getSymbol(addLibraryDecoration("GLX"), "glXGetProcAddress");
 #else
   throw loading_exception("Cannot use a GLX context on this platform");
 #endif
@@ -91,7 +96,7 @@ context::function context::egl()
 {
 #if defined(VTK_OPENGL_HAS_EGL)
   gladLoaderLoadEGL(EGL_NO_DISPLAY);
-  return getSymbol("EGL", "eglGetProcAddress");
+  return getSymbol(addLibraryDecoration("EGL"), "eglGetProcAddress");
 #else
   throw loading_exception("Cannot use a EGL context on this platform");
 #endif
@@ -100,10 +105,12 @@ context::function context::egl()
 //----------------------------------------------------------------------------
 context::function context::osmesa()
 {
-#if defined(__linux__) || defined(__FreeBSD__)
-  return getSymbol("OSMesa", "OSMesaGetProcAddress");
+#if defined(__APPLE__)
+  return getSymbol("libOSMesa.dylib", "OSMesaGetProcAddress");
+#elif defined(__linux__) || defined(__FreeBSD__)
+  return getSymbol("libOSMesa.so", "OSMesaGetProcAddress");
 #elif _WIN32
-  return getSymbol("osmesa", "OSMesaGetProcAddress");
+  return getSymbol("osmesa.dll", "OSMesaGetProcAddress");
 #else
   throw loading_exception("Cannot use a OSMesa context on this platform");
 #endif
