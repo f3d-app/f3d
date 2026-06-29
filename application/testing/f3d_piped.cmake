c3f24c9ce0e266065a1186a46028316e0e9fb1a8
Jean Fechter <jean.fechter@gmail.com>
2026-06-29 10:23:17 +0200

    Add PIPED_INPUT

--- application/testing/f3d_piped.cmake
+++ application/testing/f3d_piped.cmake
@@ -2,8 +2,17 @@ cmake_minimum_required(VERSION 3.19)
 
 separate_arguments(F3D_ARGS NATIVE_COMMAND ${F3D_ARGS})
 
+# By default the piped data is passed as the input model through the `-` filename.
+# When F3D_PIPED_NO_FILENAME is set, no filename is provided and the data is expected
+# to be consumed through an option such as `--load-statefile=-`.
+if(F3D_PIPED_NO_FILENAME)
+  set(F3D_PIPED_FILENAME_ARG)
+else()
+  set(F3D_PIPED_FILENAME_ARG -)
+endif()
+
 execute_process(
-  COMMAND ${F3D_EXE} - ${F3D_ARGS}
+  COMMAND ${F3D_EXE} ${F3D_PIPED_FILENAME_ARG} ${F3D_ARGS}
   INPUT_FILE ${F3D_PIPED_DATA}
   COMMAND_ERROR_IS_FATAL ANY
   )
