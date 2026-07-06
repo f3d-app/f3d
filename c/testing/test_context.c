cbf9b7e63ac3900203eb299d579efc0191635825
SahilSonar-04 <sonarsahil4@gmail.com>
2026-07-06 23:31:08 +0530

    fix(test): replace always-true assertions with plain calls in test_context.c

--- c/testing/test_context.c
+++ c/testing/test_context.c
@@ -6,40 +6,35 @@ int test_context()
 {
   f3d_test_t test;
   f3d_test_init(&test);
-  
+
   f3d_context_t* context;
 
-  // none of the functions should throw exception
+  // none of these should throw; a crash fails the test binary regardless
   context = f3d_context_glx();
-  f3d_test_check(&test, "glx context call did not throw", 1);
   if (context)
   {
     f3d_context_delete(context);
   }
 
   context = f3d_context_wgl();
-  f3d_test_check(&test, "wgl context call did not throw", 1);
   if (context)
   {
     f3d_context_delete(context);
   }
 
   context = f3d_context_cocoa();
-  f3d_test_check(&test, "cocoa context call did not throw", 1);
   if (context)
   {
     f3d_context_delete(context);
   }
 
   context = f3d_context_egl();
-  f3d_test_check(&test, "egl context call did not throw", 1);
   if (context)
   {
     f3d_context_delete(context);
   }
 
   context = f3d_context_osmesa();
-  f3d_test_check(&test, "osmesa context call did not throw", 1);
   if (context)
   {
     f3d_context_delete(context);
