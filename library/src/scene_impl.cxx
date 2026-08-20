1a3a50c611cf09b7da06dd44867d5a97469f9b25
Kirill Adamov <highwayman180298@gmail.com>
2026-08-20 23:12:13 +0300

    remove add var

--- library/src/scene_impl.cxx
+++ library/src/scene_impl.cxx
@@ -258,11 +258,10 @@ scene& scene_impl::add(const std::vector<fs::path>& filePaths)
       throw scene::load_failure_exception(filePath.string() + " does not exists");
     }
     std::optional<std::string> forceReader = this->Internals->Options.scene.force_reader;
-    const bool skipContentCheck = this->Internals->Options.scene.skip_content_check;
     file_availability availability = f3d::file_availability::UNSUPPORTED_EXTENSION;
     // Recover the importer for the provided file path
     const f3d::reader* reader = f3d::factory::instance()->getReader(
-      filePath.string(), forceReader, skipContentCheck, availability);
+      filePath.string(), forceReader, this->Internals->Options.scene.skip_content_check, availability);
     auto fail = [&](const std::string& message)
     {
       if (forceReader)
