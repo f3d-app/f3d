32165611809cbe5d636b8cfeb1799030189ffd9c
Simplify_ <amitharavindpai@gmail.com>
2026-09-02 13:13:35 +0000

    #3233 : Re-added support to dxf r12 format. Now ignores leading whitespaces

--- plugins/assimp/module/vtkF3DAssimpImporter.cxx
+++ plugins/assimp/module/vtkF3DAssimpImporter.cxx
@@ -45,6 +45,9 @@
 #include <regex>
 #include <set>
 #include <unordered_map>
+#include <string>
+#include <algorithm>
+#include <cctype>
 
 vtkStandardNewMacro(vtkF3DAssimpImporter);
 
@@ -1474,8 +1477,14 @@ bool vtkF3DAssimpImporter::CanReadFile(vtkResourceStream* stream, std::string& h
     parser->ReadLine(line3) == vtkParseResult::EndOfLine &&
     parser->ReadLine(line4) == vtkParseResult::EndOfLine)
   {
+    
+    line1.erase(line1.begin(), std::find_if(line1.begin(),line1.end(), [] (unsigned char ch) {return !std::isspace(ch);}));
+    line2.erase(line2.begin(), std::find_if(line2.begin(),line2.end(), [] (unsigned char ch) {return !std::isspace(ch);}));
+    line3.erase(line3.begin(), std::find_if(line3.begin(),line3.end(), [] (unsigned char ch) {return !std::isspace(ch);}));
+    line4.erase(line4.begin(), std::find_if(line4.begin(),line4.end(), [] (unsigned char ch) {return !std::isspace(ch);}));
+
     if (line1.starts_with("0") && line2.starts_with("SECTION") && line3.starts_with("2") &&
-      line4.starts_with("HEADER"))
+      ((line4.starts_with("HEADER")) || line4.starts_with("ENTITIES")))
     {
       hint = "dxf";
       return true;
