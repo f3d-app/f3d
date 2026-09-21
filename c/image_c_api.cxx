a2e6f020b6f5745dc453794965ba52a06b2da39c
Edward Shao <shaoe@usc.edu>
2026-09-21 02:50:10 -0700

    Removed duplicate function

--- c/image_c_api.cxx
+++ c/image_c_api.cxx
@@ -78,6 +78,7 @@ f3d_image_t* f3d_image_create_path(const char* path)
   return reinterpret_cast<f3d_image_t*>(img);
 }
 
+//----------------------------------------------------------------------------
 f3d_image_t* f3d_image_new_stream(unsigned char* buffer, unsigned int size)
 {
   if (!buffer)
@@ -91,25 +92,6 @@ f3d_image_t* f3d_image_new_stream(unsigned char* buffer, unsigned int size)
   try
   {
     img = new f3d::image(reinterpret_cast<std::byte*>(buffer), size);
-  }
-  catch (const f3d::image::read_exception& e)
-  {
-    std::cerr << "Error loading image: " << e.what() << "\n";
-    return nullptr;
-  }
-
-  return reinterpret_cast<f3d_image_t*>(img);
-}
-
-//----------------------------------------------------------------------------
-f3d_image_t* f3d_image_new_stream(unsigned char* byte, unsigned int size)
-{
-
-  f3d::image* img = nullptr;
-
-  try
-  {
-    img = new f3d::image(reinterpret_cast<std::byte*>(byte), static_cast<size_t>(size));
   }
   catch (const f3d::image::read_exception& e)
   {
