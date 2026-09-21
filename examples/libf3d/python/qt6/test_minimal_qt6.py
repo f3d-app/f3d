74983802110951677bf5bb827668353ba5f44316
snoyer <noyer.stephane@gmail.com>
2026-09-21 17:08:47 +0400

    improve skipping of tests with optional dependencies

--- examples/libf3d/python/qt6/test_minimal_qt6.py
+++ examples/libf3d/python/qt6/test_minimal_qt6.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 
-from minimal_qt6 import main
-
-from PySide6.QtWidgets import QApplication
+import pytest
 
 
 def test_minimal_qt6():
+    pytest.importorskip("PySide6")
+    from minimal_qt6 import main
+    from PySide6.QtWidgets import QApplication
+
     example_script = Path(__file__).parent.parent / "qt6" / "minimal_qt6.py"
     file = Path(__file__).parent.parent / "tests" / "data" / "cow.vtp"
     if app := QApplication.instance():
