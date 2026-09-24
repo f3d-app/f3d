6fec1e39d93cd3a7b21d9d18478bbdc8431a73a0
CPerry26 <codyy@live.com>
2026-09-24 19:09:22 -0400

    ISSUE-2082: VTK changes

--- library/src/interactor_impl.cxx
+++ library/src/interactor_impl.cxx
@@ -2048,6 +2048,10 @@ bool interactor_impl::playInteraction(const fs::path& file, double loopTime)
 
     bool loop = this->Internals->StartEventLoop(loopTime);
     this->Internals->Recorder->SetFileName(file.string().c_str());
+
+    // Tell the VTK event recorder to handle stream versions < 1.3 for old repeat count behavior
+    // compatibility
+    this->Internals->Recorder->OldRepeatCountWorkAroundOn();
     this->Internals->Recorder->Play();
 
     if (loop)
