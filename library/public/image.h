080b7370190abefe8ea4f70d6520ce4916d4e076
Edward Shao <shaoe@usc.edu>
2026-09-25 02:56:30 -0700

    Update library/public/image.h docs
    Co-authored-by: Michael MIGLIORE <mcmigliore@gmail.com>

--- library/public/image.h
+++ library/public/image.h
@@ -60,7 +60,7 @@ public:
   /**
    * Read provided buffer into a new image instance, the following formats are
    * supported: PNG, PNM , BMP, HDR, JPEG, TGA, WebP. EXR files are also
-   * supported if the associated module is built. calling getSupportedFormats(). The complete list
+   * supported if the associated module is built. The complete list
    * can be retrieved at runtime by calling getSupportedFormats(). The format is identified by
    * parsing the stream header. VTK >= 9.6.20260128 required.
    *
